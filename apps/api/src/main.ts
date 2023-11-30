2522551800dd13fb89d16265f029127efd997e35
drmodun <jan.modun.st@gmail.com>
2023-11-30 16:26:47 +0100

    added swagger config

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -1,12 +1,21 @@
 import { NestFactory, Reflector } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ClassSerializerInterceptor, ValidationPipe } from '@nestjs/common';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
   app.useGlobalInterceptors(new ClassSerializerInterceptor(app.get(Reflector)));
   app.setGlobalPrefix('/api');
+  const config = new DocumentBuilder()
+    .setTitle('DDays API')
+    .setDescription('Backend for DDays application')
+    .setVersion('0.1')
+    .addBearerAuth()
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('swagger', app, document);
   await app.listen(3000);
 }
 bootstrap();
