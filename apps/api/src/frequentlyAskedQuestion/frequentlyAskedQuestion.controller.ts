a2a4635c52f629181797d8614bfdd225f7e64960
ivojovanovic01 <ivo.jovanovic2@gmail.com>
2023-12-02 11:47:13 +0100

    added crud endpoints in controller

--- apps/api/src/frequentlyAskedQuestion/frequentlyAskedQuestion.controller.ts
+++ apps/api/src/frequentlyAskedQuestion/frequentlyAskedQuestion.controller.ts
@@ -1,7 +1,35 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Post, Put } from '@nestjs/common';
 import { FrequentlyAskedQuestionService } from './frequentlyAskedQuestion.service';
 
 @Controller('faq')
 export class FrequentlyAskedQuestionController {
   constructor(private readonly frequentlyAskedQuestionService: FrequentlyAskedQuestionService) {}
+
+  @Get()
+  async getAll() {
+    const frequentlyAskedQuestions = await this.frequentlyAskedQuestionService.getAll();
+
+    return frequentlyAskedQuestions;
+  }
+
+  @Post()
+  async create() {
+    const createdFrequentlyAskedQuestion = await this.frequentlyAskedQuestionService.create();
+
+    return createdFrequentlyAskedQuestion;
+  }
+
+  @Put(':id')
+  async update() {
+    const updatedFrequentlyAskedQuestion = await this.frequentlyAskedQuestionService.update();
+
+    return updatedFrequentlyAskedQuestion;
+  }
+
+  @Delete(':id')
+  async remove() {
+    const deletedFrequentlyAskedQuestion = await this.frequentlyAskedQuestionService.remove();
+
+    return deletedFrequentlyAskedQuestion;
+  }
 }
