bea91d8368fd89a5c567eafb17c43c1299df035e
ivojovanovic01 <ivo.jovanovic2@gmail.com>
2023-12-02 13:08:50 +0100

    added create and update dtos witohout validation

---
+++ apps/api/src/frequentlyAskedQuestion/dto/create-frequentlyAskedQuestion.dto.ts
@@ -0,0 +1,5 @@
+
+export class CreateFrequentlyAskedQuestionDto {
+  question: string;
+  answer: string;
+}
---
+++ apps/api/src/frequentlyAskedQuestion/dto/update-frequentlyAskedQuestion.dto.ts
@@ -0,0 +1,5 @@
+
+export class UpdatedFrequentlyAskedQuestion {
+    question: string;
+    answer: string;
+}
