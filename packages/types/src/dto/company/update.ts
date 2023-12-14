961d8ed55c8b7f56c2ea2a2a0b8b133f7f11a2c7
dujesa <duje@theremnants.app>
2023-12-14 03:00:26 +0100

    Remove partial type from update dto

--- packages/types/src/dto/company/update.ts
+++ packages/types/src/dto/company/update.ts
@@ -1,9 +1,32 @@
-import { getCreateCompanyDto } from './create';
-import { PartialType } from '@nestjs/mapped-types';
+import { IsEnum, IsNumber, IsString } from 'class-validator';
+import { SponsorCategory } from 'src/model';
 
 export const getUpdateCompanyDto = (ApiPropertySwagger?: any) => {
-    const CreateCompanyDto = getCreateCompanyDto(ApiPropertySwagger);
-    class UpdateCompanyDto extends PartialType(CreateCompanyDto) {
-    }
-    return UpdateCompanyDto
-}
+  const ApiProperty = ApiPropertySwagger || function () {};
+  class UpdateCompanyDto {
+    @IsString()
+    @ApiProperty()
+    name: string;
+
+    @IsString()
+    @ApiProperty()
+    description: string;
+
+    @IsEnum(SponsorCategory)
+    @ApiProperty()
+    sponsorCategory: string;
+
+    @IsString()
+    @ApiProperty()
+    websiteUrl: string;
+
+    @IsString()
+    @ApiProperty()
+    boothLocation: string;
+
+    @IsNumber()
+    @ApiProperty()
+    codeId: number;
+  }
+  return UpdateCompanyDto;
+};
