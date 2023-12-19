350517a1185a51b326d4c51a236c5130a0d02206
tsiklic1 <100917481+tsiklic1@users.noreply.github.com>
2023-12-19 08:30:59 +0100

    changing table component to take object[] or undefined

--- apps/admin/src/components/Table/Table.tsx
+++ apps/admin/src/components/Table/Table.tsx
@@ -3,7 +3,7 @@ import c from './Table.module.scss';
 
 type TableProps = {
   headers: string[];
-  data: object[];
+  data: object[] | undefined;
   buttonActions?: {
     label: string;
     action: (row: object) => void;
@@ -25,32 +25,32 @@ const Table: React.FC<TableProps> = ({ headers, data, buttonActions = [] }) => {
         </tr>
       </thead>
       <tbody>
-        {data.map((row, i) => {
-          return (
-            <tr key={i}>
-              {Object.values(row).map((value, i) => {
-                return (
-                  <td className={c.td} key={i}>
-                    {value}
-                  </td>
-                );
-              })}
-              <td className={c.buttonActions}>
-                {buttonActions.map((action, i) => {
+        {!!data &&
+          data.map((row, i) => {
+            return (
+              <tr key={i}>
+                {Object.values(row).map((value, i) => {
                   return (
-                    <Button
-                      variant="secondary"
-                      onClick={() => action.action(row)}
-                      key={i}
-                    >
-                      {action.label}
-                    </Button>
+                    <td className={c.td} key={i}>
+                      {value}
+                    </td>
                   );
                 })}
-              </td>
-            </tr>
-          );
-        })}
+                <td className={c.buttonActions}>
+                  {buttonActions.map((action, i) => {
+                    return (
+                      <Button
+                        variant='secondary'
+                        onClick={() => action.action(row)}
+                        key={i}>
+                        {action.label}
+                      </Button>
+                    );
+                  })}
+                </td>
+              </tr>
+            );
+          })}
       </tbody>
     </table>
   );
