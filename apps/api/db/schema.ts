213ca21877f153eadac1c6685ad293b65a6b8018
drmodun <jan.modun.st@gmail.com>
2023-12-25 15:36:00 +0100

    fixing schema and stuff

--- apps/api/db/schema.ts
+++ apps/api/db/schema.ts
@@ -88,7 +88,7 @@ export const sponsorCategory = pgEnum('sponsor_category', [
 
 export const company = pgTable('company', {
   id: serial('id').primaryKey().notNull(),
-  email: text('official_email').notNull().unique(), //TODO: Use this for stuff like password reset and email validation
+  email: text('email').notNull().unique(), //TODO: Use this for stuff like password reset and email validation
   password: text('password').notNull(),
   sponsorCategory: sponsorCategory('sponsor_category'),
   name: text('name'),
