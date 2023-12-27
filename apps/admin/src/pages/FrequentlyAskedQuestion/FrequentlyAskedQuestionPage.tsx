4f0ba686f194e2947d618a4f1f67a8bf5fbc4f59
ivojovanovic01 <ivo.jovanovic2@gmail.com>
2023-12-27 23:34:39 +0100

    created faq delete modal

--- apps/admin/src/pages/FrequentlyAskedQuestion/FrequentlyAskedQuestionPage.tsx
+++ apps/admin/src/pages/FrequentlyAskedQuestion/FrequentlyAskedQuestionPage.tsx
@@ -1,35 +1,75 @@
+import { FrequentlyAskedQuestion } from '@ddays-app/types/src/model/frequentlyAskedQuestion';
+import { useState } from 'react';
+
 import { useFetchAllFrequentlyAskedQuestions } from '../../api/useFetchFrequentlyAskedQuestions';
+import Button from '../../components/Button';
+import Modal from '../../components/Modal';
 import Table from '../../components/Table';
 
 const headers = ['Id', 'Pitanje', 'Odgovor', 'Akcije'];
 
-const buttonActions = [
-  {
-    label: 'Uredi',
-    action: (row: object) => {
-      console.log('Uredi', row);
-    },
-  },
-  {
-    label: 'Obriši',
-    action: (row: object) => {
-      console.log('Obriši', row);
-    },
-  },
-];
-
 const FrequentlyAskedQuestionPage = () => {
+  const [isAddModalOpen, setIsAddModalOpen] = useState(false);
+  const [isEditModalOpen, setIsEditModalOpen] = useState(false);
+  const [frequentlyAskedQuestionToEdit, setFrequentlyAskedQuestionToEdit] =
+    useState<FrequentlyAskedQuestion | null>(null);
+
+  const [isDeleteModalOpen, setIsDeleteModalOpen] = useState(false);
+  const [
+    frequentlyAskedQuestionToDeleteId,
+    setFrequentlyAskedQuestionToDeleteId,
+  ] = useState<number | null>(null);
+
+  const buttonActions = [
+    {
+      label: 'Uredi',
+      action: (row: FrequentlyAskedQuestion) => {
+        setFrequentlyAskedQuestionToEdit(row);
+        setIsEditModalOpen(!isEditModalOpen);
+      },
+    },
+    {
+      label: 'Obriši',
+      action: (row: FrequentlyAskedQuestion) => {
+        setIsDeleteModalOpen(!isDeleteModalOpen);
+        setFrequentlyAskedQuestionToDeleteId(row.id);
+      },
+    },
+  ];
+
   const { data: frequentlyAskedQuestions, isLoading } =
     useFetchAllFrequentlyAskedQuestions();
+
+  const DeleteFrequentlyAskedQuestionModal = () => {
+    return (
+      <Modal
+        isOpen={isDeleteModalOpen}
+        toggleModal={() => setIsDeleteModalOpen(!isDeleteModalOpen)}>
+        <h2>Obriši faq</h2>
+        <p>ime</p>
+        <p>Jesi li siguran?</p>
+
+        <Button variant='secondary'>Obriši</Button>
+      </Modal>
+    );
+  };
 
   if (isLoading) return <div>Loading...</div>;
 
   return (
-    <Table
-      headers={headers}
-      data={frequentlyAskedQuestions}
-      buttonActions={buttonActions}
-    />
+    <>
+      <Button style={{ marginTop: '10px' }} variant='secondary'>
+        Dodaj novo pitanje
+      </Button>
+
+      <Table
+        headers={headers}
+        data={frequentlyAskedQuestions}
+        buttonActions={buttonActions}
+      />
+
+      <DeleteFrequentlyAskedQuestionModal />
+    </>
   );
 };
 
