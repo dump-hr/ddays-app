2fa1aeda37709f482a5208a29b3d12cd73903aff
Bartol Deak <b@bdeak.net>
2024-01-02 22:14:19 +0100

    add docker build

--- apps/api/src/interests/interests.dto.ts
+++ apps/api/src/interests/interests.dto.ts
@@ -1,8 +1,8 @@
-import { getCreateInterestDto, getUpdateCompanyDto } from '@ddays-app/types';
+import { getCreateInterestDto, getUpdateInterestDto } from '@ddays-app/types';
 import { ApiProperty } from '@nestjs/swagger';
 
 export const _CreateInterestDto = getCreateInterestDto(ApiProperty);
 export class CreateInterestDto extends _CreateInterestDto {}
 
-export const _UpdateInterestDto = getUpdateCompanyDto(ApiProperty);
+export const _UpdateInterestDto = getUpdateInterestDto(ApiProperty);
 export class UpdateInterestDto extends _UpdateInterestDto {}
