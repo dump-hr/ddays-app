80b6a553134dbf913992985bfaa16704852cb636
dujesa <duje@theremnants.app>
2024-01-02 22:29:11 +0100

    Migrations and locks

--- apps/api/db/migrations/0005_first_jean_grey.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE IF NOT EXISTS "notification" (
-	"id" serial PRIMARY KEY NOT NULL,
-	"title" text NOT NULL,
-	"description" text NOT NULL,
-	"is_active" boolean DEFAULT false,
-	"activated_at" timestamp
-);
