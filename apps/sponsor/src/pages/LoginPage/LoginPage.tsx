d31c72a851fec81ae4e546bb7edda105a39142dc
tsiklic1 <100917481+tsiklic1@users.noreply.github.com>
2024-01-05 10:36:35 +0100

    styling login page

--- apps/sponsor/src/pages/LoginPage/LoginPage.tsx
+++ apps/sponsor/src/pages/LoginPage/LoginPage.tsx
@@ -1,9 +1,19 @@
 import LoginForm from '../../components/LoginForm';
+import c from './LoginPage.module.scss';
 
 const LoginPage = () => {
   return (
-    <div>
-      <LoginForm />
+    <div className={c.background}>
+      <div className={c.formWrapper}>
+        <div className={c.backgroundImg}></div>
+        <div className={c.formContainer}>
+          <svg>
+            <use href={`/logo.svg#logo`} />
+          </svg>
+          <h1 className={c.loginTitle}>Prijava u Partners App</h1>
+          <LoginForm />
+        </div>
+      </div>
     </div>
   );
 };
