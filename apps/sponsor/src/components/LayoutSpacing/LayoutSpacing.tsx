7545895a6818d82ea997d96c42fa974a04be353e
Lovre Tomić <92167015+lovretomic@users.noreply.github.com>
2024-01-06 15:18:56 +0100

    added layout spacing

---
+++ apps/sponsor/src/components/LayoutSpacing/LayoutSpacing.tsx
@@ -0,0 +1,9 @@
+type Props = {
+  children?: React.ReactNode;
+};
+
+const LayoutSpacing = ({ children }: Props) => {
+  return <div id='layoutSpacing'>{children}</div>;
+};
+
+export default LayoutSpacing;
