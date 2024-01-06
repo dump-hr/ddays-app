80235246580e00301678de795094dbd725659140
Lovre Tomić <92167015+lovretomic@users.noreply.github.com>
2024-01-06 15:23:59 +0100

    added page and route

--- apps/sponsor/src/constants/paths.ts
+++ apps/sponsor/src/constants/paths.ts
@@ -1,6 +1,7 @@
 export enum Path {
   Home = '/',
   Login = '/login',
+  Profile = '/profile',
 
   CatchAll = '/:path*',
 }
---
+++ apps/sponsor/src/pages/CompanyProfile/CompanyProfile.tsx
@@ -0,0 +1,5 @@
+const CompanyProfile = () => {
+  return <div></div>;
+};
+
+export default CompanyProfile;
