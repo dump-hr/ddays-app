c7cabae3d5aeeb54d3cfca343c3dfacc5f1dfddc
Lovre Tomić <92167015+lovretomic@users.noreply.github.com>
2024-01-07 20:07:42 +0100

    finished job offer section

--- apps/sponsor/src/formSteps/CompanyProfile/CompanyProfile.tsx
+++ apps/sponsor/src/formSteps/CompanyProfile/CompanyProfile.tsx
@@ -1,5 +1,6 @@
 import CircularButton from '../../components/CircularButton';
 import InfoCard from '../../components/InfoCard';
+import JobOffer from '../../components/InfoCard/JobOffer';
 import LayoutSpacing from '../../components/LayoutSpacing';
 import Pill from '../../components/Pill';
 import CoverImage from './assets/cover.png';
@@ -9,14 +10,10 @@ const data = {
   description: '',
   jobOffers: [
     {
-      title: '',
-      description: '',
-      location: '',
-    },
-    {
-      title: '',
-      description: '',
-      location: '',
+      title: 'Java Developer',
+      description:
+        'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+      location: 'Split',
     },
   ],
   interests: {
@@ -52,10 +49,29 @@ const InterestsCardContent = () => {
 };
 
 const JobOffersCardContent = () => {
-  return <></>;
+  return (
+    <>
+      {data.jobOffers.map((jobOffer) => (
+        <JobOffer
+          title={jobOffer.title}
+          description={jobOffer.description}
+          location={jobOffer.location}
+        />
+      ))}
+    </>
+  );
 };
 
 const CompanyProfile = () => {
+  function dataIsEmpty() {
+    return (
+      data.interests.development.length == 0 &&
+      data.interests.design.length == 0 &&
+      data.interests.marketing.length == 0 &&
+      data.interests.tech.length == 0
+    );
+  }
+
   return (
     <>
       <section className={c.headerInfo}>
@@ -69,7 +85,7 @@ const CompanyProfile = () => {
                 <p>profi.co</p>
               </div>
               <CircularButton className={c.submitButton}>
-                Predaj materijale
+                {dataIsEmpty() ? 'Predaj materijale' : 'Uredi materijale'}
               </CircularButton>
             </div>
           </div>
@@ -99,7 +115,7 @@ const CompanyProfile = () => {
                 title='Oglasi za posao'
                 buttonText='Postavite oglase za posao'
                 onClick={() => console.log('Clicked')}>
-                {data.jobOffers.length > 0 ? (
+                {data.jobOffers.length == 0 ? (
                   <p className={c.cardContentParagraph}>
                     Nema postavljenih oglasa
                   </p>
