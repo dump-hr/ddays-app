eab7c2f1fe69ac273c2c98f0145d7110d5168a52
drmodun <jan.modun.st@gmail.com>
2024-01-07 15:40:22 +0100

    added remaining fields and headers

--- apps/admin/src/pages/CompaniesPage/CompaniesPage.tsx
+++ apps/admin/src/pages/CompaniesPage/CompaniesPage.tsx
@@ -1,4 +1,15 @@
-import { QuestionType } from '@ddays-app/types';
+import { QuestionType, SponsorCategory } from '@ddays-app/types';
+
+const headers = [
+  'Ime',
+  'Opis',
+  'Website',
+  'Email',
+  'Lokacija štanda',
+  'Kod',
+  'Kategorija sponzorstva',
+  'Akcije',
+];
 
 const questions = [
   {
@@ -19,8 +30,34 @@ const questions = [
     title: 'Website',
   },
   {
-    
+    id: 'email',
+    type: QuestionType.Field,
+    title: 'Email',
+    rules: { required: 'Obavezno polje' },
+  },
+  {
+    id: 'boothLocation',
+    type: QuestionType.Field,
+    title: 'Lokacija štanda',
+  },
+  {
+    id: 'codeId',
+    type: QuestionType.Field,
+    title: 'Kod', //TODO: ovo ce bit select nakon sto se napravi kodovi endpoint
+  },
+  {
+    id: 'sponsorCategory',
+    type: QuestionType.Select,
+    title: 'Kategorija sponzorstva',
+    options: [
+      SponsorCategory.Bronze,
+      SponsorCategory.Gold,
+      SponsorCategory.Silver,
+      SponsorCategory.FoodAndBeverage, //If more are needed just add them
+    ],
   },
 ];
 
-export const CompaniesPage = () => {};
+export const CompaniesPage = () => {
+  
+};
