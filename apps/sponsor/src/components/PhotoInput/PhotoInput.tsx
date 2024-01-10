7fd4f3f20cdebc9b4d672087731e993640eb99f5
dbaric99 <damjana.baric1999@gmail.com>
2024-01-10 23:13:52 +0100

    Minor improvements

--- apps/sponsor/src/components/PhotoInput/PhotoInput.tsx
+++ apps/sponsor/src/components/PhotoInput/PhotoInput.tsx
@@ -1,20 +1,17 @@
+import { useEffect, useState } from 'react';
 import { useDropzone } from 'react-dropzone';
-import { useState, useEffect } from 'react';
 
-import styles from './PhotoInput.module.scss';
 import sprite from '../../../public/sprite.svg';
+import { Message } from '../../constants/messages';
 import { photoHelper } from '../../helpers/photoHelper';
 import { ErrorMessage } from '.';
+import styles from './PhotoInput.module.scss';
 
 type PhotoInputProps = {
-  crop?: boolean;
   label?: string;
   displayErrorMessages?: boolean;
   inputConstraints?: {
-    imageType?: string;
-    aspectRatio?: number;
     mimeTypes?: string[];
-    maxFileSize?: number;
     maxDimensions?: {
       width: number;
       height: number;
@@ -25,51 +22,55 @@ type PhotoInputProps = {
 };
 
 interface FileWithPreview extends File {
-    preview?: string;
+  preview?: string;
 }
 
 const PhotoInput: React.FC<PhotoInputProps> = ({
   label,
   displayErrorMessages = false,
   inputConstraints,
-  height=362,
+  height = 362,
 }) => {
-  const errorMessages = {
-    blackAndWhite: 'Logo mora biti crno bijeli',
-    wrongDimensions: 'Fotografija mora imati dimenzije manje od {width}x{height}',
-  }
-
   const [files, setFiles] = useState<FileWithPreview[]>([]);
   const [isBlackAndWhite, setIsBlackAndWhite] = useState<boolean | null>(null);
-  const [isWithinDimensions, setIsWithinDimensions] = useState<boolean | null>(null);
+  const [isWithinDimensions, setIsWithinDimensions] = useState<boolean | null>(
+    null,
+  );
 
-  const { acceptedFiles, getRootProps, getInputProps } =
-    useDropzone({
-      accept: inputConstraints?.mimeTypes?.reduce((acc, type) => ({ ...acc, [`image/${type}`]: [] }), {}) || { 'image/*': [] },
-      onDrop: async (acceptedFiles) => {
-        if (inputConstraints?.checkBlackAndWhite) {
-            let blackAndWhitePromises = acceptedFiles.map(photoHelper.checkBlackAndWhite);
-            const results = await Promise.all(blackAndWhitePromises);
-            setIsBlackAndWhite(results.every((result) => result));
-        }
+  const { acceptedFiles, getRootProps, getInputProps } = useDropzone({
+    accept: inputConstraints?.mimeTypes?.reduce(
+      (acc, type) => ({ ...acc, [`image/${type}`]: [] }),
+      {},
+    ) || { 'image/*': [] },
+    onDrop: async (acceptedFiles) => {
+      if (inputConstraints?.checkBlackAndWhite) {
+        const blackAndWhitePromises = acceptedFiles.map(
+          photoHelper.checkBlackAndWhite,
+        );
+        const results = await Promise.all(blackAndWhitePromises);
+        setIsBlackAndWhite(results.every((result) => result));
+      }
 
-        if(inputConstraints?.maxDimensions) {
-            const dimensionsPromises = acceptedFiles.map((file) =>
-            photoHelper.checkImageDimensions(file, inputConstraints.maxDimensions || { width: 0, height: 0 })
-          );
-          const dimensionsResults = await Promise.all(dimensionsPromises);
-          setIsWithinDimensions(dimensionsResults.every((result) => result));
-        }
-
-        setFiles(
-          acceptedFiles.map((file) =>
-            Object.assign(file, {
-              preview: URL.createObjectURL(file),
-            }),
+      if (inputConstraints?.maxDimensions) {
+        const dimensionsPromises = acceptedFiles.map((file) =>
+          photoHelper.checkImageDimensions(
+            file,
+            inputConstraints.maxDimensions || { width: 0, height: 0 },
           ),
         );
-      },
-    });
+        const dimensionsResults = await Promise.all(dimensionsPromises);
+        setIsWithinDimensions(dimensionsResults.every((result) => result));
+      }
+
+      setFiles(
+        acceptedFiles.map((file) =>
+          Object.assign(file, {
+            preview: URL.createObjectURL(file),
+          }),
+        ),
+      );
+    },
+  });
 
   const thumbs = files.map((file) => (
     <div className={styles.thumb}>
@@ -79,26 +80,29 @@ const PhotoInput: React.FC<PhotoInputProps> = ({
           className={styles.image}
           onLoad={() => {
             if (file.preview) {
-                URL.revokeObjectURL(file?.preview);
+              URL.revokeObjectURL(file?.preview);
             }
           }}
-          style={{maxHeight: `${height}px`}}
+          style={{ maxHeight: `${height}px` }}
         />
       </div>
     </div>
   ));
 
   useEffect(() => {
-    return () => files.forEach((file) => {
-        if(file.preview) {
-            URL.revokeObjectURL(file.preview);
+    return () =>
+      files.forEach((file) => {
+        if (file.preview) {
+          URL.revokeObjectURL(file.preview);
         }
-    });
+      });
   }, []);
 
   return (
-    <div className={styles.inputArea} style={{height: `${height}px`}}>
-      <div className={styles.inputAreaContainer} style={{height: `${height}px`}}>
+    <div className={styles.inputArea} style={{ height: `${height}px` }}>
+      <div
+        className={styles.inputAreaContainer}
+        style={{ height: `${height}px` }}>
         <label className={styles.inputAreaLabel}>
           {!acceptedFiles.length && (
             <div {...getRootProps()}>
@@ -111,26 +115,32 @@ const PhotoInput: React.FC<PhotoInputProps> = ({
               </div>
             </div>
           )}
-          <aside className={styles.thumbsContainer}>
-            {thumbs}
-          </aside>
+          <aside className={styles.thumbsContainer}>{thumbs}</aside>
         </label>
       </div>
       <div className={styles.errorContainer}>
         {displayErrorMessages && (
           <>
-            {isBlackAndWhite === false && inputConstraints?.checkBlackAndWhite && (
-              <ErrorMessage display={true} message={errorMessages.blackAndWhite} />
-            )}
-            {isWithinDimensions === false && inputConstraints?.maxDimensions && (
-              <ErrorMessage
-                display={true}
-                message={errorMessages.wrongDimensions.replace(
-                  '{width}',
-                  `${inputConstraints.maxDimensions.width}`,
-                ).replace('{height}', `${inputConstraints.maxDimensions.height}`)}
-              />
-            )}
+            {isBlackAndWhite === false &&
+              inputConstraints?.checkBlackAndWhite && (
+                <ErrorMessage
+                  display={true}
+                  message={Message.BlackAndWhiteError}
+                />
+              )}
+            {isWithinDimensions === false &&
+              inputConstraints?.maxDimensions && (
+                <ErrorMessage
+                  display={true}
+                  message={Message.DimensionsError.replace(
+                    '{width}',
+                    `${inputConstraints.maxDimensions.width}`,
+                  ).replace(
+                    '{height}',
+                    `${inputConstraints.maxDimensions.height}`,
+                  )}
+                />
+              )}
           </>
         )}
       </div>
