1e1a27f4c903f0988b1e5c3b6b053fa87810ce53
Bartol Deak <b@bdeak.net>
2024-01-13 19:18:58 +0100

    undo migrations

--- apps/api/db/migrations/0009_volatile_veda.sql
+++
@@ -1,5 +0,0 @@
-DO $$ BEGIN
- ALTER TABLE "event" ADD CONSTRAINT "event_code_id_code_id_fk" FOREIGN KEY ("code_id") REFERENCES "code"("id") ON DELETE no action ON UPDATE no action;
-EXCEPTION
- WHEN duplicate_object THEN null;
-END $$;
