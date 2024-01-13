fcfdd2613d6eef44dbcc66a0ef92d76fe40aaab6
drmodun <jan.modun.st@gmail.com>
2024-01-13 20:32:33 +0100

    changing things to remove conflicts

--- apps/api/db/migrations/0001_chilly_micromax.sql
+++
@@ -1,7 +0,0 @@
-ALTER TABLE "companyInterests" DROP CONSTRAINT "companyInterests_companyId_company_id_fk";
---> statement-breakpoint
-DO $$ BEGIN
- ALTER TABLE "companyInterests" ADD CONSTRAINT "companyInterests_companyId_company_id_fk" FOREIGN KEY ("companyId") REFERENCES "company"("id") ON DELETE cascade ON UPDATE no action;
-EXCEPTION
- WHEN duplicate_object THEN null;
-END $$;
