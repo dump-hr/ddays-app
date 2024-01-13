c50d8fdde8f56b6f76df6f77fbe7662acb0cb401
drmodun <jan.modun.st@gmail.com>
2024-01-13 15:39:59 +0100

    fixing migrations

--- apps/api/db/migrations/0001_chilly_micromax.sql
+++ apps/api/db/migrations/0001_chilly_micromax.sql
@@ -5,3 +5,6 @@ DO $$ BEGIN
 EXCEPTION
  WHEN duplicate_object THEN null;
 END $$;
+
+ALTER TABLE "company" DROP CONSTRAINT "company_email_unique";--> statement-breakpoint
+ALTER TABLE "company" DROP COLUMN IF EXISTS "email";
--- apps/api/db/migrations/0001_lying_jocasta.sql
+++
@@ -1,2 +0,0 @@
-ALTER TABLE "company" DROP CONSTRAINT "company_email_unique";--> statement-breakpoint
-ALTER TABLE "company" DROP COLUMN IF EXISTS "email";
