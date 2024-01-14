4aed668d1846e39d5805e928f75853971701f012
Bartol Deak <b@bdeak.net>
2024-01-14 02:23:18 +0100

    autorun migrations on app startup

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -1,16 +1,23 @@
-import { ClassSerializerInterceptor, ValidationPipe } from '@nestjs/common';
+import {
+  ClassSerializerInterceptor,
+  INestApplication,
+  ValidationPipe,
+} from '@nestjs/common';
 import { NestFactory, Reflector } from '@nestjs/core';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import * as migrator from 'drizzle-orm/node-postgres/migrator';
+import { drizzle } from 'drizzle-orm/postgres-js';
 import { createProxyMiddleware } from 'http-proxy-middleware';
+import postgres from 'postgres';
 
 import { AppModule } from './app.module';
 
-async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-
+const setupClassValidator = (app: INestApplication) => {
   app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
   app.useGlobalInterceptors(new ClassSerializerInterceptor(app.get(Reflector)));
+};
 
+const setupSwagger = (app: INestApplication) => {
   const config = new DocumentBuilder()
     .setTitle('DDays API')
     .setDescription('Backend for DDays application')
@@ -19,23 +26,53 @@ async function bootstrap() {
     .build();
 
   const document = SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('api/swagger', app, document);
+  SwaggerModule.setup('/api/swagger', app, document);
+};
 
+const setupFrontendDevServerProxies = (app: INestApplication) => {
+  if (process.env.NODE_ENV !== 'dev') return;
+
+  app.use(
+    '/admin',
+    createProxyMiddleware({
+      target: 'http://localhost:3002',
+    }),
+  );
+
+  app.use(
+    '/sponsor',
+    createProxyMiddleware({
+      target: 'http://localhost:3003',
+    }),
+  );
+};
+
+const migrate = async () => {
+  const sql = postgres(process.env.DATABASE_URL, {
+    max: 1,
+  });
+
+  await migrator.migrate(drizzle(sql), { migrationsFolder: './db/migrations' });
+};
+
+const run = async (app: INestApplication) => {
   app.setGlobalPrefix('api');
 
-  if (process.env.NODE_ENV === 'dev') {
-    app.use(
-      '/admin',
-      createProxyMiddleware({ target: 'http://localhost:3002' }),
-    );
-    app.use(
-      '/sponsor',
-      createProxyMiddleware({ target: 'http://localhost:3003' }),
-    );
-  }
-
   const port = process.env.PORT || 3000;
+
   await app.listen(port);
+
   console.log(`Application is running on: http://localhost:${port}`);
+};
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+
+  setupClassValidator(app);
+  setupSwagger(app);
+  setupFrontendDevServerProxies(app);
+
+  await migrate();
+  await run(app);
 }
 bootstrap();
