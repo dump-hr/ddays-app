0a036c35826d54a6abe9baa92bc28045ac211c66
tsiklic1 <100917481+tsiklic1@users.noreply.github.com>
2024-01-28 15:44:59 +0100

    renaming postgres-error file

--- apps/api/src/postgres-error.filter.ts
+++ apps/api/src/postgres-error.filter.ts
@@ -1,5 +1,10 @@
-import { ArgumentsHost, Catch, ExceptionFilter } from '@nestjs/common';
-import { Request, Response } from 'express';
+import {
+  ArgumentsHost,
+  Catch,
+  ExceptionFilter,
+  HttpStatus,
+} from '@nestjs/common';
+import { Response } from 'express';
 import { PostgresError } from 'postgres';
 
 @Catch(PostgresError)
@@ -7,14 +12,11 @@ export class PostgresErrorFilter implements ExceptionFilter {
   catch(exception: PostgresError, host: ArgumentsHost) {
     const ctx = host.switchToHttp();
     const response = ctx.getResponse<Response>();
-    const request = ctx.getRequest<Request>();
     const message = exception.detail;
 
-    response.status(500).json({
-      statusCode: 500,
+    response.status(HttpStatus.BAD_REQUEST).json({
+      statusCode: HttpStatus.BAD_REQUEST,
       message: message,
-      timeStamp: new Date().toISOString(),
-      path: request.url,
     });
   }
 }
