5103bc13202e32ceccec19c27bb0ac28e0fdbeda
Bartol Deak <b@bdeak.net>
2024-02-15 00:35:16 +0100

    fixes

--- apps/admin/src/api/interest/useInterestGetAll.ts
+++ apps/admin/src/api/interest/useInterestGetAll.ts
@@ -1,14 +1,12 @@
-import { InterestDto as InterestPublicDto } from '@ddays-app/types';
+import { InterestDto } from '@ddays-app/types';
 import { QueryOptions, useQuery } from 'react-query';
 
 import { api } from '..';
 
 const interestGetAll = async () => {
-  return await api.get<never, InterestPublicDto[]>('/interest');
+  return await api.get<never, InterestDto[]>('/interest');
 };
 
-export const useInterestGetAll = (
-  options?: QueryOptions<InterestPublicDto[]>,
-) => {
+export const useInterestGetAll = (options?: QueryOptions<InterestDto[]>) => {
   return useQuery(['interest'], interestGetAll, options);
 };
