9a86f30cac269cee080c0d631f708b69f9678fce
tsiklic1 <100917481+tsiklic1@users.noreply.github.com>
2024-02-19 20:27:48 +0100

    adding useCompanyUpdateBookOfStandards.ts

---
+++ apps/sponsor/src/api/company/useCompanyUpdateBookOfStandards.ts
@@ -0,0 +1,25 @@
+import toast from 'react-hot-toast';
+import { useMutation, useQueryClient } from 'react-query';
+
+import { api } from '..';
+
+const companyUpdateBookOfStandards = async (file: File) => {
+  const data = new FormData();
+  data.append('file', file);
+
+  return await api.patchForm('company/book-of-standards', data);
+};
+
+export const useCompanyUpdateBookOfStandards = () => {
+  const queryClient = useQueryClient();
+
+  return useMutation(companyUpdateBookOfStandards, {
+    onSuccess: () => {
+      queryClient.invalidateQueries(['company', 'current']);
+      toast.success('Knjiga standarda uspješno uploadana');
+    },
+    onError: (error: string) => {
+      toast.error(error);
+    },
+  });
+};
