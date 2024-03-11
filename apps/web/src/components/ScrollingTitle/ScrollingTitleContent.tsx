d1fb72496b2d528e1ded722f95abe7d118813182
Lovre Tomić <tomiclovre05@gmail.com>
2024-03-11 10:18:22 +0100

    added people images

--- apps/web/src/components/ScrollingTitle/ScrollingTitleContent.tsx
+++ apps/web/src/components/ScrollingTitle/ScrollingTitleContent.tsx
@@ -1,9 +1,21 @@
 import DaysSticker from '../../assets/images/days-sticker.png';
+import Digitalac from '../../assets/images/digitalac.png';
+import Dizajner from '../../assets/images/dizajner.png';
+import Marketingas from '../../assets/images/marketingas.png';
+import Poduzetnik from '../../assets/images/poduzetnik.png';
+import Programer from '../../assets/images/programer.png';
 import c from './ScrollingTitle.module.scss';
 
 const Content = () => {
   return (
     <div className={c.content}>
+      <div className={c.people}>
+        <img src={Digitalac} alt='' />
+        <img src={Marketingas} alt='' />
+        <img src={Programer} alt='' />
+        <img src={Dizajner} alt='' />
+        <img src={Poduzetnik} alt='' />
+      </div>
       <h1>Digitalaca</h1>
       <img src={DaysSticker} alt='' />
       <h1>Marketingaša</h1>
