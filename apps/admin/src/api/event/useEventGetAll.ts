4bbd9aee2701707c8cffa8149f43372da87ff0bd
tsiklic1 <100917481+tsiklic1@users.noreply.github.com>
2024-03-11 16:50:33 +0100

    adidng useEventGetAll

---
+++ apps/admin/src/api/event/useEventGetAll.ts
@@ -0,0 +1,12 @@
+import { EventDto } from '@ddays-app/types';
+import { QueryOptions, useQuery } from 'react-query';
+
+import { api } from '..';
+
+const eventGetAll = () => {
+  return api.get<never, EventDto[]>('event');
+};
+
+export const useEventGetAll = (options?: QueryOptions<EventDto[]>) => {
+  return useQuery(['event'], eventGetAll, options);
+};
--- apps/admin/src/pages/EventPage.tsx
+++ apps/admin/src/pages/EventPage.tsx
@@ -1,4 +1,12 @@
+import { useEffect, useState } from 'react';
+
+import { useEventCreate } from '../api/event/useEventCreate';
+
 const EventPage = () => {
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const [eventToEditId, setEventToEditId] = useState<number>();
+
+  useEffect(() => {}, []);
   return <></>;
 };
 
