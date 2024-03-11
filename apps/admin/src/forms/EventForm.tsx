d93c80274f0f64ba5fd0b2d4f7c6885385b0da10
tsiklic1 <100917481+tsiklic1@users.noreply.github.com>
2024-03-11 17:27:40 +0100

    adding event table to event page

---
+++ apps/admin/src/forms/EventForm.tsx
@@ -0,0 +1,8 @@
+type EventFormProps = {
+  id?: number;
+  onSuccess: () => void;
+};
+
+export const EventForm: React.FC<EventFormProps> = ({ id, onSuccess }) => {
+  return <></>;
+};
--- apps/admin/src/pages/EventPage.tsx
+++ apps/admin/src/pages/EventPage.tsx
@@ -1,13 +1,64 @@
-import { useEffect, useState } from 'react';
+import { useState } from 'react';
 
-import { useEventCreate } from '../api/event/useEventCreate';
+import { useEventGetAll } from '../api/event/useEventGetAll';
+import { Button } from '../components/Button';
+import { Modal } from '../components/Modal';
+import { Table } from '../components/Table';
+import { EventForm } from '../forms/EventForm';
 
 const EventPage = () => {
   const [isModalOpen, setIsModalOpen] = useState(false);
   const [eventToEditId, setEventToEditId] = useState<number>();
 
-  useEffect(() => {}, []);
-  return <></>;
+  const events = useEventGetAll();
+
+  if (events.isLoading) {
+    return <div>Loading...</div>;
+  }
+
+  return (
+    <>
+      <Modal
+        isOpen={isModalOpen}
+        onClose={() => {
+          setIsModalOpen(false);
+          setEventToEditId(undefined);
+        }}>
+        <EventForm
+          id={eventToEditId}
+          onSuccess={() => {
+            setIsModalOpen(false);
+            setEventToEditId(undefined);
+          }}
+        />
+      </Modal>
+
+      <Button variant='primary' onClick={() => setIsModalOpen(true)}>
+        New
+      </Button>
+
+      <Table
+        data={events.data}
+        actions={[
+          {
+            label: 'Ured',
+            action: (event) => {
+              setEventToEditId(event.id);
+              setIsModalOpen(true);
+            },
+          },
+          {
+            label: 'Obriši',
+            action: () => {
+              if (confirm('Jesi li siguran?')) {
+                return;
+              }
+            },
+          },
+        ]}
+      />
+    </>
+  );
 };
 
 export default EventPage;
