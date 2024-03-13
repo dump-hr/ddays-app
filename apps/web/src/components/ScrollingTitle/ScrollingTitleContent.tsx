df94f7f4efaa179bb10bbfcd60f9854f24f22952
Lovre Tomić <tomiclovre05@gmail.com>
2024-03-13 12:51:45 +0100

    improved scrolling responsivity

--- apps/web/src/components/ScrollingTitle/ScrollingTitleContent.tsx
+++ apps/web/src/components/ScrollingTitle/ScrollingTitleContent.tsx
@@ -1,31 +1,21 @@
 import DaysSticker from '../../assets/images/days-sticker.png';
-import Digitalac from '../../assets/images/digitalac.png';
-import Dizajner from '../../assets/images/dizajner.png';
-import Marketingas from '../../assets/images/marketingas.png';
-import Poduzetnik from '../../assets/images/poduzetnik.png';
-import Programer from '../../assets/images/programer.png';
+import People from '../../assets/images/people.png';
 import c from './ScrollingTitle.module.scss';
 
 const Content = () => {
   return (
     <div className={c.content}>
-      <div className={c.people}>
-        <img src={Digitalac} alt='' />
-        <img src={Marketingas} alt='' />
-        <img src={Programer} alt='' />
-        <img src={Dizajner} alt='' />
-        <img src={Poduzetnik} alt='' />
-      </div>
-      <h1>Digitalaca</h1>
-      <img src={DaysSticker} alt='' />
-      <h1>Marketingaša</h1>
-      <img src={DaysSticker} alt='' />
-      <h1>Programera</h1>
-      <img src={DaysSticker} alt='' />
-      <h1>Dizajnera</h1>
-      <img src={DaysSticker} alt='' />
-      <h1>Poduzetnika</h1>
-      <img src={DaysSticker} alt='' />
+      <img className={c.people} src={People} alt='' />
+      <h1 className={c.title}>Digitalaca</h1>
+      <img className={c.sticker} src={DaysSticker} alt='' />
+      <h1 className={c.title}>Marketingaša</h1>
+      <img className={c.sticker} src={DaysSticker} alt='' />
+      <h1 className={c.title}>Programera</h1>
+      <img className={c.sticker} src={DaysSticker} alt='' />
+      <h1 className={c.title}>Dizajnera</h1>
+      <img className={c.sticker} src={DaysSticker} alt='' />
+      <h1 className={c.title}>Poduzetnika</h1>
+      <img className={c.sticker} src={DaysSticker} alt='' />
     </div>
   );
 };
