f65addf2a33bee8a5e72cc4149086bc85736d3fc
Lovre Tomić <tomiclovre05@gmail.com>
2024-03-14 20:56:16 +0100

    added header component

---
+++ apps/web/src/components/Header/Header.tsx
@@ -0,0 +1,18 @@
+import c from './Header.module.scss';
+
+const Header = () => {
+  return (
+    <header className={c.header}>
+      <p className={c.text}>
+        SPLIT <span className={c.separator}>//</span> FESB <br /> 23.{' '}
+        <span className={c.separator}>—</span> 24. 05. 2024.
+      </p>
+      <p className={c.text}>
+        BESPLATNA KONFERENCIJA <br />
+        ZA NOVU GENERACIJU
+      </p>
+    </header>
+  );
+};
+
+export default Header;
--- apps/web/src/pages/LandingPage/LandingPage.tsx
+++ apps/web/src/pages/LandingPage/LandingPage.tsx
@@ -1,9 +1,11 @@
+import Header from '../../components/Header';
 import Hero from '../../components/Hero';
 import Schedule from '../../components/Schedule';
 
 export const LandingPage: React.FC = () => {
   return (
     <>
+      <Header />
       <Hero />
       <Schedule />
     </>
