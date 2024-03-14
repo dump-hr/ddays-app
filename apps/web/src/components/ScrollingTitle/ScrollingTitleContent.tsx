e97c0b423567a7d9111438505510ff1224002bd8
Lovre Tomić <tomiclovre05@gmail.com>
2024-03-14 20:41:03 +0100

    fixed content ratios

--- apps/web/src/components/ScrollingTitle/ScrollingTitleContent.tsx
+++ apps/web/src/components/ScrollingTitle/ScrollingTitleContent.tsx
@@ -1,11 +1,22 @@
 import DaysSticker from '../../assets/images/days-sticker.png';
-import People from '../../assets/images/people.png';
+import Digitalac from '../../assets/images/digitalac.png';
+import Dizajner from '../../assets/images/dizajner.png';
+import Marketingas from '../../assets/images/marketingas.png';
+import Poduzetnik from '../../assets/images/poduzetnik.png';
+import Programer from '../../assets/images/programer.png';
 import c from './ScrollingTitle.module.scss';
 
 const Content = () => {
   return (
     <div className={c.content}>
-      <img className={c.people} src={People} alt='' />
+      <div className={c.people}>
+        <img className={c.person} src={Digitalac} alt='' />
+        <img className={c.person} src={Marketingas} alt='' />
+        <img className={c.person} src={Programer} alt='' />
+        <img className={c.person} src={Dizajner} alt='' />
+        <img className={c.person} src={Poduzetnik} alt='' />
+      </div>
+
       <div className={c.stickerTitle}>
         <h1 className={c.title}>Digitalaca</h1>
         <img className={c.sticker} src={DaysSticker} alt='' />
