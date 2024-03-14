6d9dd1e3cb08d8d0adacec4e0b1937b134ca3de8
drmodun <jan.modun.st@gmail.com>
2024-03-14 12:41:57 +0100

    finished desktop version

--- apps/web/src/pages/LandingPage/LandingPage.tsx
+++ apps/web/src/pages/LandingPage/LandingPage.tsx
@@ -1,12 +1,14 @@
+import grain from 'assets/images/grain.png';
 import DuckieSection from 'components/DuckieSection';
 
 import Schedule from '../../components/Schedule';
+import classes from './LandingPage.module.scss';
 
 export const LandingPage: React.FC = () => {
   return (
-    <>
+    <div className={classes.container}>
       <Schedule />
       <DuckieSection />
-    </>
+    </div>
   );
 };
