76d0a47b17022f099a283167358ebc5d24362052
Lovre Tomić <tomiclovre05@gmail.com>
2024-03-16 20:19:29 +0100

    added bottom navigation menu

--- apps/web/src/components/Hero/Hero.tsx
+++ apps/web/src/components/Hero/Hero.tsx
@@ -7,6 +7,13 @@ const Hero = () => {
       <div className={c.content}>
         <ScrollingTitle />
       </div>
+      <nav className={c.menu}>
+        <a href=''>Konferencija</a>
+        <a href=''>Speakeri</a>
+        <a href=''>Raspored</a>
+        <a href=''>Sponzori</a>
+        <a href=''>Kontakt</a>
+      </nav>
     </section>
   );
 };
