e4dbbb299ee948abd4aab31ca5b67d7f9915ad19
Lovre Tomić <tomiclovre05@gmail.com>
2024-03-16 20:03:30 +0100

    added header blending logic

--- apps/web/src/components/Header/Header.tsx
+++ apps/web/src/components/Header/Header.tsx
@@ -1,10 +1,32 @@
+import clsx from 'clsx';
+import { useEffect, useState } from 'react';
+
 import Button from '../Button';
 import HamburgerButton from '../HamburgerButton';
 import c from './Header.module.scss';
 
 const Header = () => {
+  const [scrollY, setScrollY] = useState(0);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      setScrollY(window.scrollY);
+    };
+
+    window.addEventListener('scroll', handleScroll);
+
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+  }, []);
+
+  const classes = clsx({
+    [c.header]: true,
+    [c.blend]: scrollY > 0.85 * window.innerHeight,
+  });
+
   return (
-    <header className={c.header}>
+    <header className={classes}>
       <p className={c.text}>
         SPLIT <span className={c.separator}>//</span> FESB <br /> 23.{' '}
         <span className={c.separator}>—</span> 24. 05. 2024.
