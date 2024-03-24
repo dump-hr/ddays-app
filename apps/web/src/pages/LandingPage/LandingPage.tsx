4b77c5d3b900ce3295708b3e3094fe81b83bb6aa
Bartol Deak <b@bdeak.net>
2024-03-24 16:51:22 +0100

    move grain to component

--- apps/web/src/pages/LandingPage/LandingPage.tsx
+++ apps/web/src/pages/LandingPage/LandingPage.tsx
@@ -1,13 +1,12 @@
 import DuckieSection from 'components/DuckieSection';
 
 import Schedule from '../../components/Schedule';
-import classes from './LandingPage.module.scss';
 
 export const LandingPage: React.FC = () => {
   return (
-    <div className={classes.container}>
+    <>
       <Schedule />
       <DuckieSection />
-    </div>
+    </>
   );
 };
