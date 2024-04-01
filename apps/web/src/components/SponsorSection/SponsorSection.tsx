a9d7f68491937b42470f3c6782a227225d09d187
drmodun <jan.modun.st@gmail.com>
2024-04-02 00:22:27 +0200

    made main component structure

--- apps/web/src/components/SponsorSection/SponsorSection.tsx
+++ apps/web/src/components/SponsorSection/SponsorSection.tsx
@@ -1,11 +1,15 @@
-import placeholder from 'assets/images/placeholder.png';
+import bronzeSponsor from 'assets/images/bronze-sponsor.svg';
+import goldSponsor from 'assets/images/gold-sponsor.svg';
+import goldenPlacehoder from 'assets/images/golden-placeholder.svg';
+import placeholder from 'assets/images/placeholder.svg';
+import silverSponsor from 'assets/images/silver-sponsor.svg';
 import clsx from 'clsx';
 
 import classes from './SponsorSection.module.css';
 
 export const SponsorSection: React.FC = () => (
-  <div className={classes.container}>
-    <div className={classes.topSection}>
+  <section className={classes.container}>
+    <figure className={classes.topSection}>
       <div className={classes.sectionBreaker}></div>
       <span className={classes.idea}>Vjerujemo u jaka prijateljstva.</span>
       <span className={classes.title}>
@@ -58,8 +62,58 @@ export const SponsorSection: React.FC = () => (
         alt='agilno-placeholder'
       />
       <div className={classes.sectionBreaker}></div>
-    </div>
-  </div>
+    </figure>
+    <article className={classes.sponsorSection}>
+      <img src={goldSponsor} alt='zlatni sponzori' />
+      <span className={classes.sponsorTier}>Zlatni sponzori</span>
+      <section className={classes.goldenRow}>
+        <figure className={classes.image}>
+          <img src={goldenPlacehoder} alt='zlatni sponzor' />
+          <div className={classes.openPositions}>
+            <span className={classes.label}>Open positions</span>
+            <span className={classes.number}>9</span>
+          </div>
+        </figure>
+      </section>
+    </article>
+    <article className={classes.sponsorSection}>
+      <img src={silverSponsor} alt='srebrni sponzori' />
+      <span className={classes.sponsorTier}>Srebrni sponzori</span>
+      <section className={classes.logos}>
+        <figure className={classes.logo}>
+          <img src={placeholder} alt='agilno' />
+        </figure>
+      </section>
+    </article>
+    <article className={classes.sponsorSection}>
+      <img src={silverSponsor} alt='brončani sponzori' />
+      <span className={classes.sponsorTier}>Brončani sponzori</span>
+      <section className={classes.logos}>
+        <figure className={classes.logo}>
+          <img src={placeholder} alt='agilno' />
+        </figure>
+      </section>
+    </article>
+    <article className={classes.sponsorSection}>
+      <span className={classes.sponsorTier}>Medijski pokrovitelji</span>
+      <section className={classes.logos}>
+        <figure className={classes.logo}>
+          <img src={placeholder} alt='agilno' />
+        </figure>
+      </section>
+    </article>
+    <article className={classes.sponsorSection}>
+      <span className={classes.sponsorTier}>
+        Organizacijski partneri & prijatelji
+      </span>
+      <section className={classes.logos}>
+        <figure className={classes.logo}>
+          <img src={placeholder} alt='agilno' />
+        </figure>
+      </section>
+    </article>
+    <div className={classes.sectionBreaker}></div>
+  </section>
 );
 
 //TODO: the upper section has to be scroll controlled along with some other pretty complex stuff, so in my opinion it is not critical to do that now and we shoul focus on making it dynamic later
