f94cfe295c4a919cfc60ce08e893c37150c2cb66
tsiklic1 <100917481+tsiklic1@users.noreply.github.com>
2024-04-05 22:08:20 +0200

    modal scroll behavior fix

--- apps/web/src/components/SpeakersSection/SpeakerModal.tsx
+++ apps/web/src/components/SpeakersSection/SpeakerModal.tsx
@@ -17,6 +17,22 @@ const SpeakerModal: React.FC<SpeakerModalProps> = ({
   speaker,
 }) => {
   useEffect(() => {
+    if (isOpen) {
+      const width = document.body.clientWidth;
+      document.body.style.overflow = 'hidden';
+      document.body.style.width = `${width}px`;
+    } else {
+      document.body.style.overflow = 'visible';
+      document.body.style.width = `auto`;
+    }
+
+    return () => {
+      document.body.style.overflow = 'visible';
+      document.body.style.width = `auto`;
+    };
+  }, [isOpen]);
+
+  useEffect(() => {
     const handleEsc = (e: KeyboardEvent) => {
       if (e.key === 'Escape') {
         close();
@@ -37,12 +53,15 @@ const SpeakerModal: React.FC<SpeakerModalProps> = ({
   return (
     <div className={c.modalBackground} onClick={close}>
       <div className={c.modal}>
-        <div className={c.container} onClick={(e) => e.stopPropagation()}>
+        <div
+          data-lenis-prevent
+          className={c.container}
+          onClick={(e) => e.stopPropagation()}>
           <img src={CloseSvg} alt='Close' className={c.close} onClick={close} />
           <div className={c.modalImage}>
             <FilmFrame imageSrc={speaker.photo} width={320} height={400} />
           </div>
-          <div className={c.modalLeft}>
+          <div className={c.modalRight}>
             <h2 className={c.modalSpeakerName}>
               {speaker.firstName} {speaker.lastName}
             </h2>
@@ -51,10 +70,7 @@ const SpeakerModal: React.FC<SpeakerModalProps> = ({
               {speaker.company?.name ? '@ ' + speaker.company?.name : ''}
             </h3>
             <section className={c.modalDescription}>
-              description triba dodat polje... Lorem ipsum dolor sit amet
-              consectetur adipisicing elit. Nostrum mollitia nisi debitis ad
-              inventore, in iste placeat temporibus id qui, deserunt a aliquam
-              minima atque, molestias cumque explicabo? Atque, fugiat?
+              {speaker.description}
             </section>
           </div>
         </div>
