1db37629d208ac0f016931593cf2f45b19fe9c79
tsiklic1 <100917481+tsiklic1@users.noreply.github.com>
2024-04-10 12:42:03 +0200

    adding footer duckie image

--- apps/web/src/components/FooterSection/FooterSection.tsx
+++ apps/web/src/components/FooterSection/FooterSection.tsx
@@ -1,5 +1,21 @@
+import c from './FooterSection.module.scss';
+
 const FooterSection = () => {
-  return <></>;
+  return (
+    <footer className={c.footer}>
+      <div className={c.background}>
+        <div className={c.footerTitleWrapper}>
+          <h2 className={c.bigText}>TL;DR</h2>
+          <h2 className={c.bigText}>DOĐI NA DAYSE</h2>
+          <button className={c.button}>
+            {`[ `}
+            REGISTRIRAJ SVOJ DOLAZAK
+            {` ]`}
+          </button>
+        </div>
+      </div>
+    </footer>
+  );
 };
 
 export default FooterSection;
