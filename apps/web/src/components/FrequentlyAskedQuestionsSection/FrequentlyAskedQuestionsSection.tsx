d5d84111fce3a72ad89d440f68fb4a9e624b1868
ivojovanovic01 <45316330+ivojovanovic01@users.noreply.github.com>
2024-04-15 10:27:24 +0200

    added useFrequentlyAskedQuestionGetAll

--- apps/web/src/components/FrequentlyAskedQuestionsSection/FrequentlyAskedQuestionsSection.tsx
+++ apps/web/src/components/FrequentlyAskedQuestionsSection/FrequentlyAskedQuestionsSection.tsx
@@ -1,5 +1,16 @@
+import { useFrequentlyAskedQuestionGetAll } from '../../api/frequently-asked-question/useFrequentlyAskedQuestionGetAll';
+
 const FrequentlyAskedQuestionsSection = () => {
-  return <div>FrequentlyAskedQuestionsSection</div>;
+  const { data: frequentlyAskedQuestions, isLoading } =
+    useFrequentlyAskedQuestionGetAll();
+
+  if (isLoading) <div>Loading...</div>;
+
+  return (
+    <div>
+      {frequentlyAskedQuestions?.map((faq) => <div>{faq.question}</div>)}
+    </div>
+  );
 };
 
 export default FrequentlyAskedQuestionsSection;
