6e9b7a0c93b7852125f206e4150a29087b6e17dc
Lovre Tomić <tomiclovre05@gmail.com>
2024-04-17 11:59:19 +0200

    added content and styles

--- apps/web/src/components/GallerySection/GallerySection.tsx
+++ apps/web/src/components/GallerySection/GallerySection.tsx
@@ -1,8 +1,14 @@
 import c from './GallerySection.module.scss';
-import GalleryBackground from '../../assets/images/gallery-background.png';
+import ArrowTopRight from '../../assets/icons/arrow-top-right.svg';
 
 const GallerySection = () => {
-  return <section className={c.gallerySection}></section>;
+  return (
+    <section className={c.gallerySection}>
+      <h3 className={c.title}>Galerija</h3>
+      <p className={c.subtitle}>Throwback na prošlogodišnja izdanja Daysa</p>
+      <img src={ArrowTopRight} className={c.arrow} />
+    </section>
+  );
 };
 
 export default GallerySection;
