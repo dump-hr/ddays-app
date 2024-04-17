f7fe8befa63a1cd6a11b35ca7ed375b98d35da21
Lovre Tomić <tomiclovre05@gmail.com>
2024-04-17 12:03:05 +0200

    added link on click

--- apps/web/src/components/GallerySection/GallerySection.tsx
+++ apps/web/src/components/GallerySection/GallerySection.tsx
@@ -2,8 +2,15 @@ import c from './GallerySection.module.scss';
 import ArrowTopRight from '../../assets/icons/arrow-top-right.svg';
 
 const GallerySection = () => {
+  const onClickHandler = () => {
+    window.open(
+      'https://www.flickr.com/photos/143904306@N07/albums/',
+      '_blank',
+    );
+  };
+
   return (
-    <section className={c.gallerySection}>
+    <section className={c.gallerySection} onClick={onClickHandler}>
       <h3 className={c.title}>Galerija</h3>
       <p className={c.subtitle}>Throwback na prošlogodišnja izdanja Daysa</p>
       <img src={ArrowTopRight} className={c.arrow} />
