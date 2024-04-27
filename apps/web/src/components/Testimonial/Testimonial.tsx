9118a268778bd3f10fe37a1e674f163ae73cda3f
Lovre Tomić <tomiclovre05@gmail.com>
2024-04-27 17:45:32 +0200

    added content and layout

--- apps/web/src/components/Testimonial/Testimonial.tsx
+++ apps/web/src/components/Testimonial/Testimonial.tsx
@@ -1,5 +1,6 @@
 import clsx from 'clsx';
 
+import QuotesIcon from '../../assets/icons/quotes.svg';
 import c from './Testimonial.module.scss';
 
 const color = {
@@ -10,9 +11,12 @@ const color = {
 
 type TestimonialProps = {
   color: keyof typeof color;
+  name: string;
+  position: string;
+  text: string;
 };
 
-const Testimonial = ({ color }: TestimonialProps) => {
+const Testimonial = ({ color, name, position, text }: TestimonialProps) => {
   const classes = clsx({
     [c.testimonial]: true,
     [c.white]: color === 'white',
@@ -20,7 +24,20 @@ const Testimonial = ({ color }: TestimonialProps) => {
     [c.beige]: color === 'beige',
   });
 
-  return <div className={classes}>Testimonial</div>;
+  return (
+    <div className={classes}>
+      <div className={c.leftWrapper}>
+        <img src={QuotesIcon} className={c.quote} />
+        <div className={c.infoWrapper}>
+          <p className={c.name}>{name}</p>
+          <p className={c.position}>{position}</p>
+        </div>
+      </div>
+      <div className={c.rightWrapper}>
+        <p className={c.text}>{text}</p>
+      </div>
+    </div>
+  );
 };
 
 export default Testimonial;
--- apps/web/src/components/TestimonialsSection/TestimonialsSection.tsx
+++ apps/web/src/components/TestimonialsSection/TestimonialsSection.tsx
@@ -1,9 +1,17 @@
 import Testimonial from 'components/Testimonial/Testimonial';
 
 const TestimonialsSection = () => {
+  const data = {
+    name: 'Nicolette Pribanić',
+    position: 'Software Engineering Manager @ Google',
+    text: 'Preporučila bih ovu konferenciju svim studentima, kao i inženjerima koji već rade u nekoj IT tvrtki, a žele napredovati u karijeri u tehnološkoj industriji ili upoznati predstavnike raznih tvrtki te naučiti ponešto o njima iz prve ruke.',
+  };
+
   return (
     <section>
-      <Testimonial color='white' />
+      <Testimonial color='white' {...data} />
+      <Testimonial color='black' {...data} />
+      <Testimonial color='beige' {...data} />
     </section>
   );
 };
