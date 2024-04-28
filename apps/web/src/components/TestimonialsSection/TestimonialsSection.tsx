98ad538d605ab21e74b01241a8943a49aeb3dbc0
Lovre Tomić <tomiclovre05@gmail.com>
2024-04-28 17:07:12 +0200

    updated testimonial content

--- apps/web/src/components/TestimonialsSection/TestimonialsSection.tsx
+++ apps/web/src/components/TestimonialsSection/TestimonialsSection.tsx
@@ -3,17 +3,29 @@ import Testimonial from 'components/Testimonial/Testimonial';
 import c from './TestimonialsSection.module.scss';
 
 const TestimonialsSection = () => {
-  const data = {
-    name: 'Nicolette Pribanić',
-    position: 'Software Engineering Manager @ Google',
-    text: 'Preporučila bih ovu konferenciju svim studentima, kao i inženjerima koji već rade u nekoj IT tvrtki, a žele napredovati u karijeri u tehnološkoj industriji ili upoznati predstavnike raznih tvrtki te naučiti ponešto o njima iz prve ruke.',
-  };
+  const testimonials = [
+    {
+      name: 'Luka Abrus',
+      position: 'Partner and co-founder @ Five',
+      text: 'DUMP Days su nedvojbeno najbolja studentska konferencija u Hrvatskoj, a i jedna od boljih tehnoloških konferencija na kojima sam bio u regiji i šire.',
+    },
+    {
+      name: 'Dragan Petric',
+      position: 'Executive Editor @ Bug',
+      text: 'Sudjelovao sam na DUMP Daysima preko nekoliko puta kao speaker i uvijek je to bilo vanserijsko iskustvo kakvo nisam u prilici doživjeti niti na jednoj drugoj tehnološkoj konferenciji u našoj regiji.',
+    },
+    {
+      name: 'Nicolette Pribanić',
+      position: 'Software Engineering Manager @ Google',
+      text: 'Preporučila bih ovu konferenciju svim studentima, kao i inženjerima koji već rade u nekoj IT tvrtki, a žele napredovati u karijeri u tehnološkoj industriji ili upoznati predstavnike raznih tvrtki te naučiti ponešto o njima iz prve ruke.',
+    },
+  ];
 
   return (
     <section className={c.testimonialsSection}>
-      <Testimonial color='white' {...data} />
-      <Testimonial color='black' {...data} />
-      <Testimonial color='beige' {...data} />
+      <Testimonial color='white' {...testimonials[0]} />
+      <Testimonial color='black' {...testimonials[1]} />
+      <Testimonial color='beige' {...testimonials[2]} />
     </section>
   );
 };
