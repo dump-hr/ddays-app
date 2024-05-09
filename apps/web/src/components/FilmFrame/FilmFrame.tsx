b03c8142340eb6f50f8381dfc13614cfd68039e6
Lovre Tomić <tomiclovre05@gmail.com>
2024-05-09 11:08:23 +0200

    Revert "removed film frame"
    This reverts commit 3984aaeffc18b25b579ac36d2486cb9f237928fd.

---
+++ apps/web/src/components/FilmFrame/FilmFrame.tsx
@@ -0,0 +1,19 @@
+import FilmFrameImage from '../../assets/film-frame.webp';
+import c from './FilmFrame.module.scss';
+
+type FilmFrameProps = {
+  imageSrc: string | undefined;
+  width: number;
+  height: number;
+};
+
+const FilmFrame: React.FC<FilmFrameProps> = ({ imageSrc, width, height }) => {
+  return (
+    <div style={{ height: height, width: width }} className={c.frameWrapper}>
+      <img className={c.frame} src={FilmFrameImage} alt='' />
+      <img className={c.frameImage} src={imageSrc} />
+    </div>
+  );
+};
+
+export default FilmFrame;
