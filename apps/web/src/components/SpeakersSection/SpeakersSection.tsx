a39f8828513b189dbff1b50f372345c482043559
Lovre Tomić <92167015+lovretomic@users.noreply.github.com>
2024-09-28 22:45:40 +0200

    added base content

--- apps/web/src/components/SpeakersSection/SpeakersSection.tsx
+++ apps/web/src/components/SpeakersSection/SpeakersSection.tsx
@@ -1,4 +1,6 @@
+import { SectionBreaker } from 'components/SectionBreaker';
 import { useSpeakerWithCompanyGetAll } from '../../api/speaker/useSpeakerWithCompanyGetAll';
+import c from './SpeakersSection.module.scss';
 
 const SpeakersSection = () => {
   const speakers = useSpeakerWithCompanyGetAll();
@@ -6,6 +8,17 @@ const SpeakersSection = () => {
   if (speakers.isLoading || !speakers.data) {
     return null;
   }
+
+  return (
+    <section className={c.speakersSection} id='speakeri'>
+      <div className={c.titleWrapper}>
+        <p className={c.subtitle}>Digitalni inovatori pod istim krovom</p>
+        <h1 className={c.title}>Speakeri</h1>
+      </div>
+      <div className={c.speakerCardsWrapper}></div>
+      <SectionBreaker fg='green' />
+    </section>
+  );
 };
 
 export default SpeakersSection;
