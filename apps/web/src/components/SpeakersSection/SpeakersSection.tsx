17dd1da3b977e3c8c2a547c51088be9597382d8e
Lovre Tomić <92167015+lovretomic@users.noreply.github.com>
2024-09-28 22:41:06 +0200

    removed everything else

--- apps/web/src/components/SpeakersSection/SpeakersSection.tsx
+++ apps/web/src/components/SpeakersSection/SpeakersSection.tsx
@@ -1,8 +1,4 @@
-import { SectionBreaker } from 'components/SectionBreaker';
-
 import { useSpeakerWithCompanyGetAll } from '../../api/speaker/useSpeakerWithCompanyGetAll';
-
-import c from './SpeakersSection.module.scss';
 
 const SpeakersSection = () => {
   const speakers = useSpeakerWithCompanyGetAll();
@@ -10,23 +6,6 @@ const SpeakersSection = () => {
   if (speakers.isLoading || !speakers.data) {
     return null;
   }
-
-  return (
-    <div className={c.background} id='speakeri'>
-      <div className={c.wrapper}>
-        <div className={c.headerContainer}>
-          <p className={c.headerSmallText}>
-            Digitalni inovatori pod istim krovom
-          </p>
-          <h2 className={c.headerTitle}>SPEAKERI ({speakers.data.length})</h2>
-        </div>
-      </div>
-      <div className={c.sectionBreaker}>
-        <SectionBreaker fg='green' />
-        <div className={c.breakerPadding} />
-      </div>
-    </div>
-  );
 };
 
 export default SpeakersSection;
