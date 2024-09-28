a7cda9cc93650f380ef75167a1de7bdad448f4fd
Lovre Tomić <92167015+lovretomic@users.noreply.github.com>
2024-09-28 22:35:21 +0200

    removed speakers grid

--- apps/web/src/components/SpeakersSection/SpeakersSection.tsx
+++ apps/web/src/components/SpeakersSection/SpeakersSection.tsx
@@ -1,31 +1,15 @@
-import clsx from 'clsx';
 import { SectionBreaker } from 'components/SectionBreaker';
-import { useState } from 'react';
 
 import { useSpeakerWithCompanyGetAll } from '../../api/speaker/useSpeakerWithCompanyGetAll';
-import { useScreenSize } from '../../hooks/useScreenSize';
-import SpeakerCard from './SpeakerCard';
+
 import c from './SpeakersSection.module.scss';
-import { getCardWidth, getColumns } from './utils';
 
 const SpeakersSection = () => {
-  const { screenWidth, isMobile } = useScreenSize(950);
-  const cardAspectRatio = 401 / 320;
-  const [isExpanded, setIsExpanded] = useState(false);
-
   const speakers = useSpeakerWithCompanyGetAll();
-
-  const handleSetExpanded = () => {
-    setIsExpanded((prev) => !prev);
-  };
 
   if (speakers.isLoading || !speakers.data) {
     return null;
   }
-
-  const balancedArray = getColumns(speakers.data, isMobile);
-
-  const cardWidth = getCardWidth(screenWidth);
 
   return (
     <div className={c.background} id='speakeri'>
@@ -36,30 +20,6 @@ const SpeakersSection = () => {
           </p>
           <h2 className={c.headerTitle}>SPEAKERI ({speakers.data.length})</h2>
         </div>
-        <div
-          className={clsx(c.speakersWrapperContainer, {
-            [c.expanded]: isExpanded,
-          })}>
-          <div className={c.speakersWrapper}>
-            {balancedArray?.map((subArray, index) => (
-              <div key={index} className={c.speakersColumn}>
-                {subArray.map((speaker) => (
-                  <SpeakerCard
-                    key={speaker.id}
-                    speaker={speaker}
-                    height={cardWidth * cardAspectRatio}
-                    width={cardWidth}
-                  />
-                ))}
-              </div>
-            ))}
-          </div>
-        </div>
-        {!isExpanded && (
-          <div onClick={handleSetExpanded} className={c.expandOption}>
-            [ Pogledajte sve predavače ]
-          </div>
-        )}
       </div>
       <div className={c.sectionBreaker}>
         <SectionBreaker fg='green' />
