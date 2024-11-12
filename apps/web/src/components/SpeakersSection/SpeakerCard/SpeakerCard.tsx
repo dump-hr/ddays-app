cbcabf7723754898b2e3d2b11b94407d2d2925a8
Lovre Tomić <tomiclovre05@gmail.com>
2024-11-12 09:29:12 +0100

    added motion animations

--- apps/web/src/components/SpeakersSection/SpeakerCard/SpeakerCard.tsx
+++ apps/web/src/components/SpeakersSection/SpeakerCard/SpeakerCard.tsx
@@ -1,5 +1,6 @@
 import { SpeakerWithCompanyDto } from '@ddays-app/types';
 import clsx from 'clsx';
+import { motion } from 'framer-motion';
 
 import c from './SpeakerCard.module.scss';
 
@@ -7,11 +8,20 @@ type SpeakerCardProps = {
   speaker: SpeakerWithCompanyDto;
 } & React.HTMLAttributes<HTMLDivElement>;
 
-const SpeakerCard = ({ speaker, className, ...handlers }: SpeakerCardProps) => {
+const SpeakerCard = ({
+  speaker,
+  className /*, ...handlers*/,
+}: SpeakerCardProps) => {
   const classes = clsx(c.speakerCard, className);
 
   return (
-    <div className={classes} {...handlers}>
+    <motion.div
+      initial={{ opacity: 0 }}
+      whileInView={{ opacity: 1 }}
+      transition={{ ease: 'easeIn', duration: 1 }}
+      className={classes}
+      //{...handlers} <-- Zbog ovoga ne radi modion.div
+    >
       <img className={c.image} src={speaker.photo} alt='' />
       <p className={c.fullName}>
         {speaker.firstName} {speaker.lastName}
@@ -21,7 +31,7 @@ const SpeakerCard = ({ speaker, className, ...handlers }: SpeakerCardProps) => {
           {speaker.title} @ {speaker.company?.name}
         </p>
       )}
-    </div>
+    </motion.div>
   );
 };
 
