e375e6890d9b08b3d30ef867ae15c76f554abde0
Lovre Tomić <tomiclovre05@gmail.com>
2024-11-15 16:06:50 +0100

    added scss preprocessor

--- apps/app/vite.config.ts
+++ apps/app/vite.config.ts
@@ -1,7 +1,24 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import path from 'path';
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  css: {
+    preprocessorOptions: {
+      scss: {
+        additionalData: `
+          @import "./src/styles/colors.scss";
+          @import "./src/styles/fonts.scss";
+          @import "./src/styles/mixins.scss";
+        `,
+      },
+    },
+  },
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
+  },
+});
