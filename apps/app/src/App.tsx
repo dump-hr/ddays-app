87dcff98042c8237785ffc7f250d1b5e9da43c99
tsiklic1 <100917481+tsiklic1@users.noreply.github.com>
2024-11-20 21:28:19 +0100

    added Button

--- apps/app/src/App.tsx
+++ apps/app/src/App.tsx
@@ -1,7 +1,10 @@
+import Button from './components/Button';
+
 function App() {
   return (
     <>
       <p>DUMP Days 2025 App</p>
+      <Button>Click me</Button>
     </>
   );
 }
--- apps/app/src/components/Button/Button.tsx
+++ apps/app/src/components/Button/Button.tsx
@@ -1,5 +1,15 @@
-const Button = () => {
-  return <></>;
+import c from './Button.module.scss';
+
+type ButtonProps = {
+  children: React.ReactNode;
+} & React.ButtonHTMLAttributes<HTMLButtonElement>;
+
+const Button: React.FC<ButtonProps> = ({ children, ...handlers }) => {
+  return (
+    <button className={c.button} {...handlers}>
+      {children}
+    </button>
+  );
 };
 
 export default Button;
