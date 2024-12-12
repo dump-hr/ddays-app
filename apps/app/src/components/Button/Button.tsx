3d0b18a1d23b1a2e13d96feb697dd48fde6969e1
tsiklic1 <100917481+tsiklic1@users.noreply.github.com>
2024-12-12 10:53:54 +0100

    added button variants, hover and active states

--- apps/app/src/components/Button/Button.tsx
+++ apps/app/src/components/Button/Button.tsx
@@ -1,12 +1,29 @@
 import c from './Button.module.scss';
+import clsx from 'clsx';
 
 type ButtonProps = {
   children: React.ReactNode;
+  variant: 'orange' | 'black' | 'beige';
+  active?: boolean;
 } & React.ButtonHTMLAttributes<HTMLButtonElement>;
 
-const Button: React.FC<ButtonProps> = ({ children, ...handlers }) => {
+const Button: React.FC<ButtonProps> = ({
+  children,
+  variant,
+  active,
+  ...handlers
+}) => {
   return (
-    <button className={c.button} {...handlers}>
+    <button
+      className={clsx(c.button, {
+        [c.orange]: variant === 'orange',
+        [c.black]: variant === 'black',
+        [c.beige]: variant === 'beige',
+        [c.orangeActive]: active && variant === 'orange',
+        [c.blackActive]: active && variant === 'black',
+        [c.beigeActive]: active && variant === 'beige',
+      })}
+      {...handlers}>
       {children}
     </button>
   );
