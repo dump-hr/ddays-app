87b8f551996138d3149b5fe61b6b14af6f1d653e
tsiklic1 <100917481+tsiklic1@users.noreply.github.com>
2024-12-12 17:25:14 +0100

    added notification expanding logic

--- apps/app/src/App.tsx
+++ apps/app/src/App.tsx
@@ -3,8 +3,34 @@ import Button from './components/Button';
 import ToggleButton from './components/ToggleButton';
 import Notification from './components/Notification';
 
+const notifications = [
+  {
+    id: 1,
+    title: 'Došle su pizze 1',
+    content: 'Dobar tek i pizzeria Mirakul nas časte pizzama',
+    time: '19 min ago',
+  },
+  {
+    id: 2,
+    title: 'Došle su pizze 2',
+    content:
+      'Dobar tek i pizzeria Mirakul nas časte pizzama, skupi svoju krišku u akvariju. Dobar tek i pizzeria Mirakul nas pizzama, skupi svoju krišku u akvariju. Dobar tek i pizzeria Mirakul nas časte pizzama, skupi svoju krišku u akvariju. Dobar tek i pizzeria Mirakul nas pizzama, skupi svoju krišku u akvariju.',
+    time: '19 min ago',
+  },
+  {
+    id: 3,
+    title: 'Došle su pizze 3',
+    content:
+      'Dobar tek i pizzeria Mirakul nas časte pizzama, skupi svoju krišku u akvariju. Dobar tek i pizzeria Mirakul nas pizzama, skupi svoju krišku u akvariju. Dobar tek i pizzeria Mirakul nas časte pizzama, skupi svoju krišku u akvariju. Dobar tek i pizzeria Mirakul nas pizzama, skupi svoju krišku u akvariju.',
+    time: '19 min ago',
+  },
+];
+
 function App() {
   const [toggled, setToggled] = useState(false);
+  const [expandedNotificationId, setExpandedNotificationId] = useState<
+    number | null
+  >(null);
   return (
     <div>
       <p>DUMP Days 2025 App</p>
@@ -28,19 +54,14 @@ function App() {
       <br />
       <br />
       <br />
-      <Notification
-        title='Došle su pizze'
-        content={'Dobar tek i pizzeria Mirakul nas časte pizzama'}
-        time={'19 min ago'}
-      />
-
-      <Notification
-        title={'Došle su pizze'}
-        content={
-          'Dobar tek i pizzeria Mirakul nas časte pizzama, skupi svoju krišku u akvariju. Dobar tek i pizzeria Mirakul nas pizzama, skupi svoju krišku u akvariju. Dobar tek i pizzeria Mirakul nas časte pizzama, skupi svoju krišku u akvariju. Dobar tek i pizzeria Mirakul nas pizzama, skupi svoju krišku u akvariju.'
-        }
-        time={'19 min ago'}
-      />
+      {notifications.map((notification) => (
+        <Notification
+          key={notification.id}
+          {...notification}
+          expandedNotificationId={expandedNotificationId}
+          setExpandedNotificationId={setExpandedNotificationId}
+        />
+      ))}
     </div>
   );
 }
--- apps/app/src/components/Notification/Notification.tsx
+++ apps/app/src/components/Notification/Notification.tsx
@@ -1,45 +1,76 @@
 // import { useEffect, useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import c from './Notification.module.scss';
 import clsx from 'clsx';
 
 type NotificationProps = {
+  id: number;
   title: string;
   content: string;
   time: string;
+  expandedNotificationId: number | null;
+  setExpandedNotificationId: (id: number | null) => void;
 };
 
 //TODO: function that returns time string ex: 1 h ago or 10 min ago
 
-const maxContentLength = 129;
-
 const Notification: React.FC<NotificationProps> = ({
+  id,
   title,
   content,
   time,
+  expandedNotificationId,
+  setExpandedNotificationId,
 }) => {
+  const [showExpandButton, setShowExpandButton] = useState(false);
+  const isExpanded = expandedNotificationId === id;
+  console.log(expandedNotificationId);
+
+  const ref = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    if (ref.current) {
+      setShowExpandButton(
+        ref.current.scrollHeight !== ref.current.clientHeight,
+      );
+    }
+  }, [expandedNotificationId]);
+
   return (
     <div className={c.notificationWrapper}>
       <div className={clsx(c.flex, c.titleWrapper)}>
         <h4 className={c.title}>{title}</h4>
         <div className={c.orangeDot}></div>
       </div>
-      {content.length < maxContentLength ? (
-        <p className={c.content}>{content}</p>
-      ) : (
-        <div className={c.flex}>
-          <p
-            className={c.content}
-            style={{
-              WebkitLineClamp: 3,
-              WebkitBoxOrient: 'vertical',
-              overflow: 'hidden',
-              display: '-webkit-box',
+
+      <div className={c.flex}>
+        <p
+          ref={ref}
+          className={c.content}
+          style={
+            isExpanded
+              ? undefined
+              : {
+                  WebkitLineClamp: 3,
+                  WebkitBoxOrient: 'vertical',
+                  overflow: 'hidden',
+                  display: '-webkit-box',
+                }
+          }>
+          {content}
+          {showExpandButton && '...'}
+        </p>
+        {showExpandButton && (
+          <button
+            className={c.expandButton}
+            onClick={() => {
+              setExpandedNotificationId(id);
+              setShowExpandButton(false);
             }}>
-            {content}...
-          </p>
-          <button className={c.expandButton}>Proširi</button>
-        </div>
-      )}
+            Proširi
+          </button>
+        )}
+      </div>
       <p className={c.time}>{time}</p>
       <div className={c.dottedBreak}></div>
     </div>
