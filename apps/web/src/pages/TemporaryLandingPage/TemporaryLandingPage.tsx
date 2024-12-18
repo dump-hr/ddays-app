f231502e78daf5cf724621ed4bcece2d75abdbf9
Lovre Tomić <tomiclovre05@gmail.com>
2024-12-18 16:53:25 +0100

    removed unused logic

--- apps/web/src/pages/TemporaryLandingPage/TemporaryLandingPage.tsx
+++ apps/web/src/pages/TemporaryLandingPage/TemporaryLandingPage.tsx
@@ -1,15 +1,11 @@
 import { ReactLenis } from '@studio-freight/react-lenis';
-import MobileMenu from 'components/MobileMenu';
 import TemporaryFooterSection from 'components/TemporaryFooterSection';
 import TemporaryHeader from 'components/TemporaryHeader';
 import TemporaryHero from 'components/TemporaryHero';
-import { useState } from 'react';
 
 import Button from '../../components/Button';
-import { landingNavigation } from '../../constants/landing-navigation';
 
 export const TemporaryLandingPage: React.FC = () => {
-  const [mobileMenuOpen, setMobileMenuOpen] = useState(false);
   const RegisterButton = (
     <Button
       onClick={() => (window.location.href = 'https://days-app.dump.hr/login')}>
@@ -17,21 +13,10 @@ export const TemporaryLandingPage: React.FC = () => {
     </Button>
   );
 
-  const toggleMobileMenu = () => {
-    setMobileMenuOpen((prev) => !prev);
-  };
-
   return (
     <ReactLenis root>
       <TemporaryHeader />
-      {window.innerWidth < 768 && (
-        <MobileMenu
-          Button={RegisterButton}
-          isOpen={mobileMenuOpen}
-          toggle={toggleMobileMenu}
-          items={landingNavigation}
-        />
-      )}
+
       <TemporaryHero Button={RegisterButton} />
       <TemporaryFooterSection />
     </ReactLenis>
