6eeed8b14a0764a178c167606927da154d8501cf
Lovre Tomić <tomiclovre05@gmail.com>
2024-12-18 17:12:18 +0100

    completed button functionality

--- apps/web/src/components/TemporaryHero/TemporaryHero.tsx
+++ apps/web/src/components/TemporaryHero/TemporaryHero.tsx
@@ -4,6 +4,13 @@ import ScrollingTitle from '../ScrollingTitle';
 import c from './TemporaryHero.module.scss';
 
 const TemporaryHero = () => {
+  function openBrochure() {
+    window.open(
+      'https://drive.google.com/file/d/1lxAC5i2bDmS76NvG_D6BEL2E0AysBQlW/view?usp=drive_link',
+      '_blank',
+    );
+  }
+
   return (
     <section className={c.hero}>
       <div className={c.content}>
@@ -12,7 +19,9 @@ const TemporaryHero = () => {
       <div className={c.textButtonsWrapper}>
         <div className={c.buttons}>
           <Button>Posjeti web</Button>
-          <Button>Postani sponzor</Button>
+          <Button className={c.orangeButton} onClick={openBrochure}>
+            Postani sponzor
+          </Button>
         </div>
       </div>
 
