0470d1c26aaa32d29261698a96608f98c87a4577
Lovre Tomić <tomiclovre05@gmail.com>
2025-01-06 19:14:14 +0100

    removed components from App.tsx

--- apps/app/src/App.tsx
+++ apps/app/src/App.tsx
@@ -1,26 +1,7 @@
-import { useState } from 'react';
-import { Input } from './components/Input';
-
 function App() {
-  const [value, setValue] = useState('');
-  const [value2, setValue2] = useState('');
-
   return (
     <>
       <p>DUMP Days 2025 App</p>
-      <div className='wrapper'>
-        <Input
-          type='password'
-          placeholder='Input'
-          value={value}
-          onChange={(e) => setValue(e.target.value)}
-        />
-        <Input
-          placeholder='Input'
-          value={value2}
-          onChange={(e) => setValue2(e.target.value)}
-        />
-      </div>
     </>
   );
 }
