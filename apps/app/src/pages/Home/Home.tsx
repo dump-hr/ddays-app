8d48ebf2545ea9fa25d84bc106447494271aa226
Lovre Tomić <tomiclovre05@gmail.com>
2025-01-06 23:52:06 +0100

    added home page base layout

---
+++ apps/app/src/pages/Home/Home.tsx
@@ -0,0 +1,12 @@
+import c from './Home.module.scss';
+
+const Home = () => {
+  return (
+    <div className={c.page}>
+      <header className={c.header}></header>
+      <main className={c.main}></main>
+    </div>
+  );
+};
+
+export default Home;
