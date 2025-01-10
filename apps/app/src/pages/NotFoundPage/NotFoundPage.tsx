adbf3172a5569a51c8cc5404c0926d51f307cb0a
dbaric99 <damjana.baric1999@gmail.com>
2025-01-10 22:39:39 +0100

    Test build

--- apps/app/src/pages/NotFoundPage/NotFoundPage.tsx
+++ apps/app/src/pages/NotFoundPage/NotFoundPage.tsx
@@ -2,7 +2,12 @@ export const NotFoundPage = () => {
   return (
     <div>
       <h1>404</h1>
-      <p>Page not found</p>
+      <p>
+        Page not found{' '}
+        <a href='https://www.youtube.com/watch?v=dQw4w9WgXcQ&ab_channel=RickAstley'>
+          home
+        </a>
+      </p>
     </div>
   );
 };
