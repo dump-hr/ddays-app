a41d7a5492965809a7ec1425a6974ee55faec40b
SimeJadrijev <sime.jadrijev@gmail.com>
2025-01-13 00:13:24 +0100

    display Header

--- apps/app/src/pages/Home/Home.tsx
+++ apps/app/src/pages/Home/Home.tsx
@@ -1,9 +1,12 @@
+import { Header } from '../../components/Header';
 import c from './Home.module.scss';
 
 const Home = () => {
   return (
     <div className={c.page}>
-      <header className={c.header}></header>
+      <header className={c.header}>
+        <Header />
+      </header>
       <main className={c.main}></main>
     </div>
   );
