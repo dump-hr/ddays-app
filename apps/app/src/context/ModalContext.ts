a159ba1b17fdd0a2dde997703196c8cfd7ae898c
dbaric99 <damjana.baric1999@gmail.com>
2025-01-12 20:27:11 +0100

    Create modal context

---
+++ apps/app/src/context/ModalContext.ts
@@ -0,0 +1,18 @@
+import { createContext, useContext } from 'react';
+import { ModalNames } from '../router/routes';
+
+type ModalContextProps = {
+  openModal: (modalName: ModalNames, submenuOption?: string) => void;
+  closeModal: () => void;
+  canOpenModal: (modalName: ModalNames) => boolean;
+};
+
+const defaultModalContext: ModalContextProps = {
+  openModal: () => {},
+  closeModal: () => {},
+  canOpenModal: () => false,
+};
+
+const ModalContext = createContext<ModalContextProps>(defaultModalContext);
+
+export const useModal = () => useContext(ModalContext);
