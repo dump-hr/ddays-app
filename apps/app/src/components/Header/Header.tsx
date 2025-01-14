ab24505b42218223b5f7708d7e52e71d0bfef814
SimeJadrijev <sime.jadrijev@gmail.com>
2025-01-14 16:04:09 +0100

    refactor Header component and styles

--- apps/app/src/components/Header/Header.tsx
+++ apps/app/src/components/Header/Header.tsx
@@ -1,62 +1,25 @@
 import styles from './Header.module.scss';
-import { HeaderCard } from './HeaderCard';
-import accreditationImage from './../../assets/images/Akreditacija.png';
-import enterCodeImage from './../../assets/images/unesiKod.png';
-import tShirtImage from './../../assets/images/majica.png';
 
-enum HeaderCards {
-  ENTER_CODE,
-  ACCREDIATION,
-  SHOPPING,
-}
-
-type HeaderCardsInfo = {
-  id: HeaderCards;
-  img: string;
-  text: string;
-  width: number;
-  height: number;
-};
-
-const headerCards: HeaderCardsInfo[] = [
-  {
-    id: HeaderCards.ENTER_CODE,
-    img: `${enterCodeImage}`,
-    text: 'Unesi kod',
-    width: 77,
-    height: 34,
-  },
-  {
-    id: HeaderCards.ACCREDIATION,
-    img: `${accreditationImage}`,
-    text: 'Akreditacija',
-    width: 60,
-    height: 39,
-  },
-  {
-    id: HeaderCards.SHOPPING,
-    img: `${tShirtImage}`,
-    text: 'Shopping',
-    width: 80,
-    height: 36,
-  },
-];
+import { useDeviceType } from '../../hooks/UseDeviceType';
+import { NotificationBell } from './NotificationBell';
+import { HeaderCardsWrapper } from './HeaderCardsWrapper';
 
 export const Header = () => {
+  const { isMobile } = useDeviceType({});
+
   return (
     <div className={styles.header}>
-      <h1 className={styles.header}>Hello Mihaela! 👋🏻</h1>
-      <div className={styles.headerCardsWrapper}>
-        {headerCards.map((card) => (
-          <HeaderCard
-            key={card.id}
-            img={card.img}
-            text={card.text}
-            width={card.width}
-            height={card.height}
-          />
-        ))}
+      <div className={styles.headerGreeting}>
+        <h1>Hello, {isMobile && <br />} Mihaela! 👋🏻</h1>
+
+        {isMobile && (
+          <div>
+            <NotificationBell />
+          </div>
+        )}
       </div>
+
+      <HeaderCardsWrapper />
     </div>
   );
 };
