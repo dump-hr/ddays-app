b9461756866da9f3cd31a494fa80714ac223f99a
dbaric99 <damjana.baric1999@gmail.com>
2025-01-16 18:53:30 +0100

    Handle click outside

--- apps/app/src/components/Modal/BaseModal.tsx
+++ apps/app/src/components/Modal/BaseModal.tsx
@@ -1,4 +1,4 @@
-import { ReactNode } from 'react';
+import { ReactNode, useEffect, useRef } from 'react';
 import sprite from '../../assets/sprite.svg';
 import styles from './Modal.module.scss';
 
@@ -17,20 +17,47 @@ export const BaseModal: React.FC<BaseModalProps> = ({
   withOverlay = false,
   children,
 }) => {
+  const modalRef = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    const handleClickOutside = (event: MouseEvent) => {
+      if (
+        modalRef.current &&
+        !modalRef.current.contains(event.target as Node)
+      ) {
+        onClose();
+      }
+    };
+
+    if (isOpen) {
+      document.addEventListener('mousedown', handleClickOutside);
+    } else {
+      document.removeEventListener('mousedown', handleClickOutside);
+    }
+
+    return () => {
+      document.removeEventListener('mousedown', handleClickOutside);
+    };
+  }, [isOpen, onClose]);
+
   if (!isOpen) return null;
 
   return (
-    <div className={styles.modalContainer}>
-      {withOverlay && <div className={styles.backgroundOverlay} />}
-      <div className={styles.modalHeaderWrapper}>
-        <p className={styles.modalTitle}>{title}</p>
-        <div onClick={() => onClose()}>
-          <svg className={styles.modalCloseIcon} width={22} height={22}>
-            <use href={`${sprite}#close-icon`} />
-          </svg>
+    <>
+      {withOverlay && (
+        <div className={styles.backgroundOverlay} onClick={onClose} />
+      )}
+      <div ref={modalRef} className={styles.modalContainer}>
+        <div className={styles.modalHeaderWrapper}>
+          <p className={styles.modalTitle}>{title}</p>
+          <div onClick={onClose}>
+            <svg className={styles.modalCloseIcon} width={22} height={22}>
+              <use href={`${sprite}#close-icon`} />
+            </svg>
+          </div>
         </div>
+        {children}
       </div>
-      {children}
-    </div>
+    </>
   );
 };
