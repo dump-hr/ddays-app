855b7d69b8da924dfe5212d9f3a9c0eda21096bc
Lovre Tomić <tomiclovre05@gmail.com>
2025-01-19 09:17:54 +0100

    refactore component to new file

--- apps/app/src/pages/Home/Home.tsx
+++ apps/app/src/pages/Home/Home.tsx
@@ -1,30 +1,12 @@
-import React from 'react';
-import RecommendationsButton from '../../components/RecommendationsButton';
-import TopCompany from '../../components/TopFirm';
 import c from './Home.module.scss';
-import { companies } from './companies';
+import TopCompaniesSection from './sections/TopCompaniesSection';
 
 const Home = () => {
   return (
     <div className={c.page}>
       <header className={c.header}></header>
       <main className={c.main}>
-        <section className={c.topCompanies}>
-          <div className={c.mainContent}>
-            <h2 className={c.title}>Top 5 firma</h2>
-            <p className={c.description}>
-              Otkrij najbolje sponzorske štandove prema ocjenama posjetitelja
-              konferencije.
-            </p>
-            {companies.map((company, index) => (
-              <React.Fragment key={index}>
-                {index !== 0 && <div className={c.divider} />}
-                <TopCompany company={company} number={index + 1} />
-              </React.Fragment>
-            ))}
-          </div>
-          <RecommendationsButton className={c.duck} />
-        </section>
+        <TopCompaniesSection />
       </main>
     </div>
   );
