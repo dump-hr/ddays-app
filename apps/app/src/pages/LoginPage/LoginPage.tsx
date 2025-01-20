2a75cd01a756b3512fe6d89fcc2ecf9618d0e3e0
andrea-zecevic <azecev01@gmail.com>
2025-01-20 21:42:04 +0100

    Login title and input

--- apps/app/src/pages/LoginPage/LoginPage.tsx
+++ apps/app/src/pages/LoginPage/LoginPage.tsx
@@ -1,7 +1,35 @@
+import { Input } from '../../components/Input';
+import c from './LoginPage.module.scss';
+import closeIcon from '../../assets/icons/Group.svg';
+
 export const LoginPage = () => {
   return (
     <div>
-      <h1>Login Page</h1>
+      <div>
+        <h1>Login Page</h1>
+      </div>
+      <div className={c.container}>
+        <div className={c.titleContainer}>
+          <h1 className={c.title}>Dobro došli natrag!</h1>
+          <img src={closeIcon} alt='Close login' className={c.closeIcon} />
+        </div>
+        <div className={c.formContainer}>
+          <Input
+            label='Email'
+            type='text'
+            placeholder='Email'
+            value=''
+            onChange={() => {}}
+          />
+          <Input
+            label='Lozinka'
+            type='password'
+            placeholder='Lozinka'
+            value=''
+            onChange={() => {}}
+          />
+        </div>
+      </div>
     </div>
   );
 };
