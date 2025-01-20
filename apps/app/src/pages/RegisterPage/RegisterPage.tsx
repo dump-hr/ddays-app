d221885b8edcd843b09e13d4e0718ea71f6a690c
SimeJadrijev <sime.jadrijev@gmail.com>
2025-01-20 22:25:49 +0100

    slice ProgressBar

--- apps/app/src/pages/RegisterPage/RegisterPage.tsx
+++ apps/app/src/pages/RegisterPage/RegisterPage.tsx
@@ -1,7 +1,10 @@
+import { ProgressBar } from '../../components/ProgressBar';
+
 export const RegisterPage = () => {
   return (
     <div>
       <h1>Register</h1>
+      <ProgressBar />
     </div>
   );
 };
