61d88a9fadc2ff7a86615bf3df87561f716b15ba
andrea-zecevic <azecev01@gmail.com>
2025-01-22 22:09:11 +0100

    Add black div

--- apps/app/src/pages/LoginPage/LoginPage.tsx
+++ apps/app/src/pages/LoginPage/LoginPage.tsx
@@ -12,44 +12,49 @@ export const LoginPage = () => {
 
   return (
     <div>
-      <div className={c.container}>
-        <div className={c.titleContainer}>
-          <h1 className={c.title}>Dobro došli natrag!</h1>
-          <img src={closeIcon} alt='Close login' className={c.closeIcon} />
+      <div className={c.wrapper}>
+        <div className={c.blackDiv}>
+          <h1>Prijava</h1>
         </div>
-        <div className={c.formContainer}>
-          <Input
-            label='Email'
-            type='text'
-            placeholder='Email'
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
-          />
-          <Input
-            label='Lozinka'
-            type='password'
-            placeholder='Lozinka'
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-          />
-          <a href={RouteNames.PASSWORD_RESET} className={c.forgotPassword}>
-            Zaboravili ste lozinku?
-          </a>
-        </div>
-        <div className={c.buttonContainer}>
-          <Button variant='orange' onClick={() => {}}>
-            Prijavi se
-          </Button>
-          <Button variant='black' onClick={() => {}}>
-            <img src={googleIcon} alt='icon' className={c.googleIcon} />
-            Nastavi s Google
-          </Button>
-        </div>
-        <div className={c.registerContainer}>
-          <span className={c.noAccount}>Nemaš račun?</span>
-          <a href={RouteNames.REGISTER} className={c.registerLink}>
-            Registriraj se
-          </a>
+        <div className={c.container}>
+          <div className={c.titleContainer}>
+            <h1 className={c.title}>Dobro došli natrag!</h1>
+            <img src={closeIcon} alt='Close login' className={c.closeIcon} />
+          </div>
+          <div className={c.formContainer}>
+            <Input
+              label='Email'
+              type='text'
+              placeholder='Email'
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+            />
+            <Input
+              label='Lozinka'
+              type='password'
+              placeholder='Lozinka'
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+            />
+            <a href={RouteNames.PASSWORD_RESET} className={c.forgotPassword}>
+              Zaboravili ste lozinku?
+            </a>
+          </div>
+          <div className={c.buttonContainer}>
+            <Button variant='orange' onClick={() => {}}>
+              Prijavi se
+            </Button>
+            <Button variant='black' onClick={() => {}}>
+              <img src={googleIcon} alt='icon' className={c.googleIcon} />
+              Nastavi s Google
+            </Button>
+          </div>
+          <div className={c.registerContainer}>
+            <span className={c.noAccount}>Nemaš račun?</span>
+            <a href={RouteNames.REGISTER} className={c.registerLink}>
+              Registriraj se
+            </a>
+          </div>
         </div>
       </div>
     </div>
