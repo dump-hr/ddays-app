38ded975b70a05d03b8cdc5c8ec46a62d6ee131b
SimeJadrijev <sime.jadrijev@gmail.com>
2025-01-23 19:54:55 +0100

    allow navigating forward and backwards on navigation bar

--- apps/app/src/components/ProgressBar/ProgressBar.tsx
+++ apps/app/src/components/ProgressBar/ProgressBar.tsx
@@ -1,18 +1,36 @@
+import { useState } from 'react';
 import c from './ProgressBar.module.scss';
 import { ProgressBarTab } from './ProgressBarTab';
 export const ProgressBar = () => {
+  const [currentStep, setCurrentStep] = useState(1);
+
+  const isAllowedForNextStep = () => {
+    return true;
+  };
+
+  const handleStepClick = (clickedStep: number) => {
+    const isGoingForward = clickedStep <= currentStep;
+    const isGoingBackwards = clickedStep === currentStep + 1;
+
+    if (isGoingBackwards) {
+      setCurrentStep(clickedStep);
+    } else if (isGoingForward && isAllowedForNextStep()) {
+      setCurrentStep(clickedStep);
+    }
+  };
   return (
     <div className={c.progressBar}>
-      <ProgressBarTab stepIndex={1} active={true} />
-      <div className={c.dots}></div>
-
-      <ProgressBarTab stepIndex={2} active={false} />
-      <div className={c.dots}></div>
-
-      <ProgressBarTab stepIndex={3} active={false} />
-      <div className={c.dots}></div>
-
-      <ProgressBarTab stepIndex={4} active={false} />
+      {[1, 2, 3, 4].map((step) => (
+        <>
+          <ProgressBarTab
+            key={step}
+            stepIndex={step}
+            active={step === currentStep}
+            onClick={handleStepClick}
+          />
+          {step < 4 && <div className={c.dots}></div>}
+        </>
+      ))}
     </div>
   );
 };
--- apps/app/src/components/ProgressBar/ProgressBarTab/ProgressBarTab.tsx
+++ apps/app/src/components/ProgressBar/ProgressBarTab/ProgressBarTab.tsx
@@ -3,13 +3,16 @@ import c from './ProgressBarTab.module.scss';
 export const ProgressBarTab = ({
   stepIndex,
   active,
+  onClick,
 }: {
   stepIndex: number;
   active: boolean;
+  onClick: (stepIndex: number) => void;
 }) => {
   return (
     <div
-      className={`${c.progressBarTab} ${active ? c.activeProgressBarTab : ''}`}>
+      className={`${c.progressBarTab} ${active ? c.activeProgressBarTab : ''}`}
+      onClick={() => onClick(stepIndex)}>
       {stepIndex}
     </div>
   );
