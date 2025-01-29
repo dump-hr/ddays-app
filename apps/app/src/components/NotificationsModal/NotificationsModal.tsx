70206a1847868df823577c008cd9821683c98f6a
ToniGrbic <tonigrbic.5@gmail.com>
2025-01-29 03:37:37 +0100

    switch tabs logic and no notifications alert

--- apps/app/src/components/NotificationsModal/NotificationsModal.tsx
+++ apps/app/src/components/NotificationsModal/NotificationsModal.tsx
@@ -1,10 +1,11 @@
+import { useEffect, useMemo, useState } from 'react';
 import styles from './NotificationsModal.module.scss';
-import ArrowLeft from '../../assets/icons/arrow-left.svg';
-import TabGroup from '../TabGroup';
-import Tab from '../Tab';
-import Notification from '../Notification/Notification';
+import ArrowLeft from '@/assets/icons/arrow-left.svg';
+import IconBell from '@/assets/icons/icon-bell.svg';
+import TabGroup from '@/components/TabGroup';
+import Tab from '@/components/Tab';
+import Notification from '@/components/Notification/Notification';
 import { notifications } from './notifications.const';
-import { useState } from 'react';
 
 enum Tabs {
   Sve,
@@ -21,7 +22,26 @@ export const NotificationsModal: React.FC<NotificationsModalProps> = ({
   const [expandedNotificationId, setExpandedNotificationId] = useState<
     number | null
   >(null);
-  const handleTabChange = () => {};
+  const [notificationsTab, setNotificationsTab] = useState<string | number>(
+    Tabs.Sve,
+  );
+  const [displayedNotifications, setDisplayedNotifications] =
+    useState(notifications);
+
+  const handleTabChange = (tab: string) => {
+    setNotificationsTab(tab);
+  };
+
+  const allNotifications = useMemo(() => notifications, []);
+  const unreadNotifications = useMemo(() => notifications.slice(0, 2), []);
+
+  useEffect(() => {
+    if (notificationsTab === Tabs.Sve) {
+      setDisplayedNotifications(allNotifications);
+    } else if (notificationsTab === Tabs.Nepročitano) {
+      setDisplayedNotifications(unreadNotifications);
+    }
+  }, [notificationsTab, allNotifications, unreadNotifications]);
 
   return (
     <div className={styles.modalWrapper}>
@@ -34,23 +54,36 @@ export const NotificationsModal: React.FC<NotificationsModalProps> = ({
           </div>
           <h2 className={styles.title}>NOTIFIKACIJE</h2>
         </div>
-        <TabGroup setter={handleTabChange}>
-          <Tab id={Tabs.Sve}>Sve</Tab>
-          <Tab id={Tabs.Nepročitano}>Nepročitano</Tab>
-        </TabGroup>
-        <div className={styles.notificationsContainer}>
-          {notifications.map((notification) => (
-            <Notification
-              key={notification.id}
-              title={notification.title}
-              content={notification.content}
-              time={notification.time}
-              expandedNotificationId={expandedNotificationId}
-              id={notification.id}
-              setExpandedNotificationId={setExpandedNotificationId}
-            />
-          ))}
-        </div>
+        {notifications.length !== 0 ? (
+          <>
+            <TabGroup setter={handleTabChange}>
+              <Tab id={Tabs.Sve}>Sve</Tab>
+              <Tab id={Tabs.Nepročitano}>Nepročitano</Tab>
+            </TabGroup>
+            <div className={styles.notificationsContainer}>
+              {displayedNotifications.map((notification) => (
+                <Notification
+                  key={notification.id}
+                  id={notification.id}
+                  title={notification.title}
+                  content={notification.content}
+                  time={notification.time}
+                  expandedNotificationId={expandedNotificationId}
+                  setExpandedNotificationId={setExpandedNotificationId}
+                  notificationsLength={displayedNotifications.length}
+                />
+              ))}
+            </div>
+          </>
+        ) : (
+          <div className={styles.noNotificationsContainer}>
+            <img src={IconBell} alt='bell' />
+            <p className={styles.noNotificationsTitle}>NEMA NOTIFIKACIJA</p>
+            <p className={styles.noNotificationsLabel}>
+              Nažalost nemamo novosti za tebe, možda uskoro te zatrebamo
+            </p>
+          </div>
+        )}
       </div>
     </div>
   );
