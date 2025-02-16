cbb8f7b664c8e49918b5711d59d206db5a8d562a
ToniGrbic <tonigrbic.5@gmail.com>
2025-02-16 02:19:23 +0100

    formatting

--- apps/app/src/pages/ShoppingPage/sections/ShoppingHeader/ShoppingHeader.tsx
+++ apps/app/src/pages/ShoppingPage/sections/ShoppingHeader/ShoppingHeader.tsx
@@ -1,4 +1,4 @@
-import styles from './ShoppingHeader.module.scss'
+import styles from './ShoppingHeader.module.scss';
 import CrossIcon from '@/assets/icons/cross-icon.svg';
 import TransactionsIcon from '@/assets/icons/transactions-icon.svg';
 import ShoppingCartIcon from '@/assets/icons/shopping-cart-icon.svg';
@@ -6,22 +6,22 @@ import ShoppingCartIcon from '@/assets/icons/shopping-cart-icon.svg';
 const ShoppingHeader = () => {
   return (
     <div className={styles.headerContainer}>
-        <div className={styles.header}>
-          <h1 className={styles.title}>SHOPPING</h1>
-          <img src={CrossIcon} alt='' />
-        </div>
-        <div className={styles.navigation}>
-          <button className={styles.navButton}>
-            <img src={TransactionsIcon} alt='' />
-            <p>Transakcije</p>
-          </button>
-          <button className={styles.navButton}>
-            <img src={ShoppingCartIcon} alt='' />
-            <p>Košarica</p>
-          </button>
-        </div>
+      <div className={styles.header}>
+        <h1 className={styles.title}>SHOPPING</h1>
+        <img src={CrossIcon} alt='' />
       </div>
-  )
-}
+      <div className={styles.navigation}>
+        <button className={styles.navButton}>
+          <img src={TransactionsIcon} alt='' />
+          <p>Transakcije</p>
+        </button>
+        <button className={styles.navButton}>
+          <img src={ShoppingCartIcon} alt='' />
+          <p>Košarica</p>
+        </button>
+      </div>
+    </div>
+  );
+};
 
-export default ShoppingHeader
+export default ShoppingHeader;
