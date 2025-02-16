54144aed76c1a2843ffb71e498b2d6de45789b5c
ToniGrbic <tonigrbic.5@gmail.com>
2025-02-16 01:45:34 +0100

    shopping page layout and products grid

--- apps/app/src/components/ShoppingItem/ShoppingItem.tsx
+++ apps/app/src/components/ShoppingItem/ShoppingItem.tsx
@@ -1,12 +1,15 @@
+import React from 'react';
 import styles from './ShoppingItem.module.scss';
 import StarIcon from '@/assets/icons/rating-star-1.svg';
 import MugImg from '@/assets/images/coffee-mug.png';
 
-const ShoppingItem = () => {
+const ShoppingItem: React.FC = () => {
   return (
     <div className={styles.product}>
-      <div className={styles.dottedBorderHorizontal} />
-      <div className={styles.dottedBorderVertical} />
+      <div className={styles.dottedBorderTopHorizontal} />
+      <div className={styles.dottedBorderLeftVertical} />
+      <div className={styles.dottedBorderBottomHorizontal} />
+      <div className={styles.dottedBorderRightVertical} />
       <img src={MugImg} alt='' />
       <h3>Gucci dump days majica</h3>
       <p className={styles.productSupply}>Na zalihama: 4</p>
