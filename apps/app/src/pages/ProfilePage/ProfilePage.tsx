356b8a0e123f4e5a074e9e0b196d9a62af4c4ce9
Lovre Tomić <tomiclovre05@gmail.com>
2025-02-17 15:37:24 +0100

    added first content

--- apps/app/src/pages/ProfilePage/ProfilePage.tsx
+++ apps/app/src/pages/ProfilePage/ProfilePage.tsx
@@ -1,7 +1,18 @@
+import c from './ProfilePage.module.scss';
+
 export const ProfilePage = () => {
   return (
-    <div>
-      <h1>Profile</h1>
-    </div>
+    <>
+      <header className={c.header}>
+        <div className={c.basicInfoWrapper}>
+          <p className={c.title}>
+            <span>Profil</span> <br />
+            Marija Gudelj
+          </p>
+        </div>
+        <div className={c.statsWrapper}></div>
+      </header>
+      <main className={c.main}></main>
+    </>
   );
 };
