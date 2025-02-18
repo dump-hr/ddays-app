72cb98b6467fe67e552f197fe60973d0f130b3b5
ToniGrbic <tonigrbic.5@gmail.com>
2025-02-19 00:25:59 +0100

    ShoppingHeader desktop and responsiveness

--- apps/app/src/pages/ShoppingPage/sections/ShoppingHeader/ShoppingHeader.tsx
+++ apps/app/src/pages/ShoppingPage/sections/ShoppingHeader/ShoppingHeader.tsx
@@ -1,17 +1,33 @@
+import { useEffect, useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 import styles from './ShoppingHeader.module.scss';
 import CrossIcon from '@/assets/icons/cross-icon.svg';
 import TransactionsIcon from '@/assets/icons/transactions-icon.svg';
 import ShoppingCartIcon from '@/assets/icons/shopping-cart-icon.svg';
 import { HeaderCard } from '../../../../components/Header/HeaderCard/HeaderCard';
-import { useNavigate } from 'react-router-dom';
 import { RouteNames } from '../../../../router/routes';
+import { useDeviceType } from '../../../../hooks/UseDeviceType';
 
 const ShoppingHeader = () => {
+  const [headerCardWidth, setHeaderCardWidth] = useState(168);
+  const [headerCardHeight, setHeaderCardHeight] = useState(110);
+  
+  const { isMobile } = useDeviceType({ breakpoint: 769 });
   const navigate = useNavigate();
 
   const navigateHome = () => {
     navigate(RouteNames.HOME);
   };
+
+  useEffect(() => {
+    if (isMobile) {
+      setHeaderCardWidth(120);
+      setHeaderCardHeight(78);
+    } else {
+      setHeaderCardWidth(136);
+      setHeaderCardHeight(136);
+    }
+  }, [isMobile]);
 
   return (
     <div className={styles.headerContainer}>
@@ -23,16 +39,16 @@ const ShoppingHeader = () => {
         <HeaderCard
           img={TransactionsIcon}
           text='Transakcije'
-          width={168}
-          height={110}
+          width={headerCardWidth}
+          height={headerCardHeight}
           imgHeight={78}
           imgWidth={44}
         />
         <HeaderCard
           img={ShoppingCartIcon}
           text='Košarica'
-          width={168}
-          height={110}
+          width={headerCardWidth}
+          height={headerCardHeight}
           imgHeight={78}
           imgWidth={44}
         />
