6b4bd139335aa922b95109770061976feb0bbb99
ToniGrbic <tonigrbic.5@gmail.com>
2025-02-19 23:42:23 +0100

    disabled buttons logic and style

--- apps/app/src/components/ShoppingItem/ShoppingItem.tsx
+++ apps/app/src/components/ShoppingItem/ShoppingItem.tsx
@@ -1,16 +1,27 @@
 import React from 'react';
 import styles from './ShoppingItem.module.scss';
 import StarIcon from '@/assets/icons/rating-star-1.svg';
+import StarIconGrey from '@/assets/icons/rating-star-disabled.svg';
 import { ShopItemDto } from '@ddays-app/types/src/dto/shop';
 import { ShopItemType } from '@ddays-app/types/src/enum';
 import { getShopItemImgFromType } from '../../helpers/getShopItemImgFromType';
 
 interface ShoppingItemProps {
   isInCart: boolean;
+  userPointsAmount: number;
   product: ShopItemDto;
 }
 
-const ShoppingItem: React.FC<ShoppingItemProps> = ({ isInCart, product }) => {
+const ShoppingItem: React.FC<ShoppingItemProps> = ({
+  isInCart,
+  product,
+  userPointsAmount,
+}) => {
+  const outOfStock = product.quantity === 0;
+  const notEnoughPoints = userPointsAmount < product.price;
+
+  const disabled = isInCart || outOfStock || notEnoughPoints;
+  
   return (
     <div className={styles.product}>
       <div className={styles.dottedBorderTopHorizontal} />
@@ -21,13 +32,27 @@ const ShoppingItem: React.FC<ShoppingItemProps> = ({ isInCart, product }) => {
       <img
         className={styles.productImage}
         src={getShopItemImgFromType(product.type as ShopItemType)}
-        alt=''
+        alt='shop-item'
       />
       <h3>{product.itemName}</h3>
-      <p className={styles.productSupply}>Na zalihama: {product.quantity}</p>
-      <div className={styles.addToCartButton}>
-        <img src={StarIcon} alt='' />
-        <span className={styles.pointsText}>{product.price}</span>
+      <p
+        style={disabled ? { color: '#B3B3B2' } : {}}
+        className={styles.productSupply}>
+        Na zalihama: {product.quantity}
+      </p>
+      <div
+        aria-disabled={disabled}
+        className={`${styles.addToCartButton} ${
+          disabled ? styles.addToCartButtonDisabled : ''
+        }`}>
+        <img
+          className={styles.ratingStar}
+          src={disabled ? StarIconGrey : StarIcon}
+          alt=''
+        />
+        <span className={styles.pointsText}>
+          {!isInCart ? product.price : 'MAX. 1'}
+        </span>
       </div>
     </div>
   );
