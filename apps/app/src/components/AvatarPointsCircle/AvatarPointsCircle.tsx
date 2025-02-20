1e467f11828bdd545037f7fdf8fd98d2cdf8db87
Lovre Tomić <tomiclovre05@gmail.com>
2025-02-20 12:07:08 +0100

    added structure

--- apps/app/src/components/AvatarPointsCircle/AvatarPointsCircle.tsx
+++ apps/app/src/components/AvatarPointsCircle/AvatarPointsCircle.tsx
@@ -1,13 +1,26 @@
-//import { getLevelFromPoints } from '../../helpers/getLevelFromPoints';
+import { getLevelFromPoints } from '../../helpers/getLevelFromPoints';
+import c from './AvatarPointsCircle.module.scss';
 
 type AvatarPointsCircleProps = {
   points: number;
   avatar: string;
 };
 
-const AvatarPointsCircle: React.FC<AvatarPointsCircleProps> = ({ points }) => {
-  // const levelStats = getLevelFromPoints(points);
-  return <>{points}</>;
+const AvatarPointsCircle: React.FC<AvatarPointsCircleProps> = ({
+  points,
+  avatar,
+}) => {
+  const levelStats = getLevelFromPoints(points);
+  return (
+    <div className={c.outerCircle}>
+      <div className={c.innerCircle}>
+        <img src={avatar} alt='' />
+        <div className={c.levelWrapper}>
+          <p className={c.level}>{levelStats.level}</p>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default AvatarPointsCircle;
