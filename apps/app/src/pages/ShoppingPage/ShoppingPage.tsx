3a16f33fffbb5105a976bf9885278d347bd5aac1
ToniGrbic <tonigrbic.5@gmail.com>
2025-02-20 17:54:12 +0100

    fix default value

--- apps/app/src/pages/ShoppingPage/ShoppingPage.tsx
+++ apps/app/src/pages/ShoppingPage/ShoppingPage.tsx
@@ -4,7 +4,7 @@ import ShoppingHeader from './sections/ShoppingHeader';
 import { useState } from 'react';
 
 export const ShoppingPage = () => {
-  const [numItemsInCart, setNumItemsInCart] = useState(1);
+  const [numItemsInCart, setNumItemsInCart] = useState(0);
   return (
     <div className={styles.wrapper}>
       <ShoppingHeader numItemsInCart={numItemsInCart} />
