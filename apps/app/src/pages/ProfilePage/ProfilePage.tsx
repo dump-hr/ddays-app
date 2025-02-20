ce5d3363d5c327e9b835f27c457215d22238ec55
Lovre Tomić <tomiclovre05@gmail.com>
2025-02-20 14:52:44 +0100

    added navigation items

--- apps/app/src/pages/ProfilePage/ProfilePage.tsx
+++ apps/app/src/pages/ProfilePage/ProfilePage.tsx
@@ -2,6 +2,46 @@ import AvatarPointsCircle from '../../components/AvatarPointsCircle/AvatarPoints
 import c from './ProfilePage.module.scss';
 import TempAvatar from '../../assets/images/temp-avatar.png';
 import ProfileStat from '../../components/ProfileStat';
+import Like from '../../assets/icons/like.svg';
+import Award from '../../assets/icons/award.svg';
+import Bag from '../../assets/icons/bag.svg';
+import Trophy from '../../assets/icons/trophy.svg';
+import Gift from '../../assets/icons/gift.svg';
+import UserEdit from '../../assets/icons/user-edit.svg';
+import ProfileNavigationButton from '../../components/ProfileNavigationButton';
+
+const navigationItems = [
+  {
+    icon: Like,
+    label: 'Moji interesi',
+    href: '',
+  },
+  {
+    icon: Award,
+    label: 'Moja postignuća',
+    href: '',
+  },
+  {
+    icon: Bag,
+    label: 'Avatari',
+    href: '',
+  },
+  {
+    icon: Trophy,
+    label: 'Leaderboard',
+    href: '',
+  },
+  {
+    icon: Gift,
+    label: 'Nagrade',
+    href: '',
+  },
+  {
+    icon: UserEdit,
+    label: 'Postavke profila',
+    href: '',
+  },
+];
 
 export const ProfilePage = () => {
   return (
@@ -20,7 +60,19 @@ export const ProfilePage = () => {
           </div>
         </div>
       </header>
-      <main className={c.main}></main>
+      <main className={c.main}>
+        <div className={c.navigationWrapper}>
+          {navigationItems.map((item, index) => (
+            <ProfileNavigationButton
+              key={index}
+              icon={item.icon}
+              label={item.label}
+              href={item.href}
+            />
+          ))}
+        </div>
+        <div className={c.buttonsWrapper}></div>
+      </main>
     </>
   );
 };
