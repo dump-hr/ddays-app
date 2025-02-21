9596f8547990807c45a1caa905719d7371777cd8
ToniGrbic <tonigrbic.5@gmail.com>
2025-02-21 02:12:15 +0100

    logic to display ShoppingWelcome

--- apps/app/src/pages/ShoppingPage/ShoppingPage.tsx
+++ apps/app/src/pages/ShoppingPage/ShoppingPage.tsx
@@ -1,14 +1,33 @@
 import styles from './ShoppingPage.module.scss';
 import ShoppingItems from './sections/ShoppingItems';
 import ShoppingHeader from './sections/ShoppingHeader';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
+import ShoppingWelcome from './sections/ShoppingWelcome';
 
 export const ShoppingPage = () => {
   const [numItemsInCart, setNumItemsInCart] = useState(0);
+  const [firstShopVisit, setFirstShopVisit] = useState(false);
+
+  useEffect(() => {
+    if (!JSON.parse(localStorage.getItem('firstShopVisit') || 'false')) {
+      localStorage.setItem('firstShopVisit', 'true');
+      setFirstShopVisit(true);
+    } else {
+      localStorage.setItem('firstShopVisit', 'false');
+      setFirstShopVisit(false);
+    }
+  }, []);
+
   return (
     <div className={styles.wrapper}>
-      <ShoppingHeader numItemsInCart={numItemsInCart} />
-      <ShoppingItems setNumItemsInCart={setNumItemsInCart} />
+      {firstShopVisit ? (
+        <ShoppingWelcome />
+      ) : (
+        <>
+          <ShoppingHeader numItemsInCart={numItemsInCart} />
+          <ShoppingItems setNumItemsInCart={setNumItemsInCart} />
+        </>
+      )}
     </div>
   );
 };
