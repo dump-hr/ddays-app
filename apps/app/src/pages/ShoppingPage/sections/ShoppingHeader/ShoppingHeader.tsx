5ad0ff7cdccb05b7ae9f37a4772909e09abd7e0f
ToniGrbic <tonigrbic.5@gmail.com>
2025-02-21 02:12:39 +0100

    add NavigateHomeButton component

--- apps/app/src/pages/ShoppingPage/sections/ShoppingHeader/ShoppingHeader.tsx
+++ apps/app/src/pages/ShoppingPage/sections/ShoppingHeader/ShoppingHeader.tsx
@@ -1,12 +1,11 @@
 import { useEffect, useState } from 'react';
-import { useNavigate } from 'react-router-dom';
 import styles from './ShoppingHeader.module.scss';
-import CrossIcon from '@/assets/icons/cross-icon.svg';
 import TransactionsIcon from '@/assets/icons/transactions-icon.svg';
 import ShoppingCartIcon from '@/assets/icons/shopping-cart-icon.svg';
-import { HeaderCard } from '../../../../components/Header/HeaderCard/HeaderCard';
-import { RouteNames } from '../../../../router/routes';
+
 import { useDeviceType } from '../../../../hooks/UseDeviceType';
+import { HeaderCard } from '../../../../components/Header/HeaderCard/HeaderCard';
+import NavigateHomeButton from '../../../../components/NavigateHomeButton';
 
 interface ShoppingHeaderProps {
   numItemsInCart: number;
@@ -16,11 +15,6 @@ const ShoppingHeader: React.FC<ShoppingHeaderProps> = ({ numItemsInCart }) => {
   const [headerCardWidth, setHeaderCardWidth] = useState(168);
   const [headerCardHeight, setHeaderCardHeight] = useState(110);
   const { isMobile, isSmallMobile } = useDeviceType({ mobileBreakpoint: 769 });
-  const navigate = useNavigate();
-
-  const navigateHome = () => {
-    navigate(RouteNames.HOME);
-  };
 
   useEffect(() => {
     if (isMobile) {
@@ -39,7 +33,7 @@ const ShoppingHeader: React.FC<ShoppingHeaderProps> = ({ numItemsInCart }) => {
     <div className={styles.headerContainer}>
       <div className={styles.header}>
         <h1 className={styles.title}>SHOPPING</h1>
-        <img src={CrossIcon} alt='' onClick={navigateHome} />
+        <NavigateHomeButton />
       </div>
       <div className={styles.navContainer}>
         <div className={styles.navigation}>
