00b9d91b8563666a6dfaa8420f62ebb29036fdb8
Lovre Tomić <tomiclovre05@gmail.com>
2025-02-21 11:45:25 +0100

    improve responsiveness

--- apps/app/src/components/RecommendationsButton/RecommendationsButton.tsx
+++ apps/app/src/components/RecommendationsButton/RecommendationsButton.tsx
@@ -2,6 +2,8 @@ import clsx from 'clsx';
 import Arrow from '../../assets/icons/gray-arrow.svg';
 import Duck from '../../assets/images/duck.png';
 import c from './RecommendationsButton.module.scss';
+import { useEffect, useRef, useState } from 'react';
+import useRefDimensions from '../../hooks/useRefDimensions';
 
 type RecommendationsButtonProps = React.HTMLAttributes<HTMLDivElement>;
 
@@ -9,10 +11,27 @@ const RecommendationsButton: React.FC<RecommendationsButtonProps> = ({
   className,
   ...handlers
 }) => {
+  const [isCompact, setIsCompact] = useState(false);
+
+  const buttonRef = useRef(null);
+  const dimensions = useRefDimensions(buttonRef);
+  console.log(dimensions);
+
+  useEffect(() => {
+    if (dimensions.width < 400) {
+      setIsCompact(true);
+    } else {
+      setIsCompact(false);
+    }
+  }, [dimensions]);
+
   return (
-    <div className={clsx(c.button, className)} {...handlers}>
+    <div
+      className={clsx(c.button, className, { [c.compact]: isCompact })}
+      {...handlers}
+      ref={buttonRef}>
       <p className={c.text}>
-        Preporuke samo za tebe
+        Preporuke samo za tebe {dimensions.width}
         <img className={c.arrow} src={Arrow} alt='' />
       </p>
       <img className={c.duck} src={Duck} alt='' />
