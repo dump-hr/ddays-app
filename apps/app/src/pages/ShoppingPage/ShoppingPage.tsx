28a5273b6c2906a05f0b6032476d47727c2fcfbe
ToniGrbic <tonigrbic.5@gmail.com>
2025-02-21 17:44:25 +0100

    isMounted Ref  for bugfix in useEffect

--- apps/app/src/pages/ShoppingPage/ShoppingPage.tsx
+++ apps/app/src/pages/ShoppingPage/ShoppingPage.tsx
@@ -1,14 +1,17 @@
 import styles from './ShoppingPage.module.scss';
 import ShoppingItems from './sections/ShoppingItems';
 import ShoppingHeader from './sections/ShoppingHeader';
-import { useState, useEffect } from 'react';
+import { useState, useEffect, useRef } from 'react';
 import ShoppingWelcome from './sections/ShoppingWelcome';
 
 export const ShoppingPage = () => {
   const [numItemsInCart, setNumItemsInCart] = useState(0);
   const [firstShopVisit, setFirstShopVisit] = useState(false);
+  const isMounted = useRef(false);
 
   useEffect(() => {
+    if (isMounted.current) return;
+
     if (!JSON.parse(localStorage.getItem('firstShopVisit') || 'false')) {
       localStorage.setItem('firstShopVisit', 'true');
       setFirstShopVisit(true);
@@ -16,12 +19,14 @@ export const ShoppingPage = () => {
       localStorage.setItem('firstShopVisit', 'false');
       setFirstShopVisit(false);
     }
-  }, []);
+
+    isMounted.current = true;
+  }, [isMounted]);
 
   return (
     <div className={styles.wrapper}>
       {firstShopVisit ? (
-        <ShoppingWelcome />
+        <ShoppingWelcome setFirstShopVisit={setFirstShopVisit} />
       ) : (
         <>
           <ShoppingHeader numItemsInCart={numItemsInCart} />
