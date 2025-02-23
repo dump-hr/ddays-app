81c8836da838a1fd6fc340450b6089437cdbe3da
SimeJadrijev <sime.jadrijev@gmail.com>
2025-02-23 23:41:48 +0100

    fix register page slice

--- apps/app/src/pages/RegisterPage/RegisterPage.tsx
+++ apps/app/src/pages/RegisterPage/RegisterPage.tsx
@@ -1,11 +1,16 @@
 import { GeneralRegistrationForm } from '../../components/RegistrationForms/GeneralRegistrationForm/GeneralRegistrationForm';
+import { useDeviceType } from '../../hooks/UseDeviceType';
 import { RegistrationProvider } from '../../providers/RegistrationContext';
 import c from './RegisterPage.module.scss';
+import closeIcon from './../../assets/icons/remove-icon.svg';
 
 export const RegisterPage = () => {
+  const { isMobile } = useDeviceType({});
   return (
     <section className={c.page}>
-      <header className={c.header}></header>
+      <header className={c.header}>
+        <h2>REGISTRACIJA {!isMobile && <img src={closeIcon}></img>}</h2>
+      </header>
       <main className={c.main}>
         <RegistrationProvider>
           <GeneralRegistrationForm />
