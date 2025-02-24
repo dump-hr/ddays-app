4db162930c2868c25e513139c41d604b1de2754b
ToniGrbic <tonigrbic.5@gmail.com>
2025-02-24 18:35:05 +0100

    removed smallMobileBreakpoint

--- apps/app/src/hooks/UseDeviceType.ts
+++ apps/app/src/hooks/UseDeviceType.ts
@@ -1,24 +1,20 @@
 import { useState, useEffect } from 'react';
 
 enum DeviceType {
-  SMALL_MOBILE = 'small-mobile',
   MOBILE = 'mobile',
   DESKTOP = 'desktop',
 }
 
 type UseDeviceReturn = {
   isMobile: boolean;
-  isSmallMobile: boolean;
 };
 
 type UseDeviceProps = {
-  mobileBreakpoint?: number;
-  smallMobileBreakpoint?: number;
+  breakpoint?: number;
 };
 
 export const useDeviceType = ({
-  mobileBreakpoint = 769,
-  smallMobileBreakpoint = 370,
+  breakpoint = 769,
 }: UseDeviceProps): UseDeviceReturn => {
   const [deviceType, setDeviceType] = useState<DeviceType>(DeviceType.DESKTOP);
 
@@ -26,13 +22,9 @@ export const useDeviceType = ({
     if (typeof window === 'undefined') return;
 
     const checkDeviceType = () => {
-      if (window.innerWidth < smallMobileBreakpoint) {
-        setDeviceType(DeviceType.SMALL_MOBILE);
-      } else if (window.innerWidth < mobileBreakpoint) {
-        setDeviceType(DeviceType.MOBILE);
-      } else {
-        setDeviceType(DeviceType.DESKTOP);
-      }
+      const type =
+        window.innerWidth < breakpoint ? DeviceType.MOBILE : DeviceType.DESKTOP;
+      setDeviceType(type);
     };
 
     checkDeviceType();
@@ -42,10 +34,9 @@ export const useDeviceType = ({
     return () => {
       window.removeEventListener('resize', checkDeviceType);
     };
-  }, [mobileBreakpoint, smallMobileBreakpoint]);
+  }, [breakpoint]);
 
   return {
     isMobile: deviceType === DeviceType.MOBILE,
-    isSmallMobile: deviceType === DeviceType.SMALL_MOBILE,
   };
 };
