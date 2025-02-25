e6072e1fabed958621089c3892ff4d16ad1d469d
SimeJadrijev <sime.jadrijev@gmail.com>
2025-02-25 21:44:20 +0100

    add redirect from confirm-email page and fix design slightly

--- apps/app/src/components/RegistrationForms/ConfirmEmail/ConfirmEmail.tsx
+++ apps/app/src/components/RegistrationForms/ConfirmEmail/ConfirmEmail.tsx
@@ -2,13 +2,20 @@ import { useDeviceType } from '../../../hooks/UseDeviceType';
 import Button from '../../Button/Button';
 import c from './ConfirmEmail.module.scss';
 import closeIcon from './../../../assets/icons/remove-icon-black.svg';
+import { useNavigate } from 'react-router-dom';
 
 export const ConfirmEmail = () => {
   const { isMobile } = useDeviceType({});
+  const navigate = useNavigate();
   return (
     <div className={c.confirmEmail}>
       <div className={c.confirmEmailUpper}>
-        <h2>Potvrdi svoj e-mail {isMobile && <img src={closeIcon}></img>}</h2>
+        <h2>
+          Potvrdi svoj e-mail{' '}
+          {isMobile && (
+            <img src={closeIcon} onClick={() => navigate('/app')}></img>
+          )}
+        </h2>
         <p>
           Poslali smo ti e-mail na mikejla@dump.hr da potvrdimo ispravnost
           unesene adrese. Nakon što se e-mail pojavi u tvom sandučiću, klikni na
