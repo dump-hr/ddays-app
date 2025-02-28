c9c4ac70cd9c50c8dc563efcca1fb04fb2067f66
andrea-zecevic <azecev01@gmail.com>
2025-03-01 00:00:09 +0100

    Add style, width and height props

--- apps/app/src/components/StarRating/StarRating.tsx
+++ apps/app/src/components/StarRating/StarRating.tsx
@@ -3,12 +3,22 @@ import styles from './StarRating.module.scss';
 import RatingStar from '../../assets/icons/star-rating-1-full.svg';
 import EmptyStar from '../../assets/icons/star-rating-1-empty.svg';
 
-const StarRating = () => {
+interface StarRatingProps {
+  style?: React.CSSProperties;
+  width?: string;
+  height?: string;
+}
+
+const StarRating: React.FC<StarRatingProps> = ({
+  style = {},
+  width = 'auto',
+  height = 'auto',
+}) => {
   const [rating, setRating] = useState(0);
   const [hover, setHover] = useState(0);
 
   return (
-    <div className={styles.container}>
+    <div className={styles.container} style={style}>
       {[1, 2, 3, 4, 5].map((star) => (
         <button
           key={star}
@@ -17,9 +27,19 @@ const StarRating = () => {
           onClick={() => setRating(star)}
           className={styles.starButton}>
           {star <= (hover || rating) ? (
-            <img src={RatingStar} className={styles.star} alt='filled star' />
+            <img
+              src={RatingStar}
+              className={styles.star}
+              alt='filled star'
+              style={{ width, height }}
+            />
           ) : (
-            <img src={EmptyStar} className={styles.star} alt='empty star' />
+            <img
+              src={EmptyStar}
+              className={styles.star}
+              alt='empty star'
+              style={{ width, height }}
+            />
           )}
         </button>
       ))}
