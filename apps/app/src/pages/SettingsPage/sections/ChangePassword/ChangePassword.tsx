07121c80c04106dc7df2f298b10b60aa055052a8
ToniGrbic <tonigrbic.5@gmail.com>
2025-03-09 23:35:11 +0100

    logic and state inputs update for change pass

--- apps/app/src/pages/SettingsPage/sections/ChangePassword/ChangePassword.tsx
+++ apps/app/src/pages/SettingsPage/sections/ChangePassword/ChangePassword.tsx
@@ -1,46 +1,66 @@
+import toast from 'react-hot-toast';
+import styles from './ChangePassword.module.scss';
 import Button from '../../../../components/Button';
 import { Input } from '../../../../components/Input';
-import styles from './ChangePassword.module.scss';
+import { useUserContext } from '../../../../context/UserContext';
+import { useInputHandlers } from '../../../../hooks/useInputHandlers';
 
 interface ChangePasswordProps {
-  isChangingPassword: boolean;
   setIsChangingPassword: (value: boolean) => void;
 }
 
 export const ChangePassword: React.FC<ChangePasswordProps> = ({
-  isChangingPassword,
   setIsChangingPassword,
 }) => {
+  const { userSettingsData, updateUserSettingsData } = useUserContext();
+  const { handleInputChange } = useInputHandlers(updateUserSettingsData);
+
   const handleSaveClick = () => {
+    // TODO: validacija trenutne lozinke sa backend
+    if (
+      userSettingsData['newPassword'] !== userSettingsData['repeatedPassword']
+    ) {
+      toast.error('Lozinke se ne podudaraju, pokušaj ponovo!');
+      return;
+    }
+
+    //TODO izmjena lozinke sa backend
+    setPasswordInputsToDefault();
+    toast.success('Podaci uspješno izmjenjeni!');
     setIsChangingPassword(false);
+  };
+
+  const setPasswordInputsToDefault = () => {
+    updateUserSettingsData({
+      password: '',
+      newPassword: '',
+      repeatedPassword: '',
+    });
   };
 
   return (
     <div className={styles.changePasswordsContainer}>
       <div className={styles.passwordsInputs}>
         <Input
-          name='currentPassword'
+          name='password'
           type='password'
           placeholder='Trenutna lozinka'
-          disabled={!isChangingPassword}
-          value={undefined}
-          onChange={() => {}}
+          value={userSettingsData['password']}
+          onChange={handleInputChange}
         />
         <Input
           name='newPassword'
           type='password'
           placeholder='Nova lozinka'
-          disabled={!isChangingPassword}
-          value={undefined}
-          onChange={() => {}}
+          value={userSettingsData['newPassword']}
+          onChange={handleInputChange}
         />
         <Input
-          name='confirmNewPassword'
+          name='repeatedPassword'
           type='password'
           placeholder='Potvrdi novu lozinku'
-          disabled={!isChangingPassword}
-          value={undefined}
-          onChange={() => {}}
+          value={userSettingsData['repeatedPassword']}
+          onChange={handleInputChange}
         />
       </div>
       <Button variant='black' onClick={handleSaveClick}>
