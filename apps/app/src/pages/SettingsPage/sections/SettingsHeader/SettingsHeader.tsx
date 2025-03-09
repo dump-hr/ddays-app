ed988d558cba013e209b4126e1f1b68c84d4c450
ToniGrbic <tonigrbic.5@gmail.com>
2025-03-09 20:24:42 +0100

    desktop functionality, reverting state for inputs when cancel edit

--- apps/app/src/pages/SettingsPage/sections/SettingsHeader/SettingsHeader.tsx
+++ apps/app/src/pages/SettingsPage/sections/SettingsHeader/SettingsHeader.tsx
@@ -1,7 +1,13 @@
 import clsx from 'clsx';
-import styles from '../../SettingsPage.module.scss';
+import styles from './SettingsHeader.module.scss';
+import { RouteNames } from '../../../../router/routes';
+
+import { useNavigate } from 'react-router-dom';
+import { useDeviceType } from '../../../../hooks/UseDeviceType';
+import { useUserContext } from '../../../../context/UserContext';
 
 import ArrowLeftIcon from '@/assets/icons/arrow-left.svg';
+import ArrowLeftWhiteIcon from '@/assets/icons/arrow-left-white.svg';
 import CloseIcon from '@/assets/icons/close-icon.svg';
 import EditIcon from '@/assets/icons/pencil.svg';
 
@@ -18,22 +24,36 @@ export const SettingsHeader: React.FC<SettingsHeaderProps> = ({
   setIsEditing,
   setIsChangingPassword,
 }) => {
+  const navigate = useNavigate();
+  const { isMobile } = useDeviceType({ breakpoint: 768 });
+  const { userData, updateUserSettingsData } = useUserContext();
+
   const handleEditClick = () => {
     if (isChangingPassword) {
       setIsChangingPassword(false);
       return;
     }
+    if (isEditing) updateUserSettingsData(userData);
     setIsEditing(!isEditing);
   };
 
-  const setHeaderTitle = () => {
-    if (isEditing) {
-      return 'UREDI PROFIL';
-    } else if (isChangingPassword) {
-      return 'PROMJENA LOZINKE';
-    } else {
-      return 'POSTAVKE PROFILA';
+  const handleBackClickMobile = () => {
+    navigate(RouteNames.PROFILE);
+  };
+
+  const handleBackClick = () => {
+    if (isEditing || isChangingPassword) {
+      setIsEditing(false);
+      setIsChangingPassword(false);
+      return;
     }
+    navigate(RouteNames.PROFILE);
+  };
+
+  const setHeaderTitle = () => {
+    if (isEditing) return 'Uredi profil';
+    if (isChangingPassword) return 'Promjena lozinke';
+    return 'Postavke profila';
   };
 
   return (
@@ -42,22 +62,34 @@ export const SettingsHeader: React.FC<SettingsHeaderProps> = ({
         styles.settingsHeader,
         (isEditing || isChangingPassword) && styles.settingsHeaderEdit,
       )}>
-      {!isEditing && !isChangingPassword && (
-        <div className={styles.backButton}>
+      {!isEditing && !isChangingPassword && isMobile && (
+        <div className={styles.backButton} onClick={handleBackClickMobile}>
           <img src={ArrowLeftIcon} alt='back' />
+        </div>
+      )}
+      {!isMobile && (
+        <div className={styles.backButtonDesktop} onClick={handleBackClick}>
+          <img src={ArrowLeftWhiteIcon} alt='back' />
         </div>
       )}
       <h1>{setHeaderTitle()}</h1>
       <div
         onClick={handleEditClick}
         className={clsx(
-          styles.editButton,
+          isMobile && styles.editButton,
+          !isMobile && styles.editButtonDesktop,
+          !isMobile && (isEditing || isChangingPassword) && styles.displayNone,
           (isEditing || isChangingPassword) && styles.noBackground,
         )}>
-        <img
-          src={!isEditing && !isChangingPassword ? EditIcon : CloseIcon}
-          alt='edit'
-        />
+        {!isEditing && !isChangingPassword && (
+          <>
+            <img src={EditIcon} alt='edit' />
+            {!isMobile && <span>UREDI SVOJE INTERESE</span>}
+          </>
+        )}
+        {(isEditing || isChangingPassword) && isMobile && (
+          <img src={CloseIcon} alt='close' />
+        )}
       </div>
     </div>
   );
