39917843ccf1b603826bf68d0fbbca4f76e76c36
ToniGrbic <tonigrbic.5@gmail.com>
2025-03-16 20:07:30 +0100

    styling, desktop responsiveness, props

--- apps/app/src/layout/PopupLayout/PopupLayout.tsx
+++ apps/app/src/layout/PopupLayout/PopupLayout.tsx
@@ -1,15 +1,38 @@
-import { ReactNode } from 'react';
+import { PropsWithChildren } from 'react';
 import styles from './PopupLayout.module.scss';
+import CloseIcon from '@/assets/icons/close-icon.svg';
+import clsx from 'clsx';
 
 interface PopupLayoutProps {
-  children: ReactNode;
+  imgSrc?: string;
+  headerTitle: string;
+  closePopup: () => void;
+  isOpen: boolean;
 }
 
-const PopupLayout = ({ children }: PopupLayoutProps) => {
+const PopupLayout = ({
+  children,
+  headerTitle,
+  imgSrc,
+  closePopup,
+  isOpen,
+}: PropsWithChildren<PopupLayoutProps>) => {
   return (
-    <div className={styles.wrapper}>
+    <div className={clsx(styles.wrapper, { [styles.closed]: !isOpen })}>
       <div className={styles.container}>
-        <div className={styles.heading}>{children}</div>
+        <div className={styles.heading}>
+          <h2>{headerTitle}</h2>
+          <img
+            src={CloseIcon}
+            onClick={closePopup}
+            className={styles.closeIcon}
+            alt=''
+          />
+        </div>
+        <div className={styles.popupImageContainer}>
+          <img className={styles.popupImage} src={imgSrc} />
+        </div>
+        <div className={styles.contentDiv}>{children}</div>
       </div>
     </div>
   );
