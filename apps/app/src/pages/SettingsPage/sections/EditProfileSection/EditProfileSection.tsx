39d9b03ee2da876e7aab486554a1d255de52947f
ToniGrbic <tonigrbic.5@gmail.com>
2025-03-16 02:26:26 +0100

    fix build error

--- apps/app/src/pages/SettingsPage/sections/EditProfileSection/EditProfileSection.tsx
+++ apps/app/src/pages/SettingsPage/sections/EditProfileSection/EditProfileSection.tsx
@@ -16,7 +16,7 @@ import { Checkbox } from '@/components/Checkbox';
 import { Input } from '@/components/Input';
 import { useRegistration } from '@/context/RegistrationContext';
 import { SettingsEdits } from '@/types/enums';
-import { validateField, allFieldsAreFilled } from '@/helpers/validateInput';
+import { validateField } from '@/helpers/validateInput';
 import { RegistrationFormErrors } from '@/types/errors/errors.dto';
 
 interface EditProfileSectionProps {
