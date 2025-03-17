09b0bbf95d25aa012ac7627968c9b719caab597d
andrea-zecevic <azecev01@gmail.com>
2025-03-17 17:21:58 +0100

    Email sent

--- apps/app/src/pages/PasswordResetPage/PasswordResetPage.tsx
+++ apps/app/src/pages/PasswordResetPage/PasswordResetPage.tsx
@@ -130,6 +130,47 @@ export const PasswordResetPage = () => {
             </div>
           </>
         )}
+        {step === 3 && (
+          <>
+            <div className={c.pageName}>
+              <span className={c.pageTitle}>Resetiraj lozinku</span>
+              <a href={RouteNames.LOGIN}>
+                <img
+                  src={closeIcon}
+                  alt='Close login'
+                  className={c.closeIcon}
+                />
+              </a>
+            </div>
+            <div className={c.duckContainer}>
+              <img src={duckImage} alt='Slavica' className={c.duckImage} />
+            </div>
+            <div className={c.container}>
+              <div className={c.titleContainer}>
+                <h1 className={c.title}>Poslan mail!</h1>
+                <a href={RouteNames.LOGIN}>
+                  <img
+                    src={closeIcon}
+                    alt='Close login'
+                    className={c.closeIcon}
+                  />
+                </a>
+              </div>
+              <div className={c.textContainer}>
+                <p className={c.text}>
+                  Poslala sam ti link za resetiranje lozinke na
+                  {email || 'tvoj email'}. Molim te da prije nastavljanja radnje
+                  pratiš upute u mailu i autoriziraš promjenu lozinke.
+                </p>
+              </div>
+              <div className={c.buttonContainer}>
+                <Button variant='orange' onClick={handleNextStep}>
+                  Resetiraj lozinku
+                </Button>
+              </div>
+            </div>
+          </>
+        )}
       </div>
     </div>
   );
