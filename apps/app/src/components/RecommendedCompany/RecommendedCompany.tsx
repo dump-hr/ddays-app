24140dffda3eae0ab6ca32f0941df34cc14ddf4f
Lovre Tomić <tomiclovre05@gmail.com>
2025-03-25 14:23:35 +0100

    added dottedbreak

--- apps/app/src/components/RecommendedCompany/RecommendedCompany.tsx
+++ apps/app/src/components/RecommendedCompany/RecommendedCompany.tsx
@@ -6,37 +6,42 @@ import BadgeGold from '../../assets/images/badge-gold.png';
 type RecommendedCompanyProps = {
   number: number;
   company: CompanyDto;
+  hasSeparator?: boolean;
 };
 
 const RecommendedCompany: React.FC<RecommendedCompanyProps> = ({
   number,
   company,
+  hasSeparator = true,
 }) => {
   return (
-    <div
-      className={clsx({
-        [c.recommendedCompany]: true,
-        [c.first]: number === 1,
-      })}>
-      <div className={c.numberWrapper}>
-        {number === 1 ? (
-          <img src={BadgeGold} alt='Gold badge' />
-        ) : (
-          <p>0{number}</p>
-        )}
-      </div>
-      <div className={c.content}>
-        <h3 className={c.companyName}>{company.name}</h3>
-        <div className={c.interestsWrapper}>
-          {company.interests &&
-            company.interests.map((interest, i) => (
-              <span key={i} className={c.interest}>
-                {interest.name}
-              </span>
-            ))}
+    <>
+      <div
+        className={clsx({
+          [c.recommendedCompany]: true,
+          [c.first]: number === 1,
+        })}>
+        <div className={c.numberWrapper}>
+          {number === 1 ? (
+            <img src={BadgeGold} alt='Gold badge' />
+          ) : (
+            <p>0{number}</p>
+          )}
+        </div>
+        <div className={c.content}>
+          <h3 className={c.companyName}>{company.name}</h3>
+          <div className={c.interestsWrapper}>
+            {company.interests &&
+              company.interests.map((interest, i) => (
+                <span key={i} className={c.interest}>
+                  {interest.name}
+                </span>
+              ))}
+          </div>
         </div>
       </div>
-    </div>
+      {hasSeparator && number !== 1 && <div className={c.separator}></div>}
+    </>
   );
 };
 
