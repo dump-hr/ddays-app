3e177fc61cf0371908e934b27a14747391b6b70a
Lovre Tomić <tomiclovre05@gmail.com>
2025-03-25 09:52:46 +0100

    added text styles

--- apps/app/src/pages/RecommendationsPage/RecommendationsPage.tsx
+++ apps/app/src/pages/RecommendationsPage/RecommendationsPage.tsx
@@ -7,8 +7,13 @@ const RecommendationsPage = () => {
       <main className={c.content}>
         <header className={c.header}>
           <img className={c.arrow} src={ArrowLeft} />
-          <h3 className={c.title}>Preporuke</h3>
+          <h3 className={c.pageTitle}>Preporuke</h3>
         </header>
+        <h3 className={c.title}>Tvrtke</h3>
+        <p className={c.paragraph}>
+          Na temelju tvojih interesa dajemo ti listu kurceva koja ti najviše
+          odgovaraju.
+        </p>
       </main>
     </div>
   );
