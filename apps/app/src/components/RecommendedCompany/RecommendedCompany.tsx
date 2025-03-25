5cb50528bb8564358039890046a4199fad214e54
Lovre Tomić <tomiclovre05@gmail.com>
2025-03-25 09:58:49 +0100

    added recommendedcompany component body

---
+++ apps/app/src/components/RecommendedCompany/RecommendedCompany.tsx
@@ -0,0 +1,31 @@
+import { CompanyDto } from '@ddays-app/types';
+import c from './RecommendedCompany.module.scss';
+
+type RecommendedCompanyProps = {
+  number: number;
+  company: CompanyDto;
+};
+
+const RecommendedCompany: React.FC<RecommendedCompanyProps> = ({
+  number,
+  company,
+}) => {
+  return (
+    <div className={c.recommendedCompany}>
+      <div className={c.numberWrapper}>
+        <p>0{number}</p>
+      </div>
+      <div className={c.content}>
+        <h3 className={c.companyName}>{company.name}</h3>
+        {company.interests &&
+          company.interests.map((interest, i) => (
+            <span key={i} className={c.interest}>
+              {interest.name}
+            </span>
+          ))}
+      </div>
+    </div>
+  );
+};
+
+export default RecommendedCompany;
