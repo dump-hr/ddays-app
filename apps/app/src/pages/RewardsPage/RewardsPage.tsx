8681bee5f0a92197465e2458b1117bc0c6e4d95a
SimeJadrijev <sime.jadrijev@gmail.com>
2025-03-29 19:39:06 +0100

    add buttons functionalities

--- apps/app/src/pages/RewardsPage/RewardsPage.tsx
+++ apps/app/src/pages/RewardsPage/RewardsPage.tsx
@@ -3,19 +3,31 @@ import ReturnIcon from '@/assets/icons/arrow-left-white.svg';
 import CloseIcon from '@/assets/icons/close-icon.svg';
 import { Reward } from '@/components/Reward';
 import { useDeviceType } from '@/hooks/UseDeviceType';
+import { useNavigate } from 'react-router-dom';
 
 export const RewardsPage = () => {
   const { isMobile } = useDeviceType({});
+  const navigate = useNavigate();
 
   return (
     <div className={c.page}>
-      {!isMobile && <img src={ReturnIcon} alt='return icon' />}
+      {!isMobile && (
+        <img
+          src={ReturnIcon}
+          alt='return icon'
+          onClick={() => navigate('/app/profile')}
+        />
+      )}
 
       <main className={c.main}>
         {isMobile ? (
           <header>
             <h2>Nagrade</h2>
-            <img src={CloseIcon} alt='close icon' />
+            <img
+              src={CloseIcon}
+              alt='close icon'
+              onClick={() => navigate('/app/profile')}
+            />
           </header>
         ) : (
           <h2>Nagrade</h2>
