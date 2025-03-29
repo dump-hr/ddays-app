da7e4c7e35f6fd07e3677db7663558b60581d02e
SimeJadrijev <sime.jadrijev@gmail.com>
2025-03-29 19:33:41 +0100

    slice mobile design

--- apps/app/src/pages/RewardsPage/RewardsPage.tsx
+++ apps/app/src/pages/RewardsPage/RewardsPage.tsx
@@ -1,13 +1,25 @@
 import c from './RewardsPage.module.scss';
 import ReturnIcon from '@/assets/icons/arrow-left-white.svg';
+import CloseIcon from '@/assets/icons/close-icon.svg';
 import { Reward } from '@/components/Reward';
+import { useDeviceType } from '@/hooks/UseDeviceType';
 
 export const RewardsPage = () => {
+  const { isMobile } = useDeviceType({});
+
   return (
     <div className={c.page}>
-      <img src={ReturnIcon} alt='return icon' />
+      {!isMobile && <img src={ReturnIcon} alt='return icon' />}
+
       <main className={c.main}>
-        <h2>Nagrade</h2>
+        {isMobile ? (
+          <header>
+            <h2>Nagrade</h2>
+            <img src={CloseIcon} alt='close icon' />
+          </header>
+        ) : (
+          <h2>Nagrade</h2>
+        )}
         <p>
           Osvoji negrade hihi Osvoji negrade hihiOsvoji negrade hihiOsvoji
           negrade hihiOsvoji negrade hihiOsvoji negrade hihiOsvoji negrade
