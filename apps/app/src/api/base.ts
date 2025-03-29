65238c40df1b6b338801e6c738bff0c8ca8bf9dc
ToniGrbic <tonigrbic.5@gmail.com>
2025-03-29 03:00:07 +0100

    fix

--- apps/app/src/api/base.ts
+++ apps/app/src/api/base.ts
@@ -11,7 +11,7 @@ const axiosInstance = axios.create(requestConfig);
 
 // odkometirat za produkciju
 /* axiosInstance.interceptors.request.use(async (config) => {
-  const token = localStorage.getItem('sponsorAccessToken');
+  const token = localStorage.getItem('accessToken');
 
   if (token) {
     config.headers.Authorization = `Bearer ${token}`;
