fdf7d56d2a572fc71e94773a69e1a0c0faaeb926
SimeJadrijev <sime.jadrijev@gmail.com>
2025-03-31 14:07:07 +0200

    use dotted border

--- apps/app/src/components/Reward/Reward.tsx
+++ apps/app/src/components/Reward/Reward.tsx
@@ -4,6 +4,10 @@ import AwardImage from '@/assets/images/macbook.png';
 export const Reward = () => {
   return (
     <div className={c.reward}>
+      <div className={c.dottedBorderTopHorizontal} />
+      <div className={c.dottedBorderLeftVertical} />
+      <div className={c.dottedBorderBottomHorizontal} />
+      <div className={c.dottedBorderRightVertical} />
       <img src={AwardImage} alt='award image' />
       <h3>Laptop macbook</h3>
     </div>
