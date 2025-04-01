bb7549d3b956e16e5656159c7fb096bcd65a8cf3
andrea-zecevic <azecev01@gmail.com>
2025-04-01 17:32:51 +0200

    Add rating questions

--- apps/app/src/pages/RateCompanyPage/RateCompanyPage.tsx
+++ apps/app/src/pages/RateCompanyPage/RateCompanyPage.tsx
@@ -2,6 +2,7 @@ import c from './RateCompanyPage.module.scss';
 import closeIcon from '../../assets/icons/close-icon.svg';
 import Button from '../../components/Button';
 import { RouteNames } from '../../router/routes';
+import StarRating from '../../components/StarRating';
 import HRCloudLogo from '../../assets/images/HRCloud.svg';
 
 export const RateCompanyPage = () => {
@@ -22,9 +23,32 @@ export const RateCompanyPage = () => {
           </div>
           <div className={c.companyDetails}>
             <img src={HRCloudLogo} alt='Company logo' className={c.logo} />
-            <text className={c.companyLocationAtConference}>Z4</text>
+            <span className={c.companyLocationAtConference}>Z4</span>
           </div>
           <div className={c.breakline}></div>
+          <div className={c.ratingContainer}>
+            <div className={c.ratingQuestion}>
+              <h3>Generalni dojam štanda</h3>
+              <span className={c.ratingQuestionText}>
+                Kakav je dojam štand ostavio na tebe?
+              </span>
+              <StarRating style={{ marginTop: '16px' }} />
+            </div>
+            <div className={c.ratingQuestion}>
+              <h3>Sadrzaj štanda</h3>
+              <span className={c.ratingQuestionText}>
+                Što se sve nudi? Je li ti bilo zabavno?
+              </span>
+              <StarRating style={{ marginTop: '16px' }} />
+            </div>
+            <div className={c.ratingQuestion}>
+              <h3>Izlagači</h3>
+              <span className={c.ratingQuestionText}>
+                Ocjeni svoj dojam o izlagačima.
+              </span>
+              <StarRating style={{ marginTop: '16px' }} />
+            </div>
+          </div>
 
           <div className={c.buttonContainer}>
             <Button
