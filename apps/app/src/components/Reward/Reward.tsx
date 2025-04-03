34a86c97eeaa28c1c329af5fd77c3b9dc1335b5b
SimeJadrijev <sime.jadrijev@gmail.com>
2025-04-03 20:56:14 +0200

    use props for reward component

--- apps/app/src/components/Reward/Reward.tsx
+++ apps/app/src/components/Reward/Reward.tsx
@@ -1,15 +1,18 @@
 import c from './Reward.module.scss';
-import AwardImage from '@/assets/images/macbook.png';
 
-export const Reward = () => {
+type RewardProps = {
+  name: string;
+  image: string;
+};
+export const Reward = ({ name, image }: RewardProps) => {
   return (
     <div className={c.reward}>
       <div className={c.dottedBorderTopHorizontal} />
       <div className={c.dottedBorderLeftVertical} />
       <div className={c.dottedBorderBottomHorizontal} />
       <div className={c.dottedBorderRightVertical} />
-      <img src={AwardImage} alt='award image' />
-      <h3>Laptop macbook</h3>
+      <img src={image} alt={name} />
+      <h3>{name}</h3>
     </div>
   );
 };
