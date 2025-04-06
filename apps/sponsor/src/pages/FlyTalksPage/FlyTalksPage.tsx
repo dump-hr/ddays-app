ce316d0ce8d4f69b6bb40dd1a6496e749f3d5a00
Lovre Tomić <tomiclovre05@gmail.com>
2025-04-06 08:52:12 +0200

    added title section

--- apps/sponsor/src/pages/FlyTalksPage/FlyTalksPage.tsx
+++ apps/sponsor/src/pages/FlyTalksPage/FlyTalksPage.tsx
@@ -1,5 +1,16 @@
+import c from './FlyTalksPage.module.scss';
+
 const FlyTalksPage = () => {
-  return <></>;
+  return (
+    <div className={c.page}>
+      <div className={c.content}>
+        <section className={c.titleSection}>
+          <h2 className={c.title}>Fly Talks</h2>
+          <p>Ukupno prihvaćenih prijava: 3/25</p>
+        </section>
+      </div>
+    </div>
+  );
 };
 
 export default FlyTalksPage;
