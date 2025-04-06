ce5c1357c914722ddca613a13108befb5eb80157
Lovre Tomić <tomiclovre05@gmail.com>
2025-04-06 09:01:16 +0200

    added table content

--- apps/sponsor/src/pages/FlyTalksPage/FlyTalksPage.tsx
+++ apps/sponsor/src/pages/FlyTalksPage/FlyTalksPage.tsx
@@ -1,3 +1,4 @@
+import InfoMessage from '../../components/InfoMessage';
 import c from './FlyTalksPage.module.scss';
 
 const FlyTalksPage = () => {
@@ -8,6 +9,33 @@ const FlyTalksPage = () => {
           <h2 className={c.title}>Fly Talks</h2>
           <p>Ukupno prihvaćenih prijava: 3/25</p>
         </section>
+        <InfoMessage message='Odabir sudionika zatvorit će se u srijedu 21. 5. u 12:00.' />
+        <table>
+          <thead>
+            <tr>
+              <th>status</th>
+              <th>ime i prezime</th>
+              <th>e-adresa</th>
+              <th>godina rođenja</th>
+              <th>CV</th>
+              <th>Odabir</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td></td>
+              <td>Ivan</td>
+              <td>Ivan.gmail.com</td>
+              <td>2002.</td>
+              <td>
+                <button>ok</button>
+              </td>
+              <td>
+                <button>ODABRI</button>
+              </td>
+            </tr>
+          </tbody>
+        </table>
       </div>
     </div>
   );
