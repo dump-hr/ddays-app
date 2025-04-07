79ddb4b10cad21368af317bfaaae79c465bff2fc
Lovre Tomić <tomiclovre05@gmail.com>
2025-04-07 15:01:47 +0200

    fixed docker errors

--- apps/sponsor/src/constants/pages.ts
+++ apps/sponsor/src/constants/pages.ts
@@ -1,9 +1,9 @@
-import flyTalksSvg from '../assets/icons/fly-talks.svg';
+//import flyTalksSvg from '../assets/icons/fly-talks.svg';
 import materialsSvg from '../assets/icons/materials.svg';
 import profileSvg from '../assets/icons/profile.svg';
 //import standSvg from '../assets/icons/stand.svg';
 import { CompanyProfile } from '../pages/CompanyProfile';
-import FlyTalksPage from '../pages/FlyTalksPage';
+//import FlyTalksPage from '../pages/FlyTalksPage';
 import { MaterialsPage } from '../pages/MaterialsPage';
 //import { SpotsPage } from '../pages/SpotsPage/SpotsPage';
 import { Path } from './paths';
