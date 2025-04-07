644bf437e25433a10c43d25b54f275b0889340ed
ToniGrbic <tonigrbic.5@gmail.com>
2025-04-07 02:15:34 +0200

    user modify dto

--- packages/types/src/dto/user.ts
+++ packages/types/src/dto/user.ts
@@ -1,3 +1,11 @@
+import {
+  IsBoolean,
+  IsEmail,
+  IsNumber,
+  IsOptional,
+  IsString,
+} from 'class-validator';
+
 export type UserDto = {
   email: string;
   firstName: string;
@@ -11,3 +19,43 @@ export type UserDto = {
   companiesNewsEnabled: boolean;
   termsAndConditionsEnabled: boolean;
 };
+
+export class UserModifyDto {
+  @IsString()
+  firstName: string;
+
+  @IsString()
+  lastName: string;
+
+  @IsEmail()
+  email: string;
+
+  @IsString()
+  password: string;
+
+  @IsString()
+  phoneNumber: string;
+
+  @IsNumber()
+  birthYear: number;
+
+  @IsString()
+  @IsOptional()
+  educationDegree?: string;
+
+  @IsString()
+  @IsOptional()
+  occupation?: string;
+
+  @IsNumber()
+  @IsOptional()
+  newsletterEnabled?: boolean;
+
+  @IsBoolean()
+  @IsOptional()
+  companiesNewsEnabled?: boolean;
+
+  @IsBoolean()
+  @IsOptional()
+  isConfirmed?: boolean;
+}
