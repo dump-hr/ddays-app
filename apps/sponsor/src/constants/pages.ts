95ad93c23a0b565019ed693eb4e81b3f89434bc1
tsiklic1 <100917481+tsiklic1@users.noreply.github.com>
2025-04-10 16:36:12 +0200

    resolving stash conflict

--- apps/sponsor/src/constants/pages.ts
+++ apps/sponsor/src/constants/pages.ts
@@ -1,8 +1,10 @@
+//import flyTalksSvg from '../assets/icons/fly-talks.svg';
 import materialsSvg from '../assets/icons/materials.svg';
 import profileSvg from '../assets/icons/profile.svg';
 //import flyTalksSvg from '../assets/icons/fly-talks.svg';
 //import standSvg from '../assets/icons/stand.svg';
 import { CompanyProfile } from '../pages/CompanyProfile';
+//import FlyTalksPage from '../pages/FlyTalksPage';
 import { MaterialsPage } from '../pages/MaterialsPage';
 //import { SpotsPage } from '../pages/SpotsPage/SpotsPage';
 import { Path } from './paths';
@@ -21,7 +23,15 @@ export const pages: Page[] = [
     icon: materialsSvg,
     Component: MaterialsPage,
   },
-  /*{
+  ,
+  /*
+  {
+    path: Path.FlyTalks,
+    icon: flyTalksSvg,
+    name: 'Fly Talks',
+    Component: FlyTalksPage,
+  },
+  {
     path: Path.Stand,
     icon: standSvg,
     name: 'Štand',
@@ -38,8 +48,7 @@ export const pages: Page[] = [
     name: 'Odabir mjesta',
     icon: standSvg,
     Component: SpotsPage,
-  },*/
-  {
+  },*/ {
     path: Path.Profile,
     icon: profileSvg,
     name: 'Profil tvrtke',
