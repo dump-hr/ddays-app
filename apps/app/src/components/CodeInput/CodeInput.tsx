19a78c2cf59fb03ac821e5887fce60eeaa3f544b
Lovre Tomić <tomiclovre05@gmail.com>
2025-04-11 13:15:11 +0200

    refactored logic and styles

--- apps/app/src/components/CodeInput/CodeInput.tsx
+++ apps/app/src/components/CodeInput/CodeInput.tsx
@@ -9,52 +9,62 @@ const CodeInput = () => {
   function editChar(index: number, value: string) {
     const newCode = [...code];
     newCode[index] = value;
-    if (value !== '') {
-      if (index < 5) {
-        focusInput(index + 1);
-      } else {
-        focusInput(5);
-      }
-    }
-    if (value === '') {
-      if (index > 0) {
-        focusInput(index - 1);
-      } else {
-        focusInput(0);
-      }
-    }
-
     setCode(newCode);
   }
 
   function focusInput(index: number) {
-    if (inputRefs.current[index]) {
-      inputRefs.current[index]?.focus();
+    const input = inputRefs.current[index];
+
+    if (input) {
+      input.focus();
     }
+  }
+
+  function unfocusInputs() {
+    inputRefs.current.forEach((input) => {
+      if (input) {
+        input.blur();
+      }
+    });
   }
 
   function handleKeyDown(
     e: React.KeyboardEvent<HTMLInputElement>,
     index: number,
   ) {
-    if (e.key === 'Backspace' && code[index] === '') {
-      if (index > 0) {
-        const newCode = [...code];
-        newCode[index - 1] = '';
-        setCode(newCode);
-        focusInput(index - 1);
-      }
-    } else if (
-      e.key !== 'Backspace' &&
-      code[index] !== '' &&
-      index < code.length - 1
-    ) {
-      if (code[index + 1] !== '') {
-        const newCode = [...code];
-        newCode[index + 1] = '';
-        setCode(newCode);
-      }
-      focusInput(index + 1);
+    switch (e.key) {
+      case 'Backspace':
+        if (code[index] !== '') editChar(index, '');
+        if (index > 0) {
+          focusInput(index - 1);
+        }
+        break;
+      case 'ArrowLeft':
+        if (index > 0) {
+          focusInput(index - 1);
+        }
+        break;
+      case 'ArrowRight':
+        if (index < code.length - 1) {
+          focusInput(index + 1);
+        }
+        break;
+      default:
+        if (e.key.length > 1) {
+          return;
+        }
+
+        if (index < code.length - 1) {
+          focusInput(index + 1);
+        }
+
+        if (index === code.length - 1) {
+          unfocusInputs();
+        }
+
+        editChar(index, e.key);
+
+        break;
     }
   }
 
@@ -67,10 +77,6 @@ const CodeInput = () => {
           maxLength={1}
           value={code[i]}
           className={clsx(c.input, { [c.filled]: code[i] !== '' })}
-          onChange={(e) => {
-            const value = e.target.value;
-            editChar(i, value);
-          }}
           ref={(el) => (inputRefs.current[i] = el)}
           onKeyDown={(e) => handleKeyDown(e, i)}
         />
