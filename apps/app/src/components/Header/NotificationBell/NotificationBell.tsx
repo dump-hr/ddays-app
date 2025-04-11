f86f04fd08a2941ff09c99772937a6f903839095
ToniGrbic <tonigrbic.5@gmail.com>
2025-04-11 02:43:34 +0200

    open desktop modal on desktop screen

--- apps/app/src/components/Header/NotificationBell/NotificationBell.tsx
+++ apps/app/src/components/Header/NotificationBell/NotificationBell.tsx
@@ -1,15 +1,30 @@
 import sprite from '@/assets/sprite.svg';
 import styles from './NotificationBell.module.scss';
 import { useNavigate } from 'react-router-dom';
-import { RouteNames } from '../../../router/routes';
+import { RouteNames } from '@/router/routes';
+import { useDeviceType } from '@/hooks/UseDeviceType';
+import { Dispatch, SetStateAction } from 'react';
 
-export const NotificationBell: React.FC = () => {
+interface NotificationBellProps {
+  setIsOpenPopup?: Dispatch<SetStateAction<boolean>>;
+}
+
+export const NotificationBell: React.FC<NotificationBellProps> = ({
+  setIsOpenPopup = null,
+}) => {
   const navigate = useNavigate();
+  const { isMobile } = useDeviceType({ breakpoint: 768 });
+
+  const handleClick = () => {
+    if (isMobile) {
+      navigate(RouteNames.NOTIFICATIONS);
+      return;
+    }
+    if (setIsOpenPopup) setIsOpenPopup(true);
+  };
 
   return (
-    <div
-      className={styles.notificationWrapper}
-      onClick={() => navigate(RouteNames.NOTIFICATIONS)}>
+    <div className={styles.notificationWrapper} onClick={handleClick}>
       <svg className={styles.notificationIcon} width={32} height={32}>
         <use href={`${sprite}#notification-bell-icon`} />
       </svg>
