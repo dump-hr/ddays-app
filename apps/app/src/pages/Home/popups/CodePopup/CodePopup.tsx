419adc3766180a5e4e42bf67624d3528c610251d
Lovre Tomić <tomiclovre05@gmail.com>
2025-04-11 13:32:45 +0200

    added mobile styles

--- apps/app/src/pages/Home/popups/CodePopup/CodePopup.tsx
+++ apps/app/src/pages/Home/popups/CodePopup/CodePopup.tsx
@@ -1,6 +1,7 @@
 import PopupLayout from '@/layout/PopupLayout/PopupLayout';
 import c from './CodePopup.module.scss';
 import CodeInput from '@/components/CodeInput';
+import Button from '@/components/Button';
 
 type CodePopupProps = {
   closePopup: () => void;
@@ -11,12 +12,17 @@ const CodePopup: React.FC<CodePopupProps> = ({ closePopup, isOpen }) => {
   return (
     <PopupLayout
       variant='light'
+      desktopStyle='stretch'
       headerTitleComponent='UNESI KOD'
       closePopup={closePopup}
       isOpen={isOpen}>
       <div className={c.content}>
         <CodeInput />
       </div>
+      <p className={c.message}>Imaš kod za bodove? Unesi ga.</p>
+      <Button className={c.button} variant='orange' disabled>
+        Unesi
+      </Button>
     </PopupLayout>
   );
 };
