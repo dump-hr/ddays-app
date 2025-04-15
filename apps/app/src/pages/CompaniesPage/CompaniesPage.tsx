9544ef62529b6eefd50517fbab4cc7b8cb739f2a
Lovre Tomić <tomiclovre05@gmail.com>
2025-04-15 12:43:08 +0200

    added first page content and styles

--- apps/app/src/pages/CompaniesPage/CompaniesPage.tsx
+++ apps/app/src/pages/CompaniesPage/CompaniesPage.tsx
@@ -1,7 +1,19 @@
+import c from './CompaniesPage.module.scss';
+
 export const CompaniesPage = () => {
   return (
-    <div>
-      <h1>Companies</h1>
+    <div className={c.page}>
+      <header className={c.header}>
+        <h2 className={c.title}>Tvrtke</h2>
+      </header>
+      <main className={c.main}>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Architecto
+          rem autem officia laudantium veniam exercitationem ab unde voluptatum
+          voluptas. Labore est minima sunt deserunt. Voluptatibus sapiente
+          aspernatur tempora error voluptates!
+        </p>
+      </main>
     </div>
   );
 };
