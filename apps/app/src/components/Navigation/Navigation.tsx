122d6e33041e4c4cf877353cce0be0a628922619
Lovre Tomić <92167015+lovretomic@users.noreply.github.com>
2025-04-19 14:59:50 +0200

    added routes

--- apps/app/src/components/Navigation/Navigation.tsx
+++ apps/app/src/components/Navigation/Navigation.tsx
@@ -1,6 +1,8 @@
 import { ReactElement, useState } from 'react';
 import styles from './Navigation.module.scss';
 import sprite from '../../assets/sprite.svg';
+import { RouteNames } from '@/router/routes';
+import { useNavigate } from 'react-router-dom';
 
 enum Tabs {
   HOME,
@@ -15,21 +17,54 @@ type TabInfo = {
   icon: string;
   width: number;
   height: number;
+  route: string;
 };
 
 const tabs: TabInfo[] = [
-  { id: Tabs.HOME, icon: 'home-icon', width: 24, height: 24 },
-  { id: Tabs.SCHEDULE, icon: 'schedule-icon', width: 25, height: 24 },
-  { id: Tabs.FLY_TALKS, icon: 'fly-talks-icon', width: 22, height: 24 },
-  { id: Tabs.COMPANIES, icon: 'companies-icon', width: 23, height: 24 },
-  { id: Tabs.PROFILE, icon: 'profile-icon', width: 22, height: 22 },
+  {
+    id: Tabs.HOME,
+    icon: 'home-icon',
+    width: 24,
+    height: 24,
+    route: RouteNames.HOME,
+  },
+  {
+    id: Tabs.SCHEDULE,
+    icon: 'schedule-icon',
+    width: 25,
+    height: 24,
+    route: RouteNames.SCHEDULE,
+  },
+  {
+    id: Tabs.FLY_TALKS,
+    icon: 'fly-talks-icon',
+    width: 22,
+    height: 24,
+    route: RouteNames.FLY_TALKS,
+  },
+  {
+    id: Tabs.COMPANIES,
+    icon: 'companies-icon',
+    width: 23,
+    height: 24,
+    route: RouteNames.COMPANIES,
+  },
+  {
+    id: Tabs.PROFILE,
+    icon: 'profile-icon',
+    width: 22,
+    height: 22,
+    route: RouteNames.PROFILE,
+  },
 ];
 
 export const Navigation = (): ReactElement => {
   const [selectedTab, setSelectedTab] = useState<Tabs>(Tabs.HOME);
+  const navigate = useNavigate();
 
   const handleTabChange = (tab: Tabs) => {
     setSelectedTab(tab);
+    navigate(tabs[tab].route);
   };
 
   return (
