562d16c82667fe40b6ba1e2c70b39d3142da7d85
Lovre Tomić <92167015+lovretomic@users.noreply.github.com>
2025-04-19 15:54:11 +0200

    extracted navigation consts

--- apps/app/src/components/DesktopNavigation/DesktopNavigation.tsx
+++ apps/app/src/components/DesktopNavigation/DesktopNavigation.tsx
@@ -2,82 +2,22 @@ import styles from './DesktopNavigation.module.scss';
 import { ReactElement, useState } from 'react';
 import { useLocation, useNavigate } from 'react-router-dom';
 import { NotificationBell } from '../Header/NotificationBell';
-import { RouteNames } from '@/router/routes';
 import sprite from '../../assets/sprite.svg';
 import DesktopNotificationsPopup from '@/pages/Home/popups/DesktopNotificationsPopup';
-
-enum Tabs {
-  HOME,
-  SCHEDULE,
-  FLY_TALKS,
-  COMPANIES,
-  PROFILE,
-}
-
-type TabInfo = {
-  id: Tabs;
-  icon: string;
-  width: number;
-  height: number;
-  name: string;
-  route: RouteNames;
-};
-
-const tabs: TabInfo[] = [
-  {
-    id: Tabs.HOME,
-    icon: 'home-icon',
-    width: 24,
-    height: 24,
-    name: 'Home',
-    route: RouteNames.HOME,
-  },
-  {
-    id: Tabs.SCHEDULE,
-    icon: 'schedule-icon',
-    width: 25,
-    height: 24,
-    name: 'Raspored',
-    route: RouteNames.SCHEDULE,
-  },
-  {
-    id: Tabs.FLY_TALKS,
-    icon: 'fly-talks-icon',
-    width: 22,
-    height: 24,
-    name: 'Fly talks',
-    route: RouteNames.FLY_TALKS,
-  },
-  {
-    id: Tabs.COMPANIES,
-    icon: 'companies-icon',
-    width: 23,
-    height: 24,
-    name: 'Tvrtke',
-    route: RouteNames.COMPANIES,
-  },
-  {
-    id: Tabs.PROFILE,
-    icon: 'profile-icon',
-    width: 22,
-    height: 22,
-    name: 'Profil',
-    route: RouteNames.PROFILE,
-  },
-];
+import { NAVIGATION_TAB_ID, navigationTabs } from '@/router/navigationTabs';
 
 export const DesktopNavigation = (): ReactElement => {
   const [isOpenPopup, setIsOpenPopup] = useState(false);
   const navigate = useNavigate();
   const { pathname } = useLocation();
 
-  const handleTabChange = (tab: Tabs) => {
-    navigate(tabs[tab].route);
+  const handleTabChange = (tab: NAVIGATION_TAB_ID) => {
+    navigate(navigationTabs[tab].route);
   };
 
   return (
     <div className={styles.container}>
-      {tabs.map((tab) => (
+      {navigationTabs.map((tab) => (
         <div
           key={tab.id}
           className={`${styles.iconWrapper} ${
--- apps/app/src/components/MobileNavigation/MobileNavigation.tsx
+++ apps/app/src/components/MobileNavigation/MobileNavigation.tsx
@@ -1,79 +1,25 @@
 import { ReactElement, useState } from 'react';
 import styles from './MobileNavigation.module.scss';
 import sprite from '../../assets/sprite.svg';
-import { RouteNames } from '@/router/routes';
 import { useNavigate } from 'react-router-dom';
-
-enum TabId {
-  HOME,
-  SCHEDULE,
-  FLY_TALKS,
-  COMPANIES,
-  PROFILE,
-}
-
-type TabInfo = {
-  id: TabId;
-  icon: string;
-  width: number;
-  height: number;
-  route: string;
-};
-
-const tabs: TabInfo[] = [
-  {
-    id: TabId.HOME,
-    icon: 'home-icon',
-    width: 24,
-    height: 24,
-    route: RouteNames.HOME,
-  },
-  {
-    id: TabId.SCHEDULE,
-    icon: 'schedule-icon',
-    width: 25,
-    height: 24,
-    route: RouteNames.SCHEDULE,
-  },
-  {
-    id: TabId.FLY_TALKS,
-    icon: 'fly-talks-icon',
-    width: 22,
-    height: 24,
-    route: RouteNames.FLY_TALKS,
-  },
-  {
-    id: TabId.COMPANIES,
-    icon: 'companies-icon',
-    width: 23,
-    height: 24,
-    route: RouteNames.COMPANIES,
-  },
-  {
-    id: TabId.PROFILE,
-    icon: 'profile-icon',
-    width: 22,
-    height: 22,
-    route: RouteNames.PROFILE,
-  },
-];
+import { NAVIGATION_TAB_ID, navigationTabs } from '@/router/navigationTabs';
 
 export const MobileNavigation = (): ReactElement => {
   const currentPath = window.location.pathname;
-  const currentTab = tabs.find((tab) => tab.route === currentPath);
-  const [selectedTab, setSelectedTab] = useState<TabId>(
-    currentTab ? currentTab.id : TabId.HOME,
+  const currentTab = navigationTabs.find((tab) => tab.route === currentPath);
+  const [selectedTab, setSelectedTab] = useState<NAVIGATION_TAB_ID>(
+    currentTab ? currentTab.id : NAVIGATION_TAB_ID.HOME,
   );
   const navigate = useNavigate();
 
-  const handleTabChange = (tab: TabId) => {
+  const handleTabChange = (tab: NAVIGATION_TAB_ID) => {
     setSelectedTab(tab);
-    navigate(tabs[tab].route);
+    navigate(navigationTabs[tab].route);
   };
 
   return (
     <div className={styles.container}>
-      {tabs.map((tab) => (
+      {navigationTabs.map((tab) => (
         <div
           key={tab.id}
           className={`${styles.iconWrapper} ${
