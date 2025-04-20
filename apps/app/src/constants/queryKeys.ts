daedd109eeba1b396182c45b29ee4343b5cc9f30
ToniGrbic <tonigrbic.5@gmail.com>
2025-04-21 01:43:13 +0200

    new query key

--- apps/app/src/constants/queryKeys.ts
+++ apps/app/src/constants/queryKeys.ts
@@ -6,4 +6,5 @@ export const QUERY_KEYS = {
   userPoints: 'userPoints',
   userNotifications: 'userNotifications',
   currentUser: 'currentUser',
+  numOfUnreadNotifications: 'numOfUnreadNotifications',
 };
