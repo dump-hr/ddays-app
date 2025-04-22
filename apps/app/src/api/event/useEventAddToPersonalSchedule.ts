b40280081239517f0a936eda639a7bdb3d071ffa
Lovre Tomić <tomiclovre05@gmail.com>
2025-04-22 18:46:05 +0200

    added invalidating queries

--- apps/app/src/api/event/useEventAddToPersonalSchedule.ts
+++ apps/app/src/api/event/useEventAddToPersonalSchedule.ts
@@ -1,19 +1,22 @@
+import { QUERY_KEYS } from '@/constants/queryKeys';
 import { UserToEventDto } from '@ddays-app/types/src/dto/user';
 import axios from 'axios';
 import toast from 'react-hot-toast';
-import { useMutation } from 'react-query';
+import { useMutation, useQueryClient } from 'react-query';
 
 const eventAddToPersonalSchedule = (eventId: number, data: UserToEventDto) => {
   return axios.post(`/api/event/${eventId}/join`, data);
 };
 
 export const useEventAddToPersonalSchedule = () => {
+  const queryClient = useQueryClient();
   return useMutation(
     (params: { eventId: number; data: UserToEventDto }) =>
       eventAddToPersonalSchedule(params.eventId, params.data),
     {
       onSuccess: () => {
         toast.success('Događaj je dodan u tvoj raspored.');
+        void queryClient.invalidateQueries([QUERY_KEYS.events]);
       },
       onError: (error) => {
         if (axios.isAxiosError(error)) {
--- apps/app/src/api/event/useEventRemoveFromPersonalSchedule.ts
+++ apps/app/src/api/event/useEventRemoveFromPersonalSchedule.ts
@@ -1,7 +1,8 @@
+import { QUERY_KEYS } from '@/constants/queryKeys';
 import { UserToEventDto } from '@ddays-app/types/src/dto/user';
 import axios from 'axios';
 import toast from 'react-hot-toast';
-import { useMutation } from 'react-query';
+import { useMutation, useQueryClient } from 'react-query';
 
 const eventRemoveFromPersonalSchedule = (
   eventId: number,
@@ -11,12 +12,14 @@ const eventRemoveFromPersonalSchedule = (
 };
 
 export const useEventRemoveFromPersonalSchedule = () => {
+  const queryClient = useQueryClient();
   return useMutation(
     (params: { eventId: number; data: UserToEventDto }) =>
       eventRemoveFromPersonalSchedule(params.eventId, params.data),
     {
       onSuccess: () => {
         toast.success('Događaj je uklonjen iz tvog rasporeda.');
+        void queryClient.invalidateQueries([QUERY_KEYS.events]);
       },
       onError: (error) => {
         if (axios.isAxiosError(error)) {
