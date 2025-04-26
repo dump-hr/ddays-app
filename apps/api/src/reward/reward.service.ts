13c7eb56de5c00cfbd6f1f82b36130fd70fe26e6
tsiklic1 <100917481+tsiklic1@users.noreply.github.com>
2025-04-26 10:56:24 +0200

    implement reward service

--- apps/api/src/reward/reward.service.ts
+++ apps/api/src/reward/reward.service.ts
@@ -1,7 +1,26 @@
 import { Injectable } from '@nestjs/common';
 import { PrismaService } from 'src/prisma.service';
+import { RewardDto } from '@ddays-app/types/src/dto/reward';
 
 @Injectable()
 export class RewardService {
   constructor(private readonly prisma: PrismaService) {}
+
+  async createReward(createRewardDto: RewardDto) {
+    await this.prisma.reward.create({
+      data: createRewardDto,
+    });
+  }
+
+  async getAllRewards() {
+    return await this.prisma.reward.findMany({});
+  }
+
+  async removeReward(id: number) {
+    return await this.prisma.reward.delete({
+      where: {
+        id,
+      },
+    });
+  }
 }
---
+++ packages/types/src/dto/reward.ts
@@ -0,0 +1,6 @@
+export type RewardDto = {
+  id: number;
+  name: string | null;
+  imageUrl: string | null;
+  description: string | null;
+};
