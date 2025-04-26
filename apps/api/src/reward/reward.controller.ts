cfc06731645c1bc9c3f3a565fe8fb9cf65e21af2
tsiklic1 <100917481+tsiklic1@users.noreply.github.com>
2025-04-26 11:03:26 +0200

    implement reward controller

--- apps/api/src/reward/reward.controller.ts
+++ apps/api/src/reward/reward.controller.ts
@@ -1,7 +1,36 @@
-import { Controller } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Param,
+  ParseIntPipe,
+  Post,
+  UseGuards,
+} from '@nestjs/common';
 import { RewardService } from './reward.service';
+import { AdminGuard } from 'src/auth/admin.guard';
+import { RewardDto } from '@ddays-app/types/src/dto/reward';
+import { UserGuard } from 'src/auth/user.guard';
 
 @Controller('reward')
 export class RewardController {
   constructor(private readonly rewardService: RewardService) {}
+
+  @Post('create')
+  @UseGuards(AdminGuard)
+  createReward(@Body() rewardDto: RewardDto) {
+    return this.rewardService.createReward(rewardDto);
+  }
+
+  @Post()
+  @UseGuards(UserGuard)
+  getAllRewards() {
+    return this.rewardService.getAllRewards();
+  }
+
+  @Delete(':id')
+  @UseGuards(AdminGuard)
+  removeReward(@Param('id', ParseIntPipe) id: number) {
+    return this.rewardService.removeReward(id);
+  }
 }
