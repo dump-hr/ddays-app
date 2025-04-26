a6097f78069c6e09c92b666d41485dd6589408dd
Lovre Tomić <tomiclovre05@gmail.com>
2025-04-26 21:50:43 +0200

    added 3 sec cooldown

--- apps/app/src/pages/AchievementScannerPage/AchievementScannerPage.tsx
+++ apps/app/src/pages/AchievementScannerPage/AchievementScannerPage.tsx
@@ -1,6 +1,6 @@
 import { QrReader } from 'react-qr-reader';
 import c from './AchievementScannerPage.module.scss';
-import { useState } from 'react';
+import { useRef, useState } from 'react';
 import ScannedAchievementPopup from './popups/ScannedAchievementPopup';
 import { QrCodeData, QrCodeDataType } from '@/types/qr/qr';
 import toast from 'react-hot-toast';
@@ -10,8 +10,17 @@ const AchievementScannerPage = () => {
   const [data, setData] = useState('');
   const { data: achievement } = useAchievementGetByUuid(data);
   const [isOpen, setIsOpen] = useState(false);
+  const lastScanTimeRef = useRef<number>(0);
 
   function handleScan(data: string) {
+    const currentTime = Date.now();
+
+    if (currentTime - lastScanTimeRef.current < 3000) {
+      return;
+    }
+
+    lastScanTimeRef.current = currentTime;
+
     if (!data || isOpen) return;
 
     let parsedData: QrCodeData | null = null;
@@ -19,12 +28,12 @@ const AchievementScannerPage = () => {
     try {
       parsedData = JSON.parse(data) as QrCodeData;
     } catch {
-      toast.error('Ovaj QR kod nije ispravan!');
-      return;
-    }
-
-    if (!parsedData) {
-      toast.error('Podaci nisu ispravni!');
+      toast.error(
+        'Ovaj QR kod nije ispravan! ' +
+          currentTime +
+          ' ' +
+          lastScanTimeRef.current,
+      );
       return;
     }
 
