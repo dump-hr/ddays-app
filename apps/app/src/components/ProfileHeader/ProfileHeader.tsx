c517831ca342ee09165332a7a5e9f2d37e809d8d
Lovre Tomić <tomiclovre05@gmail.com>
2025-05-03 09:03:32 +0200

    added calculating percentages

--- apps/app/src/components/ProfileHeader/ProfileHeader.tsx
+++ apps/app/src/components/ProfileHeader/ProfileHeader.tsx
@@ -1,32 +1,50 @@
+import { useLoggedInUser } from '@/api/auth/useLoggedInUser';
 import AvatarPointsCircle from '../AvatarPointsCircle/AvatarPointsCircle';
 import { InterestProgressBar } from '../InterestProgressBar';
 import c from './ProfileHeader.module.scss';
 import TempAvatar from '@/assets/images/temp-avatar.png';
+import { useUserSelectedInterests } from '@/api/interests/useUserSelectedInterests';
+import { useInterestsGetAll } from '@/api/interests/useInterestsGetAll';
+import { Theme } from '@ddays-app/types';
 
 type ProfileHeaderProps = {
   pageHeader: string;
   userNameAndSurname: string;
 };
-export const ProfileHeader = ({
-  pageHeader,
-  userNameAndSurname,
-}: ProfileHeaderProps) => {
+export const ProfileHeader = ({ pageHeader }: ProfileHeaderProps) => {
+  const { data: user } = useLoggedInUser();
+  const { data: userInterests } = useUserSelectedInterests(user?.id || 0);
+  const { data: interests } = useInterestsGetAll();
+
+  function getPercentage(theme: string) {
+    if (interests === undefined || userInterests === undefined) return 0;
+
+    const userInterestsInTheme = userInterests.filter(
+      (interest) => interest.theme === theme,
+    ).length;
+
+    return Math.round((userInterestsInTheme / userInterests.length) * 100);
+  }
+
   return (
     <header className={c.header}>
       <div className={c.flexWrapper}>
         <p className={c.title}>
           <span>{pageHeader}</span> <br />
-          {userNameAndSurname}
+          {user?.firstName} {user?.lastName}
         </p>
 
         <AvatarPointsCircle points={900} avatar={TempAvatar} />
       </div>
 
       <div className={c.statsWrapper}>
-        <InterestProgressBar label='Programiranje' percentage={50} />
-        <InterestProgressBar label='Marketing' percentage={72} />
-        <InterestProgressBar label='Product' percentage={22} />
-        <InterestProgressBar label='Product' percentage={22} />
+        {Object.values(Theme).map((theme) => (
+          <InterestProgressBar
+            label={theme}
+            percentage={getPercentage(theme)}
+            key={theme}
+          />
+        ))}
       </div>
     </header>
   );
