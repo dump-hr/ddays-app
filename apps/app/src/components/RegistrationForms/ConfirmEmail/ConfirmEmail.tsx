02a83af44483a8237982fc0e4ac62d5d711bc81c
Lovre Tomić <tomiclovre05@gmail.com>
2025-05-03 20:00:41 +0200

    added confirmation resend and style update

--- apps/app/src/components/RegistrationForms/ConfirmEmail/ConfirmEmail.tsx
+++ apps/app/src/components/RegistrationForms/ConfirmEmail/ConfirmEmail.tsx
@@ -6,12 +6,22 @@ import { useNavigate, useSearchParams } from 'react-router-dom';
 import { useEffect, useState } from 'react';
 import toast from 'react-hot-toast';
 import PointModifierPopup from '@/pages/Home/popups/PointModifierPopup';
+import { useSendConfirmationEmail } from '@/api/email/useSendConfirmationEmail';
+import { useLoggedInUser } from '@/api/auth/useLoggedInUser';
+
+const COUNTDOWN_TIME = 60; // 60 seconds
 
 export const ConfirmEmail = () => {
   const { isMobile } = useDeviceType({});
   const navigate = useNavigate();
   const [searchParams] = useSearchParams();
   const [showPointsPopup, setShowPointsPopup] = useState(false);
+
+  const { mutate: sendConfirmationEmail } = useSendConfirmationEmail();
+  const { data: user } = useLoggedInUser();
+
+  const [timeLeft, setTimeLeft] = useState<number>(0);
+  const [lastSentTime, setLastSentTime] = useState<number | null>(null);
 
   useEffect(() => {
     const token = searchParams.get('token');
@@ -39,6 +49,36 @@ export const ConfirmEmail = () => {
     }
   };
 
+  const handleSendEmail = () => {
+    if (!user?.email) {
+      toast.error('Greška pri slanju emaila.');
+      return;
+    }
+
+    sendConfirmationEmail(user?.email);
+    const currentTime = Date.now();
+    setLastSentTime(currentTime);
+    setTimeLeft(COUNTDOWN_TIME);
+
+    const timer = setInterval(() => {
+      setTimeLeft((prevTime) => {
+        if (prevTime <= 1) {
+          clearInterval(timer);
+          return 0;
+        }
+        return prevTime - 1;
+      });
+    }, 1000);
+  };
+
+  useEffect(() => {
+    if (lastSentTime) {
+      const elapsed = Math.floor((Date.now() - lastSentTime) / 1000);
+      const remainingTime = Math.max(COUNTDOWN_TIME - elapsed, 0);
+      setTimeLeft(remainingTime);
+    }
+  }, [lastSentTime]);
+
   const handleClosePointsPopup = () => {
     setShowPointsPopup(false);
     navigate('/app');
@@ -54,18 +94,19 @@ export const ConfirmEmail = () => {
           )}
         </h2>
         <p>
-          Poslali smo ti e-mail da potvrdimo ispravnost unesene adrese. Nakon
-          što se e-mail pojavi u tvom sandučiću, klikni na link kako bi
-          registracija bila uspješna.
+          Poslali smo ti e-mail {user?.email ? `na ${user.email}` : ''} da
+          potvrdimo ispravnost adrese. Nakon što se e-mail pojavi u tvom
+          sandučiću, klikni na link kako bi registracija bila uspješna.
         </p>
       </div>
 
       <Button
-        children='Potvrdi email'
+        children={
+          timeLeft > 0 ? `Pošalji ponovno (${timeLeft} s)` : 'Pošalji ponovno'
+        }
         variant='orange'
-        onClick={() => {
-          toast.error('Otvori link u emailu koji smo ti poslali');
-        }}
+        onClick={handleSendEmail}
+        disabled={timeLeft > 0}
       />
 
       <PointModifierPopup
