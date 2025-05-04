cc6055e66a54bfc5f18c50aaabec71719ba2e6e2
andrea-zecevic <azecev01@gmail.com>
2025-05-04 10:49:36 +0200

    Added reset email resend

--- apps/app/src/pages/PasswordResetPage/steps/EmailSentStep.tsx
+++ apps/app/src/pages/PasswordResetPage/steps/EmailSentStep.tsx
@@ -4,6 +4,11 @@ import { Header } from '../components/Header';
 import closeIcon from '../../../assets/icons/close-icon.svg';
 import { Link } from 'react-router-dom';
 import { RouteNames } from '../../../router/routes';
+import { useState, useEffect } from 'react';
+import toast from 'react-hot-toast';
+import { sendVerificationEmail } from '../../../helpers/handleVerificationSent';
+
+const COUNTDOWN_TIME = 60; // 60 seconds
 
 interface EmailSentStepProps {
   email: string;
@@ -11,6 +16,43 @@ interface EmailSentStepProps {
 }
 
 export const EmailSentStep = ({ email, onNext }: EmailSentStepProps) => {
+  const [timeLeft, setTimeLeft] = useState<number>(0);
+  const [lastSentTime, setLastSentTime] = useState<number | null>(null);
+
+  const handleSendEmail = async () => {
+    if (!email) {
+      toast.error('Greška pri slanju emaila.');
+      return;
+    }
+
+    await sendVerificationEmail(
+      email,
+      (error) => toast.error(error),
+      () => toast.success('Email je uspješno poslan!'),
+    );
+    const currentTime = Date.now();
+    setLastSentTime(currentTime);
+    setTimeLeft(COUNTDOWN_TIME);
+
+    const timer = setInterval(() => {
+      setTimeLeft((prevTime) => {
+        if (prevTime <= 1) {
+          clearInterval(timer);
+          return 0;
+        }
+        return prevTime - 1;
+      });
+    }, 1000);
+  };
+
+  useEffect(() => {
+    if (lastSentTime) {
+      const elapsed = Math.floor((Date.now() - lastSentTime) / 1000);
+      const remainingTime = Math.max(COUNTDOWN_TIME - elapsed, 0);
+      setTimeLeft(remainingTime);
+    }
+  }, [lastSentTime]);
+
   return (
     <>
       <Header showDuck={true} />
@@ -32,6 +74,16 @@ export const EmailSentStep = ({ email, onNext }: EmailSentStepProps) => {
           <Button variant='orange' onClick={onNext}>
             Resetiraj lozinku
           </Button>
+          <Button
+            children={
+              timeLeft > 0
+                ? `Pošalji ponovno (${timeLeft} s)`
+                : 'Pošalji ponovno'
+            }
+            variant='orange'
+            onClick={handleSendEmail}
+            disabled={timeLeft > 0}
+          />
         </div>
       </div>
     </>
