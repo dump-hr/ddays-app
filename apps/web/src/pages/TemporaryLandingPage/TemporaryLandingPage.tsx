c0b86a2be68e9c4053de58110027fe7fa5a09f24
Lovre Tomić <tomiclovre05@gmail.com>
2025-05-07 00:44:52 +0200

    added log in button to web

--- apps/web/src/pages/TemporaryLandingPage/TemporaryLandingPage.tsx
+++ apps/web/src/pages/TemporaryLandingPage/TemporaryLandingPage.tsx
@@ -1,12 +1,36 @@
 import { ReactLenis } from '@studio-freight/react-lenis';
+import Button from 'components/Button';
+import Header from 'components/Header';
+import MobileMenu from 'components/MobileMenu';
 import TemporaryFooterSection from 'components/TemporaryFooterSection';
-import TemporaryHeader from 'components/TemporaryHeader';
 import TemporaryHero from 'components/TemporaryHero';
+import { useState } from 'react';
+
+import { temporaryLandingNavigation } from '../../constants/temporary-landing-navigation';
 
 export const TemporaryLandingPage: React.FC = () => {
+  const [mobileMenuOpen, setMobileMenuOpen] = useState(false);
+  const RegisterButton = (
+    <Button onClick={() => (window.location.href = 'https://days.dump.hr/app')}>
+      Prijavi se
+    </Button>
+  );
+
+  const toggleMobileMenu = () => {
+    setMobileMenuOpen((prev) => !prev);
+  };
+
   return (
     <ReactLenis root>
-      <TemporaryHeader />
+      <Header Button={RegisterButton} toggleMobileMenu={toggleMobileMenu} />
+      {window.innerWidth < 768 && (
+        <MobileMenu
+          Button={RegisterButton}
+          isOpen={mobileMenuOpen}
+          toggle={toggleMobileMenu}
+          items={temporaryLandingNavigation}
+        />
+      )}
       <TemporaryHero />
       <TemporaryFooterSection />
     </ReactLenis>
