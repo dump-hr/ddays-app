e926f92a2ed238f7141aea20334cb34e7cba70c4
Lovre Tomić <tomiclovre05@gmail.com>
2025-05-12 00:04:45 +0200

    fix

--- apps/web/src/components/FooterSection/FooterSection.tsx
+++ apps/web/src/components/FooterSection/FooterSection.tsx
@@ -21,7 +21,7 @@ const FooterSection = () => {
             <button
               className={clsx(c.button, c.grainyButton)}
               onClick={() =>
-                (window.location.href = 'https://days-app.dump.hr/app/login')
+                (window.location.href = 'https://days.dump.hr/app/login')
               }>
               {`[ `}
               REGISTRIRAJ SVOJ DOLAZAK
