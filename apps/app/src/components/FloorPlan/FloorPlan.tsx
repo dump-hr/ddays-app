1a5eff3944c87aeaa8b3bf70d762bb35b92529fb
Lovre Tomić <tomiclovre05@gmail.com>
2025-05-12 14:51:18 +0200

    added booth positions

--- apps/app/src/components/FloorPlan/FloorPlan.tsx
+++ apps/app/src/components/FloorPlan/FloorPlan.tsx
@@ -16,31 +16,35 @@ type BoothWithPosition = {
 const boothPositions: BoothWithPosition[] = [
   {
     boothString: 'Z1',
-    position: { x: 40.5, y: 63 },
+    position: { x: 40.5, y: 63.3 },
   },
   {
     boothString: 'Z2',
-    position: { x: 0, y: 0 },
+    position: { x: 52.2, y: 63.3 },
   },
   {
     boothString: 'Z3',
-    position: { x: 0, y: 0 },
+    position: { x: 52.2, y: 43 },
+  },
+  {
+    boothString: 'Z4',
+    position: { x: 43, y: 31.5 },
   },
   {
     boothString: 'S1',
-    position: { x: 0, y: 0 },
+    position: { x: 59.1, y: 65.1 },
   },
   {
     boothString: 'S2',
-    position: { x: 0, y: 0 },
+    position: { x: 64.6, y: 65.1 },
   },
   {
     boothString: 'S3',
-    position: { x: 0, y: 0 },
+    position: { x: 70, y: 65.1 },
   },
   {
     boothString: 'S4',
-    position: { x: 0, y: 0 },
+    position: { x: 75.4, y: 65.1 },
   },
   {
     boothString: 'S5',
@@ -48,35 +52,35 @@ const boothPositions: BoothWithPosition[] = [
   },
   {
     boothString: 'S6',
-    position: { x: 0, y: 0 },
+    position: { x: 70, y: 47.5 },
   },
   {
     boothString: 'S7',
-    position: { x: 0, y: 0 },
+    position: { x: 70, y: 40.5 },
   },
   {
     boothString: 'S8',
-    position: { x: 0, y: 0 },
+    position: { x: 59.1, y: 40.5 },
   },
   {
     boothString: 'S9',
-    position: { x: 0, y: 0 },
+    position: { x: 64.6, y: 40.5 },
   },
   {
     boothString: 'S10',
-    position: { x: 0, y: 0 },
+    position: { x: 64.6, y: 47.5 },
   },
   {
     boothString: 'S11',
-    position: { x: 0, y: 0 },
+    position: { x: 59.1, y: 47.5 },
   },
   {
     boothString: 'S12',
-    position: { x: 0, y: 0 },
+    position: { x: 51.6, y: 19.1 },
   },
   {
     boothString: 'S13',
-    position: { x: 0, y: 0 },
+    position: { x: 60.6, y: 19.1 },
   },
 ];
 
