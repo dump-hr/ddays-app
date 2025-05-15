2c1416796c90deb45654fca1aa9c04764e5c39a4
Lovre Tomić <tomiclovre05@gmail.com>
2025-05-15 20:11:59 +0200

    added fetching workshops

--- apps/admin/src/pages/HomePage.tsx
+++ apps/admin/src/pages/HomePage.tsx
@@ -1,8 +1,11 @@
+import { useGetWorkshopsWithUsers } from '../api/event/useGetWorkshopsWithUsers';
 import { useGetUserCount } from '../api/user/useGetUserCount';
 import c from './HomePage.module.scss';
 
 export const HomePage = () => {
   const { data: userCount } = useGetUserCount();
+  const { data: workshops } = useGetWorkshopsWithUsers();
+
   return (
     <div className={c.page}>
       <h1>Statistike 2025.</h1>
@@ -10,6 +13,10 @@ export const HomePage = () => {
         <p>Trenutno korisnika:</p>
         <h3>{userCount}</h3>
       </div>
+      <section>
+        <p>Radionice</p>
+        <button onClick={() => console.log(workshops)}>Prikaži podatke</button>
+      </section>
     </div>
   );
 };
