5745b112fdc0500b1a73f62967b3f52415e4e554
ToniGrbic <tonigrbic.5@gmail.com>
2025-05-18 20:03:43 +0200

    refactor to class validators

--- packages/types/src/dto/shop.ts
+++ packages/types/src/dto/shop.ts
@@ -1,5 +1,6 @@
-import { ShoppingCartItemStage } from '../enum';
-import { ShopItemType } from '@prisma/client';
+import { IsEnum, IsNumber, IsOptional, IsString } from 'class-validator';
+import { ShoppingCartItemStage, ShopItemType } from '../enum';
+
 export type ShopItemDto = {
   id: number;
   type: ShopItemType | null;
@@ -8,12 +9,51 @@ export type ShopItemDto = {
   price: number | null;
 };
 
-export type TransactionCreateDto = {
-  shopItemId: number;
-  userId: number;
+export class ShopItemModifyDto {
+  @IsEnum(ShopItemType)
+  @IsOptional()
+  type?: ShopItemType;
+
+  @IsOptional()
+  @IsString()
+  itemName?: string;
+
+  @IsOptional()
+  @IsNumber()
+  quantity?: number;
+
+  @IsOptional()
+  @IsNumber()
+  price?: number;
+}
+
+export class ShopItemCreateDto {
+  @IsEnum(ShopItemType)
+  type: ShopItemType;
+
+  @IsString()
+  itemName: string;
+
+  @IsNumber()
   quantity: number;
+
+  @IsNumber()
+  price: number;
+}
+
+export class TransactionCreateDto {
+  @IsNumber()
+  shopItemId: number;
+
+  @IsNumber()
+  userId: number;
+
+  @IsNumber()
+  quantity: number;
+
+  @IsEnum(ShoppingCartItemStage)
   stage: ShoppingCartItemStage;
-};
+}
 
 export type TransactionItemDto = {
   shopItemId: number;
