bf02139ce4e708f4b36df30b82c68084d4a172b1
ToniGrbic <tonigrbic.5@gmail.com>
2025-05-20 22:59:08 +0200

    removed unused nestjs cron module

--- apps/api/src/notification/notification.module.ts
+++ apps/api/src/notification/notification.module.ts
@@ -1,12 +1,10 @@
 import { Module } from '@nestjs/common';
-import { ScheduleModule } from '@nestjs/schedule';
 import { PrismaService } from 'src/prisma.service';
 
 import { NotificationController } from './notification.controller';
 import { NotificationService } from './notification.service';
 
 @Module({
-  imports: [ScheduleModule.forRoot()],
   controllers: [NotificationController],
   providers: [NotificationService, PrismaService],
 })
