c184642f90a7eef9a6c40a56a575114c5776fbe9
Lovre Tomić <tomiclovre05@gmail.com>
2025-05-21 18:31:58 +0200

    updated redirecting after code apply

--- apps/app/src/pages/Home/popups/CodePopup/CodePopup.tsx
+++ apps/app/src/pages/Home/popups/CodePopup/CodePopup.tsx
@@ -24,6 +24,14 @@ const CodePopup: React.FC<CodePopupProps> = ({
     applyCode.mutate(code, {
       onSuccess: (submittedCode) => {
         closePopup();
+        console.log(submittedCode);
+        if (submittedCode.redirectUrl) {
+          const currentUrl = new URL(window.location.href);
+          const appendPath = submittedCode.redirectUrl;
+          currentUrl.pathname =
+            currentUrl.pathname.replace(/\/$/, '') + appendPath;
+          window.location.href = currentUrl.toString();
+        }
         setCode(Array(6).fill(''));
         onSuccess(submittedCode.code.points || 0);
       },
