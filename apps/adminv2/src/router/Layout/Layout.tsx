681f114ee7ca91081fb5f182d4a9ea6f8119e3ca
Lovre Tomić <tomiclovre05@gmail.com>
2025-05-27 12:34:50 +0200

    added layout styles

--- apps/adminv2/src/router/Layout/Layout.tsx
+++ apps/adminv2/src/router/Layout/Layout.tsx
@@ -1,16 +1,14 @@
 import { Outlet } from 'react-router-dom';
 import c from './Layout.module.scss';
+import ProfilePicturePlaceholder from '/src/assets/images/profile-picture-placeholder.jpg';
 
 export const Layout = () => {
   return (
-    <>
+    <div className={c.layout}>
       <div className={c.sidebar}>
         <div className={c.profileInfoWrapper}>
           <div className={c.profileInfo}>
-            <img
-              src='https://upload.wikimedia.org/wikipedia/commons/6/65/No-Image-Placeholder.svg'
-              alt=''
-            />
+            <img src={ProfilePicturePlaceholder} alt='' />
             <div className={c.profileDetails}>
               <div className={c.roleAndName}>
                 <div className={c.role}>S</div>
@@ -19,12 +17,13 @@ export const Layout = () => {
               <p className={c.email}>lovre.tomic@dump.hr</p>
             </div>
           </div>
-          <button className={c.logoutButton}>Odjava</button>
+          <button className={c.logoutButton}>O</button>
         </div>
       </div>
-      <main>
+      <main className={c.main}>
+        <header className={c.header}></header>
         <Outlet />
       </main>
-    </>
+    </div>
   );
 };
