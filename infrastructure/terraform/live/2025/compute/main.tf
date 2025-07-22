b5bb39fc9456bc2ec8487ec4ef33612cbc877b91
Bartol Deak <b@bdeak.net>
2025-07-22 18:58:09 +0200

    add 2025 archive

--- infrastructure/terraform/live/2025/compute/main.tf
+++ infrastructure/terraform/live/2025/compute/main.tf
@@ -47,19 +47,19 @@ data "sops_file" "secrets" {
 module "web" {
   source = "../../../modules/ec2"
 
-  name_prefix               = "ddays-app-production"
+  name_prefix               = "ddays-app-2025"
   instance_type             = "t3a.nano"
   instance_count            = 1
   instance_root_device_size = 20
   subnets                   = data.aws_subnets.public_subnets.ids
   security_groups           = data.aws_security_groups.public_sg.ids
-  ssh_public_key            = file("../../../../ssh-keys/production.pub")
-  website_domain            = "days.dump.hr"
+  ssh_public_key            = file("../../../../ssh-keys/2025.pub")
+  website_domain            = "2025-days.dump.hr"
   cloudflare_zone_id        = data.cloudflare_zone.dump_hr.id
 
   tags = {
     Project     = "ddays-app"
     Role        = "web"
-    Environment = "production"
+    Environment = "2025"
   }
 }
