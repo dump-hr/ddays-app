a9e9477a170012464f13043d760081f2bb8f4325
lukazuljevic <luka.zuljo@gmail.com>
2025-09-12 18:26:38 +0200

    Add AD login

--- apps/adminv2/src/App.tsx
+++ apps/adminv2/src/App.tsx
@@ -1,6 +1,10 @@
+// import { InteractionType } from '@azure/msal-browser';
+// import { useMsalAuthentication } from '@azure/msal-react';
 import { Router } from './router/Router';
 
 function App() {
+  // useMsalAuthentication(InteractionType.Redirect);
+
   return <Router />;
 }
 
--- apps/adminv2/src/main.tsx
+++ apps/adminv2/src/main.tsx
@@ -1,10 +1,17 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { PublicClientApplication } from '@azure/msal-browser';
+import { MsalProvider } from '@azure/msal-react';
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import './index.css';
+import App from './App.tsx';
+
+import { msalConfig } from './configs/auth';
+const msalInstance = new PublicClientApplication(msalConfig);
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <MsalProvider instance={msalInstance}>
+      <App />
+    </MsalProvider>
   </StrictMode>,
-)
+);
