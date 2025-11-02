d3e1dfc45ce80651ba266364cb5a5eccadf4588c
ninom5 <nmilas00@fesb.hr>
2025-11-02 20:52:35 +0100

    UI modifications

--- apps/app/src/components/RegistrationForms/SecondStepRegistrationForm/InvitationCodeInput.tsx
+++ apps/app/src/components/RegistrationForms/SecondStepRegistrationForm/InvitationCodeInput.tsx
@@ -1,15 +1,18 @@
 import { useState, useRef } from 'react';
 import './InvitationCodeInput.scss';
 
+const CODE_LENGTH = 6;
+
 export const InvitationCodeInput = () => {
-  const CODE_LENGTH = 6;
   const [code, setCode] = useState<string[]>(Array(CODE_LENGTH).fill(''));
   const inputsRef = useRef<(HTMLInputElement | null)[]>([]);
 
   const handleChange = (value: string, index: number) => {
     if (!/^[a-zA-Z0-9]?$/.test(value)) return;
+
     const newCode = [...code];
     newCode[index] = value.toUpperCase();
+
     setCode(newCode);
 
     if (value && index < CODE_LENGTH - 1) {
@@ -29,18 +32,22 @@ export const InvitationCodeInput = () => {
   return (
     <div className='invitation-code'>
       <h3>Imaš kod?</h3>
+
       <div className='code-inputs'>
         {code.map((char, index) => (
-          <input
-            key={index}
-            ref={(el) => (inputsRef.current[index] = el)}
-            type='text'
-            maxLength={1}
-            className='code-box'
-            value={char}
-            onChange={(e) => handleChange(e.target.value, index)}
-            onKeyDown={(e) => handleKeyDown(e, index)}
-          />
+          <div key={index} className='code-input-wrapper'>
+            <input
+              ref={(el) => (inputsRef.current[index] = el)}
+              type='text'
+              maxLength={1}
+              className='code-box'
+              value={char}
+              onChange={(e) => handleChange(e.target.value, index)}
+              onKeyDown={(e) => handleKeyDown(e, index)}
+            />
+
+            {index === 2 && <span className='minus'> - </span>}
+          </div>
         ))}
       </div>
     </div>
