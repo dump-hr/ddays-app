62ce4efed539c6ec73a3bbc384feaf78d703e461
lukazuljevic <luka.zuljo@gmail.com>
2025-11-02 22:18:42 +0100

    Fix deploy job

--- apps/api/src/survey-question/survey-question.service.ts
+++ apps/api/src/survey-question/survey-question.service.ts
@@ -3,7 +3,7 @@ import {
   SurveyQuestionModifyDto,
   SurveyQuestionType,
 } from '@ddays-app/types';
-import { Injectable, NotFoundException } from '@nestjs/common';
+import { Injectable} from '@nestjs/common';
 import { PrismaService } from 'src/prisma.service';
 
 @Injectable()
