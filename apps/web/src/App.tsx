9032f1c52c127eff218816284bdc07729379fd98
lukazuljevic <luka.zuljo@gmail.com>
2026-01-18 21:37:20 +0100

    Fix favicon

--- apps/web/src/App.tsx
+++ apps/web/src/App.tsx
@@ -4,36 +4,24 @@ import { Helmet, HelmetProvider } from 'react-helmet-async';
 import { Toaster } from 'react-hot-toast';
 import { Route, Switch } from 'wouter';
 
-import faviconBlack from './assets/favicon-black.png';
-import faviconWhite from './assets/favicon-white.png';
-import { Path } from './constants/paths';
+// import { Path } from './constants/paths';
 import { getPageTitle } from './helpers';
 // import { Chatbot } from './pages/Chatbot';
-import { LandingPage } from './pages/LandingPage';
-// import { TemporaryLandingPage } from './pages/TemporaryLandingPage';
+// import { LandingPage } from './pages/LandingPage';
+import { TemporaryLandingPage } from './pages/TemporaryLandingPage';
 
 export const App = () => {
   return (
     <HelmetProvider>
       <Helmet>
-        <link
-          href={faviconBlack}
-          rel='icon'
-          media='(prefers-color-scheme: light)'
-        />
-        <link
-          href={faviconWhite}
-          rel='icon'
-          media='(prefers-color-scheme: dark)'
-        />
 
         <title>{getPageTitle()}</title>
       </Helmet>
 
       <Switch>
-        <Route path={Path.Landing} component={LandingPage} />
+        {/* <Route path={Path.Landing} component={LandingPage} /> */}
         {/*<Route path={Path.Chatbot} component={Chatbot} /> */}
-        {/* <Route path='/' component={TemporaryLandingPage} /> */}
+        <Route path='/' component={TemporaryLandingPage} />
       </Switch>
 
       <Toaster />
