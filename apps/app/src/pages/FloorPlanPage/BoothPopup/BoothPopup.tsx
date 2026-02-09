5d36453d955d6d80ff24f8a8d1ad8528490ca6c4
ToniGrbic <tonigrbic.5@gmail.com>
2026-02-09 22:49:15 +0100

    remove rate company button

--- apps/app/src/pages/FloorPlanPage/BoothPopup/BoothPopup.tsx
+++ apps/app/src/pages/FloorPlanPage/BoothPopup/BoothPopup.tsx
@@ -1,13 +1,10 @@
 import clsx from 'clsx';
 import { PropsWithChildren } from 'react';
-import { useNavigate } from 'react-router-dom';
 import styles from './BoothPopup.module.scss';
 import { FloorPlanCompanyDto, RatingDto } from '@ddays-app/types';
 import InfoLightIcon from '@/assets/icons/info-light.svg';
 import CloseIcon from '@/assets/icons/remove-icon.svg';
 import { isBoothRated } from '@/helpers/isBoothRated';
-import { RouteNames } from '@/router/routes';
-import Button from '@/components/Button';
 
 type BoothPopupProps = {
   closePopup: () => void;
@@ -24,7 +21,6 @@ const BoothPopup = ({
   availableBooths,
   userRatings,
 }: PropsWithChildren<BoothPopupProps>) => {
-  const navigate = useNavigate();
   const isRated = isBoothRated(companyInfo.booth, availableBooths, userRatings);
 
   return (
@@ -75,29 +71,13 @@ const BoothPopup = ({
           )}
 
           {!isRated && (
-            <>
-              <div className={styles.infoMessage}>
-                <img className={styles.icon} src={InfoLightIcon} />
-                <p className={styles.message}>
-                  Nakon što posjetiš štand, možeš ga ocijeniti i osvojiti
-                  dodatne bodove!
-                </p>
-              </div>
-              <Button
-                variant="orange"
-                className={styles.rateButton}
-                onClick={() => {
-                  closePopup();
-                  navigate(
-                    RouteNames.RATE_COMPANY.replace(
-                      ':companyId',
-                      String((companyInfo as unknown as { id: number }).id),
-                    ),
-                  );
-                }}>
-                Ocijeni štand
-              </Button>
-            </>
+            <div className={styles.infoMessage}>
+              <img className={styles.icon} src={InfoLightIcon} />
+              <p className={styles.message}>
+                Nakon što posjetiš štand, možeš ga ocijeniti i osvojiti dodatne
+                bodove!
+              </p>
+            </div>
           )}
         </div>
       </div>
